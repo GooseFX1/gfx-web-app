d98a5a690750b38bd42db80469db17aaf5aacfd9
Jonathan Daniels <multivurse.technologies@gmail.com>
2021-10-09 18:44:41 -0700

    improved chart UI

--- src/pages/Synths/MyPortfolio.tsx
+++ src/pages/Synths/MyPortfolio.tsx
@@ -1,8 +1,11 @@
 import { FC } from 'react'
 import styled from 'styled-components'
 import { MyPortfolioHeader } from './MyPorfolioHeader'
+import MyPortfolioChart from './MyPortfolioChart'
 
 const WRAPPER = styled.div`
+  padding: 0;
+  margin: 0;
   color: ${({ theme }) => theme.text1};
 `
 
@@ -10,6 +13,7 @@ export const MyPortfolio: FC = () => {
   return (
     <WRAPPER>
       <MyPortfolioHeader></MyPortfolioHeader>
+      <MyPortfolioChart></MyPortfolioChart>
     </WRAPPER>
   )
 }
