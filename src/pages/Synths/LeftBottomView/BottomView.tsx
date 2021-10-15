39451926a28a223a0b976f689043399256c76932
Jonathan Daniels <multivurse.technologies@gmail.com>
2021-10-15 13:37:39 -0700

    bottom view tab header

--- src/pages/Synths/LeftBottomView/BottomView.tsx
+++ src/pages/Synths/LeftBottomView/BottomView.tsx
@@ -6,72 +6,42 @@ import { Row, Col, Select, Tabs } from 'antd'
 import React from 'react'
 import { useDarkMode } from '../../../context'
 import { SVGToWhite } from '../../../styles'
+import { GtokensView } from './GtokensView'
+import { CollateralView } from './CollateralView'
 
 const { TabPane } = Tabs
 
 const { Option } = Select
-
-const BUTTON = styled.button`
-  display: flex;
-  justify-content: right;
-  align-items: center;
-  height: 80px;
-  width: 175px;
-  border: none;
-  border-radius: 20px;
-`
-const TEXT = styled.span`
-  display: flex;
-  justify-content: left;
-  align-items: flex-start;
-  font-size: 14px;
-  margin-right: 20px;
-  color: white;
-  font-weight: 500;
-`
-const ARROW = styled.div`
-  display: flex;
-  justify-content: left;
-  align-items: flex-start;
-  margin-right: 10px;
-  margin-top: 2px;
-`
-
-const MENU = styled.div`
-  width: 175px;
-  margin-top: 12%;
-  padding-top: 10px;
-  padding-bottom: 10px;
-  margin-right: 10px;
-  border-radius: 20px;
-  background-color: #131212;
-`
-
-const MENUITEM = styled.div`
-  margin-top: 0px;
-  margin-bottom: 0px;
-  height: '100%';
-  padding-bottom: 8px;
-  padding-top: 8px;
-  width: 175px;
-  border-radius: 5px;
-  background-color: #3f3f3f;
-`
 
 const Nav = ['gtokens', 'Collateral']
 
 export const BottomView: FC = () => {
   return (
     <div>
+      <div
+        style={{
+          backgroundColor: '#121212',
+          borderTopLeftRadius: 20,
+          borderTopRightRadius: 20
+        }}
+      >
+        <Button shape={'round'} style={{ outline: 'none', border: 'none' }}>
+          <img src={`${process.env.PUBLIC_URL}/img/synths/dash.svg`} style={{ height: 5, width: 25 }} />
+        </Button>
+      </div>
       <Tabs
         type={'line'}
         centered={true}
         tabBarGutter={40}
-        tabBarStyle={{ height: 80, backgroundColor: '#121212', borderRadius: 20 }}
+        tabBarStyle={{
+          height: 20,
+          marginBottom: 0,
+          backgroundColor: '#121212'
+        }}
       >
         {Nav.map((value, index) => (
           <TabPane key={index} tab={value}>
-            {index === 0 ? 'View one' : 'View two'}
+            {index === 0 ? <GtokensView /> : <CollateralView />}
           </TabPane>
         ))}
       </Tabs>
---
+++ src/pages/Synths/LeftBottomView/GtokensView.tsx
@@ -0,0 +1,28 @@
+import { FC } from 'react'
+import styled from 'styled-components'
+import { BottomView } from './BottomView'
+
+const WRAPPER = styled.div`
+  color: ${({ theme }) => theme.text1};
+  ${({ theme }) => theme.mediaWidth.fromLarge`
+  height: auto;
+  width: 100%;
+`};
+  ${({ theme }) => theme.mediaWidth.upToLarge`
+height: auto;
+width: 100%;
+`};
+`
+
+export const GtokensView: FC = () => {
+  return (
+    <div
+      style={{
+        height: 40,
+        backgroundColor: '#121212',
+        borderBottomLeftRadius: 20,
+        borderBottomRightRadius: 20
+      }}
+    ></div>
+  )
+}
