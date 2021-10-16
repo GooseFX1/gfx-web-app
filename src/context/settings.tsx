8c57cc5db733a3dc9f29d4a60ac13ab1c444884a
Terry Finkel <sheddarsheez@protonmail.com>
2021-10-16 21:53:06 +0300

    WIP

--- src/context/settings.tsx
+++ src/context/settings.tsx
@@ -11,8 +11,16 @@ interface IEndpoint {
 }
 
 export const ENDPOINTS: IEndpoint[] = [
-  { chainId: ENV.MainnetBeta, endpoint: 'https://solana-api.projectserum.com', network: WalletAdapterNetwork.Mainnet },
-  { chainId: ENV.Devnet, endpoint: 'https://api.devnet.solana.com', network: WalletAdapterNetwork.Devnet }
+  {
+    chainId: ENV.MainnetBeta,
+    endpoint: 'https://solana-api.projectserum.com',
+    network: WalletAdapterNetwork.Mainnet
+  },
+  {
+    chainId: ENV.Devnet,
+    endpoint: 'https://restless-blue-leaf.solana-devnet.quiknode.pro/061fb972cd25346376ab1d7a93c2e8fc628ae60a/',
+    network: WalletAdapterNetwork.Devnet
+  }
 ]
 
 interface ISettingsConfig {
