5db16b7459532cc9ea33042c46d5a345a360202e
Jonathan Daniels <multivurse.technologies@gmail.com>
2021-10-15 21:12:34 -0700

    added table header

--- src/pages/Synths/LeftBottomView/GtokensView.tsx
+++ src/pages/Synths/LeftBottomView/GtokensView.tsx
@@ -1,6 +1,6 @@
+import { Col, Row } from 'antd'
 import { FC } from 'react'
 import styled from 'styled-components'
-import { BottomView } from './BottomView'
 
 const WRAPPER = styled.div`
   color: ${({ theme }) => theme.text1};
@@ -13,16 +13,33 @@ height: auto;
 width: 100%;
 `};
 `
+const BOTTOMTAB = styled.div``
 
 export const GtokensView: FC = () => {
   return (
-    <div
-      style={{
-        height: 40,
-        backgroundColor: '#121212',
-        borderBottomLeftRadius: 20,
-        borderBottomRightRadius: 20
-      }}
-    ></div>
+    <WRAPPER>
+      <Row
+        justify={'space-between'}
+        align={'bottom'}
+        style={{
+          height: 50,
+          backgroundColor: '#121212',
+          borderBottomLeftRadius: 20,
+          borderBottomRightRadius: 20,
+          width: '100%',
+          display: 'flex',
+          justifyContent: 'center',
+          alignSelf: 'flex-end',
+          paddingBottom: 10
+        }}
+      >
+        <Col flex={1}>Market</Col>
+        <Col flex={1}>Current Price</Col>
+        <Col flex={1}>Average Price</Col>
+        <Col flex={1}>Amount</Col>
+        <Col flex={1}>Profit/Loss</Col>
+        <Col flex={1}>Debt</Col>
+      </Row>
+    </WRAPPER>
   )
 }
