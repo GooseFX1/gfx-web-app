49f168ee216ceaf5ce9e55acfebc29032e171a46
blanedev <1z0ranz0ran100@gmail.com>
2021-12-13 15:56:04 +0200

    Fixed vertical scrollbar issue & Added liveAuction, openbid, and fixedprice screens

--- src/pages/NFTs/Form/LabelSection.tsx
+++ src/pages/NFTs/Form/LabelSection.tsx
@@ -1,31 +1,34 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
-export const STYLED_LABLE = styled.span`
-  display: flex;
-  align-items: center;
-  font-size: 17px;
-  font-weight: 600;
-  color: ${({ theme }) => theme.text1};
-  line-height: 1;
-  margin-bottom: ${({ theme }) => theme.margins['1x']};
-  .heart-purple {
-    width: 35px;
-    height: 35px;
-    margin-left: ${({ theme }) => theme.margins['1x']};
-    padding-top: ${({ theme }) => theme.margins['1x']};
-  }
+export const STYLED_LABLE = styled.span<{ size: string }>`
+  ${({ size }) => css`
+    display: flex;
+    align-items: center;
+    font-size: ${size};
+    font-weight: 600;
+    color: ${({ theme }) => theme.text1};
+    line-height: 1;
+    margin-bottom: ${({ theme }) => theme.margins['1x']};
+    .heart-purple {
+      width: 35px;
+      height: 35px;
+      margin-left: ${({ theme }) => theme.margins['1x']};
+      padding-top: ${({ theme }) => theme.margins['1x']};
+    }
+  `}
 `
 
 type Props = {
   label: string
   isIcon?: boolean
   className?: string
+  size?: string
 }
 
-export const LabelSection = ({ label, isIcon, className }: Props) => {
+export const LabelSection = ({ label, isIcon, className, size = '17px' }: Props) => {
   return (
-    <STYLED_LABLE className={className}>
+    <STYLED_LABLE className={className} size={size}>
       {label}
       {isIcon && <img className="heart-purple" src={`${process.env.PUBLIC_URL}/img/assets/heart-purple.svg`} alt="" />}
     </STYLED_LABLE>
