8877728b8756e5a83c3ed118b68c972ca7e2660e
Luke Woodward <20819255+lukewalt@users.noreply.github.com>
2022-01-17 22:04:38 -0500

    adjusts mockdata for display purposes

--- src/pages/NFTs/Collection/mockData.js
+++ src/pages/NFTs/Collection/mockData.js
@@ -1,10 +1,3 @@
-export const statsData = [
-  { title: 'Items', total: 1500, unit: '' },
-  { title: 'Owners', total: 1300, unit: '' },
-  { title: 'Price floor', total: 1.15, unit: 'SOL' },
-  { title: 'Volume traded', total: 22300, unit: '' }
-]
-
 export const NFTsData = [
   {
     id: '1',
