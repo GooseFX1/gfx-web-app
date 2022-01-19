1cc582718638e5181ce6d18e8ba426b346721dc5
Luke Woodward <20819255+lukewalt@users.noreply.github.com>
2022-01-18 23:18:11 -0500

    creates barrelled export of `NFT` resources

--- src/api/NFTs/index.ts
+++ src/api/NFTs/index.ts
@@ -1,16 +1,2 @@
-export const NFT_API_BASE = 'nest-api'
-
-export enum NFT_API_ENDPOINTS {
-  SESSION_USER = '/user',
-  USER_ACTIVITY = '/user-activity',
-  ALL_COLLECTIONS = '/all-collections',
-  FEATURED_COLLECTIONS = '/featured-collections',
-  UPCOMING_COLLECTIONS = '/upcoming-collections',
-  SINGLE_COLLECTION = '/collection',
-  LIVE_AUCTIONS = '/live-auctions',
-  OPEN_BID = '/open-bid',
-  FIXED_PRICE = '/fixed-price',
-  SINGLE_NFT = '/nft',
-  BID = '/bid',
-  ASK = '/ask'
-}
+export * from './constants'
+export * from './actions'
