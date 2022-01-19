866ac34503797ca18b16d3a4f20a7743e72f2ec4
Luke Woodward <20819255+lukewalt@users.noreply.github.com>
2022-01-18 23:41:30 -0500

    updates user nft section by removing unneeded values

--- src/pages/NFTs/Profile/TabProfile.tsx
+++ src/pages/NFTs/Profile/TabProfile.tsx
@@ -6,22 +6,22 @@ const tabPanes = (isExplore: boolean) => [
   {
     order: '1',
     name: 'Collected',
-    component: <TabContent type="collected" data={[1, 2, 4, 5, 5, 6, 7, 8, 9, 10]} isExplore={isExplore} />
+    component: <TabContent type="collected" />
   },
   {
     order: '2',
     name: 'Created',
-    component: <TabContent type="created" data={[1, 2, 3, 4, 5]} isExplore={isExplore} />
+    component: <TabContent type="created" />
   },
   {
     order: '3',
     name: 'Favorited',
-    component: <TabContent type="favorited" data={[1, 2, 3, 4, 5]} isExplore={isExplore} />
+    component: <TabContent type="favorited" />
   },
   {
     order: '4',
     name: 'Activity',
-    component: <TabContent type="activity" data={[1, 2, 4, 5, 5, 6, 7, 8, 9, 10]} isExplore={isExplore} />
+    component: <TabContent type="activity" />
   }
 ]
 
