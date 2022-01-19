401621437e3866cddb4de50454731175d5f37450
Luke Woodward <20819255+lukewalt@users.noreply.github.com>
2022-01-18 23:38:47 -0500

    updates user profile updates modal to call `POST` and `PATCH` methods on the api; updates fields with data from `useNFTProfile` context

--- src/pages/NFTs/Profile/PopupProfile.tsx
+++ src/pages/NFTs/Profile/PopupProfile.tsx
@@ -1,65 +1,94 @@
-import React, { useState } from 'react'
+import React, { useState, useMemo } from 'react'
+import { useHistory } from 'react-router-dom'
 import { Form, Input, Upload, Button } from 'antd'
 import { PlusOutlined } from '@ant-design/icons'
 import { UploadChangeParam } from 'antd/lib/upload'
 import { UploadFile } from 'antd/lib/upload/interface'
 import { StyledPopupProfile, StyledFormProfile } from './PopupProfile.styled'
+import { useNFTProfile } from '../../../context'
+import { completeNFTUserProfile, updateNFTUser } from '../../../api/NFTs'
+import { ILocationState } from '../../../types/app_params.d'
+import { INFTProfile } from '../../../types/nft_profile.d'
 
 interface Props {
   visible: boolean
   setVisible: (value: boolean) => void
-  handleOk: () => void
   handleCancel: () => void
 }
 
-export const PopupProfile = ({ visible, setVisible, handleOk, handleCancel }: Props) => {
+export const PopupProfile = ({ visible, setVisible, handleCancel }: Props) => {
+  const { sessionUser } = useNFTProfile()
+  const history = useHistory<ILocationState>()
   const [form] = Form.useForm()
-
-  const initialValues = {
-    creator_name: 'yeoohr',
-    email: 'yeoohr@gmail.com',
-    biography: 'Bio',
-    instagram: 'instagram.com/yeoohr',
-    twitter: 'twitter.com/02yeoohr',
-    facebook: 'facebook.com/3456325089',
-    youtube: 'youtube.com/ Enter your url'
-  }
-
   const [avatar, setAvatar] = useState<any>()
+  const isCompletingProfile = useMemo(() => sessionUser.user_id === null, [sessionUser])
+  const [isLoading, setIsLoading] = useState<boolean>(false)
 
   const handleAvatar = (file: UploadChangeParam<UploadFile<any>>) => {
     setAvatar(file.fileList[0])
   }
 
   const onFinish = (values: any) => {
-    handleOk()
+    validateUserFields(values)
+
+    setIsLoading(true)
+    completeNFTUserProfile(sessionUser.pubkey).then((res) => {
+      if (res.response && res.response.status === 200 && res.response.data === true) {
+        updateNFTUser({ ...values }).then((res) => {
+          if (res === true) {
+            history.push(`NFTs/profile/${sessionUser.nickname}`)
+          } else {
+            console.error(`Error fetching user ${sessionUser.pubkey}`)
+          }
+        })
+      } else {
+        console.error('Error Completing Profile')
+        setIsLoading(false)
+      }
+    })
   }
 
   const onCancel = () => {
-    form.setFieldsValue(initialValues)
+    form.setFieldsValue(sessionUser)
     handleCancel()
+  }
+
+  const validateUserFields = (profiledata: INFTProfile): INFTProfile => {
+    return profiledata
   }
 
   return (
     <>
-      <StyledPopupProfile title="Edit profile" visible={visible} footer={null} maskClosable onCancel={onCancel}>
+      <StyledPopupProfile
+        title={isCompletingProfile ? 'Complete profile' : 'Edit profile'}
+        visible={visible}
+        footer={null}
+        maskClosable
+        onCancel={onCancel}
+      >
         <div className="avatar-wrapper">
           <div className="image-group">
-            <Upload className="avatar-image" listType="picture-card" maxCount={1} onChange={handleAvatar}>
-              <div className="image-wrap">
-                {!avatar && (
-                  <img
-                    className="img-current avatar-image"
-                    src={`${process.env.PUBLIC_URL}/img/assets/avatar-profile.png`}
-                    alt=""
-                  />
-                )}
-                <div className="icon-upload">
-                  <PlusOutlined />
+            <div>
+              <Upload className="avatar-image" listType="picture-card" maxCount={1} onChange={handleAvatar}>
+                <div className="image-wrap">
+                  {!avatar && (
+                    <img
+                      className="img-current avatar-image"
+                      src={`${
+                        !sessionUser.profile_pic_link || sessionUser.profile_pic_link === ''
+                          ? `${process.env.PUBLIC_URL}/img/assets/avatar.png}`
+                          : sessionUser.profile_pic_link
+                      }`}
+                      alt="profile"
+                    />
+                  )}
+                  <div className="icon-upload">
+                    <PlusOutlined />
+                  </div>
                 </div>
-              </div>
+              </Upload>
               <div className="text">Preview</div>
-            </Upload>
+            </div>
             <div className="note">
               <div>Minimum size 400x 400</div>
               <div>(Gif's work too).</div>
@@ -70,14 +99,14 @@ export const PopupProfile = ({ visible, setVisible, handleOk, handleCancel }: Pr
           form={form}
           layout="vertical"
           requiredMark="optional"
-          initialValues={initialValues}
+          initialValues={sessionUser}
           onFinish={onFinish}
         >
           <div className="full-width">
             <div className="half-width">
               <Form.Item
-                name="creator_name"
-                label="Creator name"
+                name="nickname"
+                label="Creator Name"
                 rules={[{ required: true, message: 'Please input create name!' }]}
               >
                 <Input />
@@ -90,19 +119,19 @@ export const PopupProfile = ({ visible, setVisible, handleOk, handleCancel }: Pr
               <div className="hint">Will be used as public URL</div>
             </div>
           </div>
-          <Form.Item name="biography" label="Biography">
+          <Form.Item name="bio" label="Bio">
             <Input />
           </Form.Item>
           <div className="section-label">Social media links</div>
           <div className="full-width">
             <div className="half-width">
-              <Form.Item label="Instagram" name="instagram">
+              <Form.Item label="Instagram" name="instagram_link">
                 <Input />
               </Form.Item>
               <div className="hint">Will be used as public URL</div>
             </div>
             <div className="half-width">
-              <Form.Item label="Twitter" name="twitter">
+              <Form.Item label="Twitter" name="twitter_link">
                 <Input />
               </Form.Item>
               <div className="hint">Will be used as public URL</div>
@@ -110,20 +139,20 @@ export const PopupProfile = ({ visible, setVisible, handleOk, handleCancel }: Pr
           </div>
           <div className="full-width">
             <div className="half-width">
-              <Form.Item label="Facebook" name="facebook">
+              <Form.Item label="Facebook" name="facebook_link">
                 <Input />
               </Form.Item>
               <div className="hint">Will be used as public URL</div>
             </div>
             <div className="half-width">
-              <Form.Item label="Youtube" name="youtube">
+              <Form.Item label="Youtube" name="youtube_link">
                 <Input />
               </Form.Item>
               <div className="hint">Will be used as public URL</div>
             </div>
           </div>
           <Button className="btn-save" type="primary" htmlType="submit">
-            Save changes
+            {isLoading ? '...Saving' : 'Save changes'}
           </Button>
         </StyledFormProfile>
       </StyledPopupProfile>
