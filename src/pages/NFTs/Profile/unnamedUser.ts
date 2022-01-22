c6e4e949ad8bc36bc8394097ba798df0dfa1716b
Luke Woodward <20819255+lukewalt@users.noreply.github.com>
2022-01-21 21:17:02 -0500

    creates an unnamed user object to apply to state when wallet is disconnected or wallet address is not linked to a user in the database

---
+++ src/pages/NFTs/Profile/unnamedUser.ts
@@ -0,0 +1,13 @@
+export const unnamedUser = {
+  user_id: null,
+  pubkey: '',
+  nickname: 'Unnamed',
+  email: '',
+  bio: '',
+  twitter_link: '',
+  instagram_link: '',
+  facebook_link: '',
+  youtube_link: '',
+  profile_pic_link: '',
+  is_verified: false
+}
