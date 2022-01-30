0089f804f1e566bc26c8e44736fa89b2466a291b
Luke Woodward <20819255+lukewalt@users.noreply.github.com>
2022-01-30 01:06:05 -0500

    removes loading icons from progress steps and updates styles

--- src/pages/NFTs/Collectible/UploadProgress.tsx
+++ src/pages/NFTs/Collectible/UploadProgress.tsx
@@ -1,7 +1,36 @@
 import React, { useEffect } from 'react'
 import { Card, Steps } from 'antd'
+import styled from 'styled-components'
 
-const WaitingStep = (props: { mint: Function; minting: boolean; confirm: Function; step: number }) => {
+//#region styles
+const CONTAINER = styled.div`
+  position: absolute;
+  top: 0px;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background: #1e1e1e;
+  .ant-card {
+    background: transparent;
+  }
+  .ant-steps-item-process > .ant-steps-item-container > .ant-steps-item-icon {
+    background: ${({ theme }) => theme.secondary5};
+    border-color: ${({ theme }) => theme.secondary5};
+    font-weight: 700;
+  }
+  .ant-steps-vertical > .ant-steps-item {
+    padding: ${({ theme }) => theme.margins['1x']} 0;
+  }
+  .ant-steps-item-icon {
+    line-height: 30px;
+  }
+  .ant-steps-item-title {
+    font-weight: 700;
+  }
+`
+//#endregion
+
+const UploadProgress = (props: { mint: Function; confirm: Function; step: number }) => {
   useEffect(() => {
     const func = async () => {
       await props.mint()
@@ -10,67 +39,42 @@ const WaitingStep = (props: { mint: Function; minting: boolean; confirm: Functio
     func()
   }, [])
 
-  const setIconForStep = (currentStep: number, componentStep) => {
-    if (currentStep === componentStep) {
-      return '...loading'
-    }
-    return null
-  }
+  // const setIconForStep = (currentStep: number, componentStep) => {
+  //   if (currentStep === componentStep) {
+  //     return <Loader />
+  //   }
+  //   return null
+  // }
 
   return (
-    <div
-      style={{
-        marginTop: 70,
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center'
-      }}
-    >
-      <Card>
+    <CONTAINER>
+      <Card bordered={false}>
         <Steps direction="vertical" current={props.step}>
-          <Steps.Step
-            className={'white-description'}
-            title="Minting"
-            description="Starting Mint Process"
-            icon={setIconForStep(props.step, 0)}
-          />
-          <Steps.Step className={'white-description'} title="Preparing Assets" icon={setIconForStep(props.step, 1)} />
+          <Steps.Step className={'white-description'} title="Minting" description="Starting Mint Process" />
+          <Steps.Step className={'white-description'} title="Preparing Assets" />
           <Steps.Step
             className={'white-description'}
             title="Signing Metadata Transaction"
             description="Approve the transaction from your wallet"
-            icon={setIconForStep(props.step, 2)}
           />
           <Steps.Step
             className={'white-description'}
             title="Sending Transaction to Solana"
             description="This will take a few seconds."
-            icon={setIconForStep(props.step, 3)}
           />
-          <Steps.Step
-            className={'white-description'}
-            title="Waiting for Initial Confirmation"
-            icon={setIconForStep(props.step, 4)}
-          />
-          <Steps.Step
-            className={'white-description'}
-            title="Waiting for Final Confirmation"
-            icon={setIconForStep(props.step, 5)}
-          />
-          <Steps.Step
-            className={'white-description'}
-            title="Uploading to Arweave"
-            icon={setIconForStep(props.step, 6)}
-          />
-          <Steps.Step className={'white-description'} title="Updating Metadata" icon={setIconForStep(props.step, 7)} />
+          <Steps.Step className={'white-description'} title="Waiting for Initial Confirmation" />
+          <Steps.Step className={'white-description'} title="Waiting for Final Confirmation" />
+          <Steps.Step className={'white-description'} title="Uploading to Arweave" />
+          <Steps.Step className={'white-description'} title="Updating Metadata" />
           <Steps.Step
             className={'white-description'}
             title="Signing Token Transaction"
             description="Approve the final transaction from your wallet"
-            icon={setIconForStep(props.step, 8)}
           />
         </Steps>
       </Card>
-    </div>
+    </CONTAINER>
   )
 }
+
+export default UploadProgress
