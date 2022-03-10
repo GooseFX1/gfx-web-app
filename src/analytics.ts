2e1f5a779f36e67d54804f185665b428dac6fd93
Luke Woodward <20819255+lukewalt@users.noreply.github.com>
2022-03-10 00:37:56 -0800

    updates app with firebase app and analytics integration

---
+++ src/analytics.ts
@@ -0,0 +1,23 @@
+import { initializeApp } from 'firebase/app'
+import { getAnalytics } from 'firebase/analytics'
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+// Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+console.log(process.env)
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_ANALYTICS_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+const analytics = getAnalytics(app)
+
+export default analytics
