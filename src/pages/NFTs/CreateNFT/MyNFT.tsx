94149e1b760b778760d2c0e2c164d8359511d439
Luke Woodward <20819255+lukewalt@users.noreply.github.com>
2022-03-21 02:45:53 -0400

    removes `mode` property in `SingleNFT; adds loading states`

--- src/pages/NFTs/CreateNFT/MyNFT.tsx
+++ src/pages/NFTs/CreateNFT/MyNFT.tsx
@@ -15,7 +15,7 @@ interface Props {
 export const MyNFT = ({ visible, setVisible, handleCancel, handleOk, backUrl, type }: Props) => {
   return (
     <>
-      <NFTDetails mode="my-created-NFT" backUrl={backUrl} handleClickPrimaryButton={() => setVisible(true)} />
+      <NFTDetails backUrl={backUrl} handleClickPrimaryButton={() => setVisible(true)} />
       {/* To test Sell your NFT View, please uncomment this line below */}
       {/* <SellYourNFTView visible={visible} handleOk={handleOk} handleCancel={handleCancel} /> */}
       <EditMyNFT type={type} visible={visible} handleOk={handleOk} handleCancel={handleCancel} />
