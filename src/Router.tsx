d44629f1e0a0a7080b05a2c399ae751845e484f4
NAK1998 <arvind98krishnan@gmail.com>
2022-04-15 10:51:23 +0530

    not found page 404

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,6 +4,7 @@ import { AppLayout } from './layouts'
 import { Crypto, Farm, NFTs, Synths, Swap } from './pages'
 import useBlacklisted from './utils/useBlacklisted'
 import { NavCollapseProvider, NFTProfileProvider } from './context'
+import { GenericNotFound } from './pages/InvalidUrl'
 
 export const Router: FC = () => {
   const blacklisted = useBlacklisted()
@@ -12,9 +13,10 @@ export const Router: FC = () => {
   return (
     <BrowserRouter>
       {window.location.pathname === '/' && <Redirect from="/" to="/swap" />}
-      <Switch>
-        <NavCollapseProvider>
-          <AppLayout>
+
+      <NavCollapseProvider>
+        <AppLayout>
+          <Switch>
             <Route exact path="/swap" component={Swap} />
             <Route exact path="/trade" component={Crypto} />
             <Route
@@ -35,9 +37,14 @@ export const Router: FC = () => {
               </NFTProfileProvider>
             </Route>
             <Route exact path="/farm" component={Farm} />
-          </AppLayout>
-        </NavCollapseProvider>
-      </Switch>
+            {window.location.href && !window.location.href.includes('/NFTs/') && (
+              <Route>
+                <GenericNotFound />
+              </Route>
+            )}
+          </Switch>
+        </AppLayout>
+      </NavCollapseProvider>
     </BrowserRouter>
   )
 }
