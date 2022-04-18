b3d106e4629b4914363d4641b5fd4717cceec5dc
Luke Woodward <20819255+lukewalt@users.noreply.github.com>
2022-04-18 11:36:41 -0400

    fixes execution of `DOMAIN` function to return origin url

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,9 +1,5 @@
 import axios from 'axios'
 
-// const DOMAIN = `${
-//   window.location.hostname.includes('staging') || process.env.NODE_ENV !== 'production' ? 'staging.' : ''
-// }goosefx.io`
-
 const DOMAIN = () => {
   const sub = window.location.hostname.includes('staging')
     ? 'staging.'
@@ -16,7 +12,7 @@ const DOMAIN = () => {
 
 const apiClient = (base: string) => {
   return axios.create({
-    baseURL: `https://${base}.${DOMAIN}`,
+    baseURL: `https://${base}.${DOMAIN()}`,
     responseType: 'json',
     headers: {
       'Content-Type': 'application/json'
