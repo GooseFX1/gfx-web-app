8764ce06e646a931966dcbdc54e127ce6f14ac74
NAK1998 <arvind98krishnan@gmail.com>
2022-05-05 16:35:41 +0530

    fix orderbook row click bug

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,8 +1,15 @@
 import React, { FC } from 'react'
 import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom'
 import { AppLayout } from './layouts'
-import { Crypto, Farm, NFTs, Swap } from './pages'
-import { CryptoProvider, NavCollapseProvider, NFTProfileProvider, PriceFeedProvider } from './context'
+import { Farm, NFTs, Swap } from './pages'
+import {
+  CryptoProvider,
+  NavCollapseProvider,
+  NFTProfileProvider,
+  OrderProvider,
+  PriceFeedProvider,
+  TradeHistoryProvider
+} from './context'
 import { GenericNotFound } from './pages/InvalidUrl'
 import { CryptoContent } from './pages/TradeV2/MovableContainer'
 
@@ -17,7 +24,11 @@ export const Router: FC = () => {
             <Route exact path="/swap/:tradePair?" component={Swap} />
             <Route exact path="/trade">
               <PriceFeedProvider>
-                <CryptoContent />
+                <TradeHistoryProvider>
+                  <OrderProvider>
+                    <CryptoContent />
+                  </OrderProvider>
+                </TradeHistoryProvider>
               </PriceFeedProvider>
             </Route>
             <Route path="/NFTs">
