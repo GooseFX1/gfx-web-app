3e779919119a11d515e55cd73039a70cdb50f9c1
Shrihari Kulkarni <shrihari.kulkarni@citrix.com>
2022-06-01 13:48:03 +0530

    can start integrating mint process

--- src/pages/NFTs/launchpad/pages/SingleCollection.tsx
+++ src/pages/NFTs/launchpad/pages/SingleCollection.tsx
@@ -1,8 +1,52 @@
-import React, { FC } from 'react'
+import React, { FC, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import { IProjectParams } from '../../../../types/nft_launchpad'
+import { fetchSelectedNFTLPData } from '../../../../api/NFTLaunchpad'
+import { useNFTLPSelected } from '../../../../context/nft_launchpad'
+import { useWallet } from '@solana/wallet-adapter-react'
+import styled from 'styled-components'
+
+const WRAPPER = styled.div`
+  display: flex;
+  height: 100%;
+  width: 100%;
+  align-items: center;
+  justify-content: space-between;
+  .left {
+    height: 250px;
+  }
+  .button {
+    border: none;
+    background: pink;
+    width: 80px;
+  }
+  .right {
+    height: 250px;
+  }
+`
 
 export const SingleCollection: FC = () => {
   const params = useParams<IProjectParams>()
-  return <div>{params.collectionId}</div>
+  const collectionId = params.collectionId
+  const wallet = useWallet()
+  const { selectedProject, setSelectedProject } = useNFTLPSelected()
+  useEffect(() => {
+    ;(async () => {
+      const data = await fetchSelectedNFTLPData(parseInt(collectionId))
+      setSelectedProject(data.data)
+    })()
+  }, [])
+
+  return (
+    <div>
+      {wallet.publicKey ? 'Wallet connected' : 'Not connected'}
+      <WRAPPER>
+        <div className="left">
+          NFT Project info
+          <button className="button">Mint Button</button>
+        </div>
+        <div className="right">NFT Project Image</div>
+      </WRAPPER>
+    </div>
+  )
 }
