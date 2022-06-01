4f48e05b5cdeda367c5a6d56c3522d7ea6606be6
Shrihari Kulkarni <shrihari.kulkarni@citrix.com>
2022-06-01 13:48:03 +0530

    can start integrating mint process

--- src/api/NFTLaunchpad/actions.ts
+++ src/api/NFTLaunchpad/actions.ts
@@ -1,6 +1,7 @@
 import { customClient } from '../../api'
 import apiClient from '../../api'
 import { NFT_LAUNCHPAD_API_ENDPOINTS, NFT_LAUNCHPAD_API_BASE } from '../NFTLaunchpad/constants'
+var axios = require('axios')
 
 const BASE_URL = 'https://ca45-103-108-117-198.ngrok.io'
 
@@ -12,9 +13,25 @@ export const fetchAllNFTLaunchpadData = async () => {
     return err
   }
 }
-export const fetchSelectedNFTLPData = async () => {
+export const fetchSelectedNFTLPData = async (collectionId: number) => {
   try {
-    const res = await customClient(BASE_URL).post(`${NFT_LAUNCHPAD_API_ENDPOINTS.GET_SELECTED_LAUNCH}`)
+    var data = JSON.stringify({
+      collectionId: 1012
+    })
+    var config = {
+      method: 'post',
+      url: 'https://ca45-103-108-117-198.ngrok.io/nft-launchpad/getOneLaunch',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      data: data
+    }
+
+    // const res = await customClient(BASE_URL, 1012)
+    // .post(`${NFT_LAUNCHPAD_API_ENDPOINTS.GET_SELECTED_LAUNCH}`)
+    // console.log(res);
+    let result = axios(config).then((response) => response.data)
+    return result
   } catch (err) {
     return err
   }
