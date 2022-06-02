8af6848b283d78fe24df1cb5743382d577160a41
John Agbanusi <agbanusijohn@gmail.com>
2022-06-02 15:40:52 +0100

    add extra tokens that are not present by searching for token with token address and it gets saved in localStorage

--- src/pages/Swap/Selector.tsx
+++ src/pages/Swap/Selector.tsx
@@ -1,8 +1,9 @@
 import React, { Dispatch, FC, SetStateAction, useEffect, useState } from 'react'
 import { Input } from 'antd'
 import styled from 'styled-components'
+import { TokenListProvider } from '@solana/spl-token-registry'
 import { ArrowClickerWhite, Modal } from '../../components'
-import { ISwapToken, useTokenRegistry, useDarkMode } from '../../context'
+import { ISwapToken, useTokenRegistry, useDarkMode, useConnectionConfig, useSwap } from '../../context'
 import { CenteredDiv, CenteredImg, SpaceBetweenDiv, SVGToWhite } from '../../styles'
 
 const BODY = styled.div`
@@ -147,17 +148,49 @@ export const Selector: FC<{
 }> = ({ height, otherToken, setToken, token, balance }) => {
   const { mode } = useDarkMode()
   const { tokens } = useTokenRegistry()
+  const { tokenA, tokenB } = useSwap()
+  const { chainId } = useConnectionConfig()
   const [filterKeywords, setFilterKeywords] = useState('')
   const [filteredTokens, setFilteredTokens] = useState(tokens)
   const [visible, setVisible] = useState(false)
 
   useEffect(() => {
     const r = new RegExp(filterKeywords, 'i')
-    const filteredTokens = tokens.filter(
+    const altTokens = JSON.parse(window.localStorage.getItem('myAddedTokenList')) || []
+    const filteredTokensList = tokens.filter(
       ({ address, name, symbol }) =>
         (r.test(name) || r.test(symbol) || r.test(address)) && (!otherToken || otherToken.address !== address)
     )
-    setFilteredTokens(filteredTokens)
+    filteredTokensList.push(...altTokens)
+
+    async function addExternalTokens() {
+      if (filteredTokensList.length < 1 && tokenA && tokenB) {
+        const list = (await new TokenListProvider().resolve()).filterByChainId(chainId).getList()
+        const filteredTokensListAlt = list.filter(({ address }) => filterKeywords === address)
+
+        if (filteredTokensListAlt.length > 0) {
+          filteredTokensList.push(filteredTokensListAlt?.[0])
+          window.localStorage.setItem('myAddedTokenList', JSON.stringify([...altTokens, filteredTokensListAlt?.[0]]))
+        }
+      }
+
+      setFilteredTokens(
+        filteredTokensList.sort((a, b) => {
+          let fa = a.symbol.toLowerCase(),
+            fb = b.symbol.toLowerCase()
+
+          if (fa < fb) {
+            return -1
+          }
+          if (fa > fb) {
+            return 1
+          }
+          return 0
+        })
+      )
+    }
+
+    addExternalTokens()
   }, [filterKeywords, otherToken, tokens])
 
   return (
