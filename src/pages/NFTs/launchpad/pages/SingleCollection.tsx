0b40d4dafd10df82a8a8b7336a49aad2d97a019e
NAK1998 <arvind98krishnan@gmail.com>
2022-06-07 16:13:17 +0530

    implementing mint button functionality

--- src/pages/NFTs/launchpad/pages/SingleCollection.tsx
+++ src/pages/NFTs/launchpad/pages/SingleCollection.tsx
@@ -12,6 +12,7 @@ import { InfoDivLightTheme, InfoDivBrightTheme } from './LaunchpadComponents'
 import { SVGDynamicReverseMode } from '../../../../styles'
 import { SkeletonCommon } from '../../Skeleton/SkeletonCommon'
 import { DETAILS_TAB_CONTENT } from '../../NFTDetails/RightSectionTabs'
+import { MintButton } from '../launchpadComp/MintButton'
 
 export const RIGHT_SECTION_TABS = styled.div<{ activeTab: string }>`
   ${({ theme, activeTab }) => css`
@@ -201,34 +202,6 @@ const WRAPPER = styled.div`
     line-height: 37px;
   }
 `
-const MINT_BUTTON_BAR = styled.div`
-  margin-top: -100px;
-  height: 70px;
-  z-index: 99;
-  position: absolute;
-  border-radius: 0 0 25px 25px;
-  width: 44%;
-  backdrop-filter: blur(23.9091px);
-  background: radial-gradient(
-    81.62% 135.01% at 15.32% 21.04%,
-    rgba(255, 255, 255, 0.05) 0%,
-    rgba(141, 141, 141, 0.05) 68.23%,
-    rgba(255, 255, 255, 0.05) 77.08%,
-    rgba(255, 255, 255, 0.0315) 100%
-  );
-`
-const MINT_BTN = styled.div`
-  background: linear-gradient(96.79deg, #f7931a 4.25%, #ac1cc7 97.61%);
-  border-radius: 47px;
-  width: 260px;
-  height: 50px;
-  margin: auto;
-  display: flex;
-  align-items: center;
-  font-weight: 600;
-  font-size: 15px;
-  justify-content: center;
-`
 
 const NFT_COVER = styled.div`
   .image-border {
@@ -266,8 +239,7 @@ const PRICE_SOCIAL = styled.div`
 export const SingleCollection: FC = () => {
   const params = useParams<IProjectParams>()
   const wallet = useWallet()
-  const { selectedProject, setSelectedProject } = useNFTLPSelected()
-  useEffect(() => console.log(selectedProject), [selectedProject])
+  const { selectedProject } = useNFTLPSelected()
 
   return (
     <div>
@@ -320,9 +292,7 @@ export const SingleCollection: FC = () => {
                 </TabPane>
               </Tabs>
             </RIGHT_SECTION_TABS>
-            <MINT_BUTTON_BAR>
-              <MINT_BTN>Mint</MINT_BTN>
-            </MINT_BUTTON_BAR>
+            <MintButton />
           </>
         </div>
         <div className="rightPart">
