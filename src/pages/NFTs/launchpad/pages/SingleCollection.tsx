0e8ebe7e5e71afd72f2ad31221f8e3a0737d1fe0
Shrihari Kulkarni <shrihari.kulkarni@citrix.com>
2022-06-12 15:46:52 +0530

    fixed png and skeleton issue

--- src/pages/NFTs/launchpad/pages/SingleCollection.tsx
+++ src/pages/NFTs/launchpad/pages/SingleCollection.tsx
@@ -104,62 +104,98 @@ export const SingleCollection: FC = () => {
   const { selectedProject } = useNFTLPSelected()
 
   const isLive = parseInt(selectedProject?.startsOn) < Date.now()
+  const displayProgressBar = isLive ? (
+    <MintProgressBar minted={selectedProject?.itemsMinted} totalNFTs={selectedProject?.items} />
+  ) : (
+    <MintStarts time={selectedProject?.startsOn} />
+  )
+  const ProgressBar = selectedProject?.items ? (
+    displayProgressBar
+  ) : (
+    <SkeletonCommon style={{ marginTop: '20px' }} width="600px" height={'70px'} borderRadius="10px" />
+  )
   return (
     <div>
       <TokenSwitch />
       <WRAPPER>
         <div className="leftPart">
-          <COLLECTION_NAME>{selectedProject?.collectionName}</COLLECTION_NAME>
-          <TAG_LINE>{selectedProject?.tagLine}</TAG_LINE>
-          <PRICE_SOCIAL>
-            <InfoDivLightTheme
-              items={selectedProject?.items}
-              currency={undefined}
-              price={undefined}
-            ></InfoDivLightTheme>
-            <InfoDivLightTheme
-              items={selectedProject}
-              price={selectedProject?.price}
-              currency={selectedProject?.currency}
-            ></InfoDivLightTheme>
-            <Row justify="space-between" align="middle" style={{ marginLeft: '10px' }}>
-              <Col span={2}>
-                <SOCIAL_ICON onClick={(e) => window.open(selectedProject?.website)}>
-                  <SVGDynamicReverseMode src="/img/assets/domains.svg" alt="domain-icon" />
-                </SOCIAL_ICON>
-              </Col>
-              <Col span={2}>
-                <SOCIAL_ICON onClick={(e) => window.open(selectedProject?.discord)}>
-                  <SVGDynamicReverseMode src="/img/assets/discord_small.svg" alt="discord-icon" />
-                </SOCIAL_ICON>
-              </Col>
-              <Col span={2}>
-                <SOCIAL_ICON onClick={(e) => window.open(selectedProject?.twitter)}>
-                  <SVGDynamicReverseMode src="/img/assets/twitter_small.svg" alt="twitter-icon" />
-                </SOCIAL_ICON>
-              </Col>
-            </Row>
-          </PRICE_SOCIAL>
+          {selectedProject?.collectionName ? (
+            <COLLECTION_NAME>{selectedProject?.collectionName}</COLLECTION_NAME>
+          ) : (
+            <COLLECTION_NAME>
+              <SkeletonCommon width="100%" height="100%" borderRadius="10px" />
+            </COLLECTION_NAME>
+          )}
+          {selectedProject?.tagLine ? (
+            <TAG_LINE>{selectedProject?.tagLine}</TAG_LINE>
+          ) : (
+            <TAG_LINE>
+              <SkeletonCommon width="100%" height="100%" borderRadius="10px" />
+            </TAG_LINE>
+          )}
+          {selectedProject?.items ? (
+            <PRICE_SOCIAL>
+              <InfoDivLightTheme
+                items={selectedProject?.items}
+                currency={undefined}
+                price={undefined}
+              ></InfoDivLightTheme>
+              <InfoDivLightTheme
+                items={selectedProject}
+                price={selectedProject?.price}
+                currency={selectedProject?.currency}
+              ></InfoDivLightTheme>
+              <Row justify="space-between" align="middle" style={{ marginLeft: '10px' }}>
+                <Col span={2}>
+                  <SOCIAL_ICON onClick={(e) => window.open(selectedProject?.website)}>
+                    <SVGDynamicReverseMode src="/img/assets/domains.svg" alt="domain-icon" />
+                  </SOCIAL_ICON>
+                </Col>
+                <Col span={2}>
+                  <SOCIAL_ICON onClick={(e) => window.open(selectedProject?.discord)}>
+                    <SVGDynamicReverseMode src="/img/assets/discord_small.svg" alt="discord-icon" />
+                  </SOCIAL_ICON>
+                </Col>
+                <Col span={2}>
+                  <SOCIAL_ICON onClick={(e) => window.open(selectedProject?.twitter)}>
+                    <SVGDynamicReverseMode src="/img/assets/twitter_small.svg" alt="twitter-icon" />
+                  </SOCIAL_ICON>
+                </Col>
+              </Row>
+            </PRICE_SOCIAL>
+          ) : (
+            <PRICE_SOCIAL>
+              <SkeletonCommon width="500px" height="35px" borderRadius="10px" />
+            </PRICE_SOCIAL>
+          )}
           <>
-            <RIGHT_SECTION_TABS activeTab={'4'}>
-              <Tabs>
-                <TabPane tab="Summary" key="1">
-                  <SUMMARY_TAB_CONTENT>
-                    <div>{selectedProject?.summary}</div>
-                  </SUMMARY_TAB_CONTENT>
-                </TabPane>
-                <TabPane tab="Roadmap" key="2">
-                  <RoadMap roadmap={selectedProject?.roadmap} />
-                </TabPane>
-                <TabPane tab="Team" key="3">
-                  <TeamMembers teamMembers={selectedProject?.team} />
-                </TabPane>
-                <TabPane tab="Vesting" key="4">
-                  <Vesting currency={selectedProject?.currency} str={'hllo'} />
-                </TabPane>
-              </Tabs>
-            </RIGHT_SECTION_TABS>
-            <MintButton isLive={isLive} />
+            {selectedProject?.summary ? (
+              <>
+                <RIGHT_SECTION_TABS activeTab={'4'}>
+                  <Tabs>
+                    <TabPane tab="Summary" key="1">
+                      <SUMMARY_TAB_CONTENT>
+                        <div>{selectedProject?.summary}</div>
+                      </SUMMARY_TAB_CONTENT>
+                    </TabPane>
+                    <TabPane tab="Roadmap" key="2">
+                      <RoadMap roadmap={selectedProject?.roadmap} />
+                    </TabPane>
+                    <TabPane tab="Team" key="3">
+                      <TeamMembers teamMembers={selectedProject?.team} />
+                    </TabPane>
+                    <TabPane tab="Vesting" key="4">
+                      <Vesting currency={selectedProject?.currency} str={'hllo'} />
+                    </TabPane>
+                  </Tabs>
+                </RIGHT_SECTION_TABS>
+                <MintButton isLive={isLive} />
+              </>
+            ) : (
+              <>
+                <SkeletonCommon width="700px" height={'450px'} borderRadius="10px" />
+              </>
+            )}
           </>
           <button> Mint</button>
         </div>
@@ -173,11 +209,7 @@ export const SingleCollection: FC = () => {
               <SkeletonCommon width="600px" height="600px" borderRadius="10px" />
             )}
           </NFT_COVER>
-          {isLive ? (
-            <MintProgressBar minted={selectedProject?.itemsMinted} totalNFTs={selectedProject?.items} />
-          ) : (
-            <MintStarts time={selectedProject?.startsOn} />
-          )}
+          {ProgressBar}
         </div>
       </WRAPPER>
     </div>
