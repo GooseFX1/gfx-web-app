bd44da7c7896cc12cd697bccab53ee9e9591dc87
Kiran Babu Muddam <kiran@goosefx.io>
2022-06-13 17:40:43 +0530

    replace facebook profile with telegram

--- src/types/nft_profile.d.ts
+++ src/types/nft_profile.d.ts
@@ -10,7 +10,7 @@ export interface INFTProfile {
   bio: string
   twitter_link: string
   instagram_link: string
-  facebook_link: string
+  telegram_link: string
   youtube_link: string
   profile_pic_link: string
   is_verified: boolean
