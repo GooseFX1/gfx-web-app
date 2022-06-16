a1623fa501b2f494f84733dbca590b520004070c
Luke Woodward <20819255+lukewalt@users.noreply.github.com>
2022-06-16 14:58:12 -0400

    replaces collection id url with collection name url

--- src/pages/NFTs/Home/Header.tsx
+++ src/pages/NFTs/Home/Header.tsx
@@ -228,7 +228,7 @@ export const Header = ({ setFilter, filter, filteredCollections, totalCollection
         {filteredCollections.length > 0 ? (
           filteredCollections.map((i, k) => (
             <Menu.Item key={k}>
-              <URL href={'/NFTs/collection/' + i.collection_id}>
+              <URL href={`/NFTs/collection/${i.collection_name}`}>
                 <DETAILS>
                   <TINYIMG src={i.profile_pic_link.length > 0 ? i.profile_pic_link : `/img/assets/nft-preview.svg`} />
                   <p style={{ margin: '0px' }}>{i.collection_name}</p>
