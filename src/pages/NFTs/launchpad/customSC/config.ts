341564dc8e315f8fccbd9f0ef0693f2acdd35efc
NAK1998 <arvind98krishnan@gmail.com>
2022-07-26 19:29:09 +0530

    mainnet link

--- src/pages/NFTs/launchpad/customSC/config.ts
+++ src/pages/NFTs/launchpad/customSC/config.ts
@@ -4,11 +4,16 @@ export const pdaWhitelistSeed = 'whitelist-config'
 
 export const MAGIC_HAT_PROGRAM_V2_ID = new PublicKey('G317XThznKMemjmSBEekMSsPedT9krKcAeRmE39drhNA')
 export const MAGIC_HAT_CREATOR = new PublicKey('4bDer2d1P357AhB8YN13b1XXBAh89d2xHex9qrM3xEtt')
-export const MAGIC_HAT_ID = new PublicKey('DK5eeZee3iCBcZPGzZu2K77R2MJWKxQnjqG4abWGkeTt')
+export const MAGIC_HAT_ID = new PublicKey('HvVtTCvHwuwz8YkmgindHHuXCkRAmZTR1ey5MG1v6293')
 //export const GOG_TIME = 1656435600;
 //export const WL_TIME = 1656437400;
-export const COMMUNITY_TIME = 1657901400
-export const PUBLIC_TIME = 1657980900
+export const COMMUNITY_TIME = 1658751368
+export const PUBLIC_TIME = 1658751368
+
+export const PUBLIC_5_TIME = 1658862000
+export const TEAM_TIME = 1658844000
+export const OG_TIME = 1658851200
+export const WHITELIST_TIME = 1658854800
 
 export const pdaSeed = 'wallet-whitelist'
 
@@ -22,9 +27,11 @@ export const pdaSeed = 'wallet-whitelist'
 //export const whiteListAddresses = ['36cf3tQCvkw6BujkPpUpwUXRi3yzHumf1xHG46jiiy3m']
 
 export const whiteListAddresses = [
-  'Ci8NApia39iuh2WLiMakBb12P9DnJh6zYCZ49EbbMkcC',
-  '5G988QmdoPPvohLayzbuhQ9iLVaetcDj7uaYyfjWrBnh'
+  '9tPEcwyqwNsrgrJHrqe6tMZYD6cx6fgSv6xA297XQsDD'
+  //'5G988QmdoPPvohLayzbuhQ9iLVaetcDj7uaYyfjWrBnh'
 ]
+
+export const whiteListAddressesFour = ['CqUXdhQHQuJVdch5ovonSmhpB5VYh1G5t7Wh415Pu1Ec']
 
 //export const whiteListAddressesOne = [
 //  '6z2V3CEZPhWLnH86gDcwc2sQ8G1QhfrvZppuDRupfskg',
@@ -32,10 +39,12 @@ export const whiteListAddresses = [
 //]
 
 export const whiteListAddressesOne = [
-  'FshKz382QRXZVxwBwEADtjBNrcKE8hvhXmHXtosZyNnf'
+  '3QiJodq6JbmhmzZ7Rf7LbtsExj2m5eMFndVw4iEPW8q2'
   //'FshKz382QRXZVxwBwEADtjBNrcKE8hvhXmHXtosZyNnf'
   //'CjS4pLEgrr4jZH7YpJLt7jbvTi9CMoEb9XzZ1zAtvTW1'
 ]
+
+export const whiteListAddressesThree = ['3fTcnXKoWMoczbThdo2vxc19VMJiZ6KBDqdE55XKb8Kh']
 
 export const priceWhitelist = 0.01
 
