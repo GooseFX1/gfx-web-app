418beb012deb5d35e017253fc8a0b9bdcf351234
Luke Woodward <20819255+lukewalt@users.noreply.github.com>
2022-08-17 17:14:01 -0400

    optimizes token toggle

--- src/components/TokenToggle.tsx
+++ src/components/TokenToggle.tsx
@@ -1,6 +1,6 @@
-import { FC } from 'react'
+import { useState, FC } from 'react'
 import styled from 'styled-components'
-import { MODE_ICON, SpaceBetweenDiv, TOKENTOGGLE } from '../styles'
+import { MODE_ICON, SpaceBetweenDiv, CenteredDiv } from '../styles'
 
 const WRAPPER = styled(SpaceBetweenDiv)`
   padding: 0.75rem 1.5rem 0.75rem 1.5rem;
@@ -17,18 +17,40 @@ const WRAPPER = styled(SpaceBetweenDiv)`
   }
 `
 
-const Toggle = styled(TOKENTOGGLE)`
+export const Toggle = styled(CenteredDiv)<{ $mode: number }>`
   height: 25px;
   width: 50px;
   border-radius: 40px;
+  margin-right: ${({ theme }) => theme.margin(5)};
+  background: ${({ $mode }) =>
+    $mode === 0
+      ? 'linear-gradient(96.79deg, #F7931A 4.25%, #AC1CC7 97.61%);'
+      : 'linear-gradient(96.79deg, #5855FF 4.25%, #DC1FFF 97.61%);'};
+  &:hover {
+    cursor: pointer;
+  }
+  > div {
+    ${({ theme }) => theme.measurements(theme.margin(2.5))}
+    ${({ theme }) => theme.roundedBorders}
+    background-color: #fff;
+    font-weight: 600;
+    transform: translateX(${({ $mode }) => ($mode === 0 ? '-' : '')}${({ theme }) => theme.margin(1.5)});
+  }
 `
 
-export const TokenToggle: FC<{ token: string; toggleToken: () => void }> = ({ token, toggleToken }) => {
+export const TokenToggle: FC<{ toggleToken: () => void }> = ({ toggleToken }) => {
+  const [position, setPosition] = useState<number>(0)
+
+  const handleToggle = () => {
+    setPosition((prev) => (prev === 0 ? 1 : 0))
+    toggleToken()
+  }
+
   return (
     <WRAPPER>
       <MODE_ICON>SOL</MODE_ICON>
 
-      <Toggle $mode={token} onClick={toggleToken}>
+      <Toggle $mode={position} onClick={handleToggle}>
         <div />
       </Toggle>
 
