4e1f171bdbed474064c328a0f368f4f1e36d2aa7
John Agbanusi <agbanusijohn@gmail.com>
2022-08-23 22:51:31 +0100

    add check for drafts in create page

--- src/pages/NFTs/Collectible/index.tsx
+++ src/pages/NFTs/Collectible/index.tsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useWallet } from '@solana/wallet-adapter-react'
 import { useHistory } from 'react-router-dom'
 import styled from 'styled-components'
-import { FloatingActionButton } from '../../../components'
+import { MainButton, FloatingActionButton } from '../../../components'
 import { Image } from 'antd'
 import { MainText } from '../../../styles'
 import { notify } from '../../../utils'
-import { useDarkMode } from '../../../context'
+import apiClient from '../../../api'
+import { useDarkMode, useNFTProfile } from '../../../context'
+import { NFT_API_BASE, NFT_API_ENDPOINTS } from '../../../api/NFTs'
 
 const UPLOAD_CONTENT = styled.div`
   position: relative;
@@ -99,10 +101,67 @@ const UPLOAD_TEXT = MainText(styled.div`
   margin-top: ${({ theme }) => theme.margin(4)};
 `)
 
+const DRAFT_CHECK = styled.div`
+  margin-top: ${({ theme }) => theme.margin(4)};
+  color: #fff !important;
+  font-family: Montserrat;
+  font-weight: 600;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  div {
+    font-size: 20px;
+  }
+
+  span {
+    font-size: 25px;
+    background-image: linear-gradient(95deg, #f7931a 1%, #ac1cc7 100%);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    -moz-background-clip: text;
+    -moz-text-fill-color: transparent;
+  }
+`
+
+const Button = styled(MainButton)`
+  height: 60px;
+  width: 274px;
+  background: linear-gradient(95deg, #f7931a 1%, #ac1cc7 100%);
+  color: white !important;
+  font-weight: 700;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 0 32px;
+  margin-top: ${({ theme }) => theme.margin(4)};
+  margin-bottom: ${({ theme }) => theme.margin(4)};
+
+  &:disabled {
+    background: linear-gradient(96.79deg, #5855ff 4.25%, #dc1fff 97.61%);
+  }
+`
+
 export const Collectible = (): JSX.Element => {
   const history = useHistory()
   const { connected, publicKey } = useWallet()
   const { mode } = useDarkMode()
+  const { sessionUser } = useNFTProfile()
+  const [draftNum, setDraftsNum] = useState(0)
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    const checkForDrafts = async () => {
+      setLoading(true)
+      const res = await apiClient(NFT_API_BASE).get(`${NFT_API_ENDPOINTS.DRAFTS}?user_id=${sessionUser?.user_id}`)
+      const result = await res.data
+      setDraftsNum(result.length)
+      setLoading(false)
+    }
+
+    checkForDrafts()
+  }, [sessionUser?.user_id])
 
   const handleSelectSingleCollectable = async () => {
     if (connected && publicKey) {
@@ -151,6 +210,24 @@ export const Collectible = (): JSX.Element => {
             <UPLOAD_TEXT>Multiple</UPLOAD_TEXT>
           </UPLOAD_FILED_CONTAINER_RIGHT>
         </UPLOAD_SECTION>
+
+        <DRAFT_CHECK>
+          {draftNum > 0 && (
+            <DESCRIPTION>
+              It seems you currently have <span>{draftNum}</span> NFTs in drafts.
+            </DESCRIPTION>
+          )}
+          <Button
+            status="action"
+            height="60px"
+            width="274px"
+            loading={loading}
+            disabled={draftNum <= 0}
+            onClick={() => history.push('/NFTs/drafts')}
+          >
+            {draftNum > 0 ? 'See my drafts' : 'No draft found!'}
+          </Button>
+        </DRAFT_CHECK>
       </UPLOAD_CONTENT>
     </>
   )
