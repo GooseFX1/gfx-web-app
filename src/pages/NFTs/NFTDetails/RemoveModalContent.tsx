6b01794b46b3e0523596b611f345f3489b51db77
Luke Woodward <20819255+lukewalt@users.noreply.github.com>
2022-09-12 17:23:25 +0100

    updates solscan icon with local reference

--- src/pages/NFTs/NFTDetails/RemoveModalContent.tsx
+++ src/pages/NFTs/NFTDetails/RemoveModalContent.tsx
@@ -61,10 +61,7 @@ const RemoveModalContent = ({ title, caption, removeFunction, pendingTxSig, netw
             <span>
               <img
                 style={{ height: '26px', marginRight: '6px' }}
-                src={`https://www.gitbook.com/cdn-cgi/image/height=40,fit=contain,dpr=2,
-                format=auto/https%3A%2F%2F2775063016-files.gitbook.io%2F~%2Ffiles%2Fv0%2Fb%
-                2Fgitbook-x-prod.appspot.com%2Fo%2Fspaces%252F-M2WGem6IdHOZpBD3zJX%252Flogo%
-                252Fw9pblAvM5UayZbYEg4Cj%252FBlack.png%3Falt%3Dmedia%26token%3D9a925146-c226-4f09-b39b-f61642681016`}
+                src={`/img/assets/solscan.png`}
                 alt="solscan-icon"
               />
             </span>
