51cc7afc1b604939044f9028019b0407f474bcb7
Shrihari Kulkarni <shrihari.kulkarni@citrix.com>
2022-09-16 22:32:30 +0530

    aded revenue table for ssl

--- src/pages/Analytics/components/FinancialsStats.tsx
+++ src/pages/Analytics/components/FinancialsStats.tsx
@@ -1,3 +1,70 @@
-import React from 'react'
+import { PublicKey } from '@solana/web3.js'
+import React, { useEffect, useState } from 'react'
+import { getFarmTokenPrices } from '../../../api/SSL'
+import { useConnectionConfig } from '../../../context'
+import { TOKEN_PROGRAM_ID } from '../../../web3'
+import { revenueCol, TOKEN_PAIRS } from '../constants'
+import { Table } from 'antd'
 
-export const FinancialsStats = () => <div>FinancialsStats</div>
+export interface IAccounts {
+  [mint: string]: IAccount
+}
+export type IAccount = {
+  amount: string
+  decimals: number
+  uiAmount: number
+  uiAmountString: string
+}
+
+export const FinancialsStats = () => <SSLRevenue />
+
+export const SSLRevenue = () => {
+  const [revenueObj, setRevenueObj] = useState<any>()
+  const [tokenPrices, setTokenprices] = useState<any>()
+  const [dataSource, setDataSource] = useState<any>()
+  //const [revenueUsingName, setRevenueUsingName] = useState<any>();
+  const publicKey = new PublicKey('6nRsKbwm7Ezz4frjkdCNjBWyFjzZfW5jsWCaSVMARcsJ')
+  const { connection } = useConnectionConfig()
+  useEffect(() => {
+    ;(async () => {
+      const [parsedAccounts] = await Promise.all([
+        connection.getParsedTokenAccountsByOwner(publicKey, { programId: TOKEN_PROGRAM_ID })
+        //connection.getBalance(publicKey)
+      ])
+      const accounts = parsedAccounts.value.reduce((acc: IAccounts, { account }) => {
+        const { mint, tokenAmount } = account.data.parsed.info
+        acc[mint] = tokenAmount
+        return acc
+      }, {})
+      const { data } = await getFarmTokenPrices({})
+      setTokenprices(data)
+      setRevenueObj(accounts)
+    })()
+  }, [])
+
+  useEffect(() => {
+    getpairInfo(revenueObj)
+  }, [revenueObj])
+
+  const getpairInfo = (revenueObj: any) => {
+    const arr = []
+    for (const mint in revenueObj) {
+      const mintPrice = tokenPrices[TOKEN_PAIRS[mint].pair]['current']
+      const mintRevenue = revenueObj[mint].uiAmount
+      arr.push({
+        token: TOKEN_PAIRS[mint].name,
+        price: '$ ' + mintPrice,
+        native: mintRevenue,
+        dollar: '$ ' + mintRevenue * mintPrice
+      })
+    }
+    setDataSource(arr)
+  }
+
+  return (
+    <div>
+      <h1>SSL Revenue</h1>
+      <Table columns={revenueCol} dataSource={dataSource} />
+    </div>
+  )
+}
--- src/utils/math.ts
+++ src/utils/math.ts
@@ -61,7 +61,7 @@ export const percentFormatter = (number: number) => {
   return number.toFixed(0) + '%'
 }
 function commafy(num) {
-  let str = num.toFixed(2)
+  let str: any = parseFloat(num).toFixed(2)
   str = str.toString().split('.')
   if (str[0].length >= 5) {
     str[0] = str[0].replace(/(\d)(?=(\d{3})+$)/g, '$1,')
