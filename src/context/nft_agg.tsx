a599503ff697ac0a6a81914a07bd168dbffe5d29
Shrihari Kulkarni <shrihari.kulkarni@citrix.com>
2022-11-23 21:13:47 +0530

    Filters context created

---
+++ src/context/nft_agg.tsx
@@ -0,0 +1,77 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { useWallet } from '@solana/wallet-adapter-react'
+import { createContext, ReactNode, useContext, useState, FC, useEffect, SetStateAction } from 'react'
+import { ICreatorData } from '../types/nft_launchpad'
+
+interface INFTAggConfig {
+  sortingAsc: boolean
+  nftCollections: any[]
+  setSortAsc: any
+  currentStep?: number
+  previousStep?: () => void
+  nextStep?: () => void
+  saveDataForStep?: (d: any) => void
+  creatorData?: ICreatorData
+  submit?: () => Promise<boolean>
+}
+
+interface NFTAgg {
+  sortingAsc: boolean
+  nftCollections: any[]
+  setSortAsc: any
+  currentStep?: number
+  saveDataForStep?: (d: any) => void
+  creatorData?: ICreatorData
+  previousStep?: () => void
+  nextStep?: () => void
+  submit?: () => Promise<boolean>
+}
+
+const NFTAggContext = createContext<INFTAggConfig>(null)
+export const NFTAggregatorProvider: FC<{ children: ReactNode }> = ({ children }) => {
+  const [sortingAsc, setSortAsc] = useState<boolean>(true)
+  const [nftCollections, setCollections] = useState<any[]>([])
+
+  useEffect(() => {
+    const collections = []
+    for (let i = 0; i < 45; i++) {
+      collections.push({
+        key: i,
+        name: 'Some random collection',
+        nft_url: 'https://ca.slack-edge.com/T021XPFKRQV-U02R0LLQ8KX-1cd23a0ef132-512',
+        collectionId: i * 5 + 1000,
+        collectionName: 'DeGods',
+        nftPrice: i * 50,
+        currency: i < 10 ? 'USDC' : 'SOL'
+      })
+    }
+    setCollections(collections)
+  }, [])
+
+  useEffect(() => {
+    const sortedData = [...nftCollections]
+    sortedData.sort((a, b) => (sortingAsc ? a.nftPrice - b.nftPrice : b.nftPrice - a.nftPrice))
+    sortedData.length && setCollections(sortedData)
+  }, [sortingAsc])
+  return (
+    <NFTAggContext.Provider
+      value={{
+        sortingAsc: sortingAsc,
+        setSortAsc: setSortAsc,
+        nftCollections: nftCollections
+      }}
+    >
+      {children}
+    </NFTAggContext.Provider>
+  )
+}
+
+export const useNFTAggregator = (): NFTAgg => {
+  const context = useContext(NFTAggContext)
+
+  if (!context) {
+    throw new Error('Missing NFT Aggregator context')
+  }
+  const { sortingAsc, setSortAsc, nftCollections } = context
+  return { sortingAsc, setSortAsc, nftCollections }
+}
