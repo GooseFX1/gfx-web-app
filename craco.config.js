9936edee33f0e809524e9c0d0baa99ecdcf60164
Luke Woodward <20819255+lukewalt@users.noreply.github.com>
2022-11-27 21:46:11 -0500

    moves craco-esbuild and craco-swc to dev deps; adds CracoEsbuildPlugin as option to craco config

--- craco.config.js
+++ craco.config.js
@@ -1,10 +1,32 @@
 const CracoSwcPlugin = require('craco-swc')
 const CracoLessPlugin = require('craco-less')
+// const CracoEsbuildPlugin = require('craco-esbuild')
 const path = require('path')
 const webpack = require('webpack')
 
 module.exports = {
   plugins: [
+    // {
+    //   plugin: CracoEsbuildPlugin,
+    //   options: {
+    //     esbuildLoaderOptions: {
+    //       loader: 'jsx', // Set the value to 'tsx' if you use typescript
+    //       target: 'es5'
+    //     },
+    //     esbuildMinimizerOptions: {
+    //       // Optional. Defaults to:
+    //       target: 'es5',
+    //       css: true // if true, OptimizeCssAssetsWebpackPlugin will also be replaced by esbuild.
+    //     },
+    //     skipEsbuildJest: false, // Optional. Set to true if you want to use babel for jest tests,
+    //     esbuildJestOptions: {
+    //       loaders: {
+    //         '.ts': 'ts',
+    //         '.tsx': 'tsx'
+    //       }
+    //     }
+    //   }
+    // },
     {
       plugin: {
         ...CracoSwcPlugin,
