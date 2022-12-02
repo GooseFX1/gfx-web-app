7ef21c1d783269b6b8b0355eb2794a898a2a3800
Shashank Shekhar <shashank@Shashanks-MacBook-Pro.local>
2022-12-02 17:11:28 +0530

    dex dropdown modal implemented
    (cherry picked from commit 28f7806fa727702e35ad63c53618ba81437e67d5)

--- src/pages/TradeV3/DropdownPairs.tsx
+++ src/pages/TradeV3/DropdownPairs.tsx
@@ -1,9 +1,10 @@
-import { Dropdown, Menu } from 'antd'
-import React, { FC, useMemo } from 'react'
+import { Dropdown } from 'antd'
+import React, { FC, useMemo, useState } from 'react'
 import { useHistory } from 'react-router-dom'
 import { MarketType, useCrypto, usePriceFeed } from '../../context'
 import styled from 'styled-components'
 import { DownOutlined } from '@ant-design/icons'
+import { Modal, SearchBar } from '../../components'
 
 const SELECTED_PAIR = styled.div`
   line-height: 40px;
@@ -31,22 +32,156 @@ const SELECTED_PAIR = styled.div`
 `
 
 const DROPDOWN_PAIR_DIV = styled.div`
-  height: 40px;
+  height: 49px;
   display: flex;
-  justify-content: space-between;
   align-items: center;
-  font-size: 14px;
-  margin-left: -5px;
-  margin-right: -5px;
+  font-size: 18px;
+  font-weight: 600;
+  cursor: pointer;
+  background: #1c1c1c;
   .asset-icon {
     width: 28px;
     height: 28px;
+    margin-right: 18px;
+    margin-left: 10px;
+  }
+  .spacing {
+    margin-right: auto;
   }
 `
+
+const DROPDOWN_MODAL = styled(Modal)`
+  width: 528px !important;
+  height: 528px !important;
+  background-color: ${({ theme }) => theme.bg20} !important;
+  border-radius: 22px;
+
+  .ant-modal-content {
+    height: 100%;
+    overflow: scroll;
+  }
+
+  .dropdown-modal-search {
+    background: red;
+    width: 440px;
+    margin: 0;
+    background-color: ${({ theme }) => theme.bg2} !important;
+
+    > input {
+      background-color: ${({ theme }) => theme.bg2} !important;
+    }
+  }
+
+  .popular {
+    font-weight: 600;
+    font-size: 18px;
+    color: #eeeeee;
+    margin: 10px 0;
+  }
+
+  .row {
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    flex-wrap: wrap;
+  }
+
+  .row:after {
+    height: 1.5px;
+    display: flex;
+    width: 528px;
+    background-color: ${({ theme }) => theme.tokenBorder};
+    content: '';
+    padding: 0;
+    margin: 0;
+  }
+
+  .popular-tokens {
+    border: 1.5px solid ${({ theme }) => theme.tokenBorder};
+    border-radius: 27px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 42px;
+    padding: 0 10px;
+    margin-bottom: 12px;
+    cursor: pointer;
+
+    .asset-icon {
+      height: 30px;
+      width: 30px;
+      margin-right: 10px;
+    }
+
+    .pair {
+      font-weight: 600;
+      font-size: 15px;
+    }
+  }
+`
+
+const GRADIENT_BORDER = styled.div<{ $hoverBorder: boolean }>`
+  background: #1c1c1c;
+  padding: 1px;
+  background: ${({ $hoverBorder }) =>
+    $hoverBorder === true ? 'linear-gradient(96.79deg, #f7931a 4.25%, #ac1cc7 97.61%);' : '#1c1c1c;'};
+`
+
+const MostPopularCrypto: FC<{ pair: string; type: MarketType }> = ({ pair, type }) => {
+  const { getAskSymbolFromPair } = useCrypto()
+
+  const symbol = useMemo(() => getAskSymbolFromPair(pair), [getAskSymbolFromPair, pair])
+  const assetIcon = useMemo(() => `/img/${type}/${type === 'synth' ? `g${symbol}` : symbol}.svg`, [symbol, type])
+  return (
+    <div className="popular-tokens">
+      <img className="asset-icon" src={assetIcon} alt="crypto-icon" />
+      <div className="pair">{pair}</div>
+    </div>
+  )
+}
+
+const SelectCryptoModal: FC = () => {
+  const { selectedCrypto, setSelectedCrypto, pairs, setShowModal, getAskSymbolFromPair } = useCrypto()
+  const symbol = useMemo(
+    () => getAskSymbolFromPair(selectedCrypto.pair),
+    [getAskSymbolFromPair, selectedCrypto.pair]
+  )
+  const history = useHistory()
+
+  const handleSelection = (item) => {
+    if (item.type === 'synth') {
+      setShowModal(false)
+      history.push('/synths')
+    } else if (selectedCrypto.pair !== symbol) {
+      setSelectedCrypto(item)
+      setShowModal(false)
+    }
+  }
+  return (
+    <div>
+      <div className="popular">Most Popular</div>
+      <div className="row">
+        {pairs &&
+          pairs.slice(0, 3).map((item, index) => (
+            <div onClick={() => handleSelection(item)} key={index}>
+              <MostPopularCrypto {...item} />
+            </div>
+          ))}
+      </div>
+      {pairs &&
+        pairs.map((item, index) => (
+          <li onClick={() => handleSelection(item)} key={index}>
+            <PairComponents {...item} />
+          </li>
+        ))}
+    </div>
+  )
+}
 
 const PairComponents: FC<{ pair: string; type: MarketType }> = ({ pair, type }) => {
   const { tokenInfo } = usePriceFeed()
   const { formatPair, getAskSymbolFromPair } = useCrypto()
+  const [hoverBorder, setHoverBorder] = useState<boolean>(false)
 
   const formattedPair = useMemo(() => formatPair(pair), [formatPair, pair])
   const symbol = useMemo(() => getAskSymbolFromPair(pair), [getAskSymbolFromPair, pair])
@@ -58,36 +193,20 @@ const PairComponents: FC<{ pair: string; type: MarketType }> = ({ pair, type }) 
   else if (changeValue && changeValue.substring(0, 1) === '+') classNameChange = 'up24h'
 
   return (
-    <DROPDOWN_PAIR_DIV>
-      <img className="asset-icon" src={assetIcon} alt="" />
-      <div>{formattedPair}</div>
-      {changeValue !== ' ' ? <div className={classNameChange}>{changeValue}%</div> : <div />}
-    </DROPDOWN_PAIR_DIV>
+    <GRADIENT_BORDER $hoverBorder={hoverBorder}>
+      <DROPDOWN_PAIR_DIV onMouseEnter={() => setHoverBorder(true)} onMouseLeave={() => setHoverBorder(false)}>
+        <img className="asset-icon" src={assetIcon} alt="" />
+        <div className="spacing">{formattedPair}</div>
+        {changeValue !== ' ' ? <div className={classNameChange}>{changeValue}%</div> : <div />}
+      </DROPDOWN_PAIR_DIV>
+    </GRADIENT_BORDER>
   )
 }
 
 export const DropdownPairs: FC = () => {
-  const { selectedCrypto, setSelectedCrypto, pairs } = useCrypto()
-  const history = useHistory()
-
-  const handleSelection = (item) => {
-    if (item.type === 'synth') {
-      history.push('/synths')
-    } else if (selectedCrypto.pair !== symbol) {
-      setSelectedCrypto(item)
-    }
-  }
-
-  const menus = (
-    <Menu>
-      {pairs.map((item, index) => (
-        <Menu.Item onClick={() => handleSelection(item)} key={index}>
-          <PairComponents {...item} />
-        </Menu.Item>
-      ))}
-    </Menu>
-  )
-  const { getAskSymbolFromPair, formatPair } = useCrypto()
+  const { selectedCrypto } = useCrypto()
+  const menus = <></>
+  const { getAskSymbolFromPair, formatPair, showModal, setShowModal } = useCrypto()
   const formattedPair = useMemo(() => formatPair(selectedCrypto.pair), [formatPair, selectedCrypto.pair])
   const symbol = useMemo(
     () => getAskSymbolFromPair(selectedCrypto.pair),
@@ -98,18 +217,33 @@ export const DropdownPairs: FC = () => {
     [symbol, selectedCrypto.type]
   )
   return (
-    <Dropdown
-      overlay={menus}
-      trigger={['click']}
-      placement="bottom"
-      align={{ offset: [0, 10] }}
-      overlayClassName="antd-radius-trade-v2"
-    >
-      <SELECTED_PAIR>
-        <img className="asset-icon" src={assetIcon} alt="" />
-        {formattedPair}
-        <DownOutlined />
-      </SELECTED_PAIR>
-    </Dropdown>
+    <>
+      {showModal && (
+        <DROPDOWN_MODAL
+          setVisible={setShowModal}
+          bigTitle={false}
+          visible={true}
+          title={<SearchBar className="dropdown-modal-search" />}
+        >
+          <SelectCryptoModal />
+        </DROPDOWN_MODAL>
+      )}
+      <Dropdown
+        overlay={menus}
+        trigger={['click']}
+        placement="bottom"
+        align={{ offset: [0, 10] }}
+        overlayClassName="antd-radius-trade-v2"
+        onVisibleChange={() => {
+          setShowModal(true)
+        }}
+      >
+        <SELECTED_PAIR>
+          <img className="asset-icon" src={assetIcon} alt="" />
+          {formattedPair}
+          <DownOutlined />
+        </SELECTED_PAIR>
+      </Dropdown>
+    </>
   )
 }
