05a09b62d12068fb8684b045af219faec457f15a
Shashank Shekhar <shashank@Shashanks-MacBook-Pro.local>
2022-12-27 21:12:59 +0530

    blacklisted countries updated

--- src/Router.tsx
+++ src/Router.tsx
@@ -11,75 +11,64 @@ import {
   NFTAdminProvider,
   OrderBookProvider
 } from './context'
-import Restricted from './pages/Restricted'
 import { GenericNotFound } from './pages/InvalidUrl'
 import { CryptoContent } from './pages/TradeV3/TradeContainer'
-import useBlacklisted from './utils/useBlacklisted'
 import { Launchpad } from './pages/NFTs/launchpad/Launchpad'
 import { Creator } from './pages/NFTs/CreatorPage/Creator'
 import { AdminWrapper } from './pages/NFTs/adminPage/components/AdminWrapper'
 import { AnalyticsWrapper } from './pages/Analytics/AnalyticsWrapper'
 import { TraderProvider } from './context/trader_risk_group'
 
-export const Router: FC = () => {
-  const blacklisted = useBlacklisted()
-  console.log('blacklisted', blacklisted, process.env.NODE_ENV)
+export const Router: FC = () => (
+  <BrowserRouter>
+    {window.location.pathname === '/' && <Redirect from="/" to="/swap" />}
 
-  return (
-    <BrowserRouter>
-      {window.location.pathname === '/' && <Redirect from="/" to="/swap" />}
-
-      <NavCollapseProvider>
-        <AppLayout>
-          <Switch>
-            <Route exact path="/swap/:tradePair?">
-              <Swap />
-            </Route>
-            <Route path="/trade">
-              {blacklisted ? (
-                <Restricted />
-              ) : (
-                <PriceFeedProvider>
-                  <TradeHistoryProvider>
-                    <OrderProvider>
-                      <TraderProvider>
-                        <OrderBookProvider>
-                          <CryptoContent />
-                        </OrderBookProvider>
-                      </TraderProvider>
-                    </OrderProvider>
-                  </TradeHistoryProvider>
-                </PriceFeedProvider>
-              )}
-            </Route>
-            <Route path="/NFTs/launchpad">
-              <Launchpad />
-            </Route>
-            <Route path="/NFTs/Creator">
-              <Creator />
-            </Route>
-            <Route path="/NFTs/admin">
-              <NFTAdminProvider>
-                <AdminWrapper />
-              </NFTAdminProvider>
-            </Route>
-            <Route path="/NFTs">
-              <NFTProfileProvider>
-                <NFTs />
-              </NFTProfileProvider>
-            </Route>
-            <Route exact path="/farm">
-              <Farm />
-            </Route>
-            <Route exact path="/analytics">
-              <AnalyticsWrapper />
-            </Route>
-            <Route>
-              <GenericNotFound />
-            </Route>
-          </Switch>
-        </AppLayout>
-      </NavCollapseProvider>
-    </BrowserRouter>
-  )
-}
+    <NavCollapseProvider>
+      <AppLayout>
+        <Switch>
+          <Route exact path="/swap/:tradePair?">
+            <Swap />
+          </Route>
+          <Route path="/trade">
+            <PriceFeedProvider>
+              <TradeHistoryProvider>
+                <OrderProvider>
+                  <TraderProvider>
+                    <OrderBookProvider>
+                      <CryptoContent />
+                    </OrderBookProvider>
+                  </TraderProvider>
+                </OrderProvider>
+              </TradeHistoryProvider>
+            </PriceFeedProvider>
+          </Route>
+          <Route path="/NFTs/launchpad">
+            <Launchpad />
+          </Route>
+          <Route path="/NFTs/Creator">
+            <Creator />
+          </Route>
+          <Route path="/NFTs/admin">
+            <NFTAdminProvider>
+              <AdminWrapper />
+            </NFTAdminProvider>
+          </Route>
+          <Route path="/NFTs">
+            <NFTProfileProvider>
+              <NFTs />
+            </NFTProfileProvider>
+          </Route>
+          <Route exact path="/farm">
+            <Farm />
+          </Route>
+          <Route exact path="/analytics">
+            <AnalyticsWrapper />
+          </Route>
+          <Route>
+            <GenericNotFound />
+          </Route>
+        </Switch>
+      </AppLayout>
+    </NavCollapseProvider>
+  </BrowserRouter>
+)
