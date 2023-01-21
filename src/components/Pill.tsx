0cca3fb69af167a9ddbcf73dd42e558357b1e3b9
Luke Woodward <20819255+lukewalt@users.noreply.github.com>
2023-01-20 21:54:30 -0500

    refactors styles of `Pill` component and adds `loading` parameter

--- src/components/Pill.tsx
+++ src/components/Pill.tsx
@@ -1,48 +1,24 @@
 import React, { FC } from 'react'
-import styled from 'styled-components'
-import tw from 'twin.macro'
+import 'twin.macro'
 import 'styled-components/macro'
 import { SkeletonCommon } from '../pages/NFTs/Skeleton/SkeletonCommon'
 
 interface IProps {
   label: string
   value: null | undefined | string
+  loading: boolean
 }
 
-export const Pill: FC<IProps> = (props: IProps) => {
-  const PILL = styled.div`
-    ${tw`items-center flex rounded-circle p-[2px]`}
-    background: linear-gradient(96deg, #f7931a 1%, #ac1cc7 99%);
-
-    .pill-inner {
-      ${tw`px-[12px] py-[8px] rounded-circle`}
-      background-color: ${({ theme }) => theme.bg1};
-    }
-
-    .pill-label {
-      color: ${({ theme }) => theme.text1};
-      font-size: 15px;
-      opacity: 0.6;
-      line-height: 18px;
-    }
-
-    strong {
-      line-height: 18px;
-      color: ${({ theme }) => theme.text1};
-    }
-  `
-  return (
-    <PILL>
-      <span className={'pill-inner'}>
-        <span className={'pill-label'}>{props.label}</span>{' '}
-        <strong>
-          {props.value == null ? (
-            <SkeletonCommon height={'15px'} width={'42px'} borderRadius={'50px'} />
-          ) : (
-            props.value
-          )}
-        </strong>
-      </span>
-    </PILL>
-  )
-}
+export const Pill: FC<IProps> = (props: IProps) => (
+  <div
+    tw="bg-gradient-to-br from-secondary-gradient-1 to-secondary-gradient-2 
+      items-center flex rounded-circle p-[2px]"
+  >
+    <span tw="dark:bg-black-1 bg-grey-5 px-[12px] py-[8px] rounded-circle">
+      <span tw="text-regular dark:text-grey-2 text-grey-1">{props.label}</span>{' '}
+      <strong tw="dark:text-white text-black">
+        {props.loading ? <SkeletonCommon height={'15px'} width={'42px'} borderRadius={'50px'} /> : props.value}
+      </strong>
+    </span>
+  </div>
+)
