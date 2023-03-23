93e7efa4306fef60894f2cd8575e4e7300fe09b7
Luke Woodward <20819255+lukewalt@users.noreply.github.com>
2023-03-23 15:56:19 -0400

    search bar funcality added

---
+++ src/pages/NFTs/NFTAgg.tsx
@@ -0,0 +1,37 @@
+import React, { ReactElement } from 'react'
+import { Route, Switch, useRouteMatch } from 'react-router-dom'
+import styled from 'styled-components'
+import { useNavCollapse } from '../../context'
+import CollectionV2 from './Collection/CollectionV2'
+import NFTLandingPageV2 from './Home/NFTLandingPageV2'
+
+const BODY_NFT = styled.div<{ $navCollapsed: boolean }>`
+  position: relative;
+  width: 100vw;
+  min-height: calc(100vh - 58px);
+  overflow-y: hidden;
+  overflow-x: hidden;
+  padding-top: calc(80px - ${({ $navCollapsed }) => ($navCollapsed ? '80px' : '0px')});
+  * {
+    font-family: Montserrat;
+  }
+`
+const NFTAgg = (): ReactElement => {
+  const { isCollapsed } = useNavCollapse()
+  const { path } = useRouteMatch()
+
+  return (
+    <BODY_NFT $navCollapsed={isCollapsed}>
+      <Switch>
+        <Route exact path={path}>
+          <NFTLandingPageV2 />
+        </Route>
+        <Route exact path="/NFTAgg/collection/:collectionName">
+          <CollectionV2 />
+        </Route>
+      </Switch>
+    </BODY_NFT>
+  )
+}
+
+export default NFTAgg
