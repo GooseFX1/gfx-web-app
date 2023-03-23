16d9219601e2a1d92f557d533a64b6e2e953d3ce
Luke Woodward <20819255+lukewalt@users.noreply.github.com>
2023-03-23 15:46:34 -0400

    table completed phase 1

--- src/pages/Farm/Columns.tsx
+++ src/pages/Farm/Columns.tsx
@@ -104,7 +104,8 @@ export const HeaderTooltip: FC<{ text: string }> = ({ text }): JSX.Element => {
   )
 }
 
-const Title = (text: string, infoText: string, isArrowDown: boolean, invert?: boolean) => (
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export const Title = (text: string, infoText: string, isArrowDown: boolean, invert?: boolean) => (
   <STYLED_TITLE>
     <div className="textTitle">{text}</div>
     {infoText && <HeaderTooltip text={infoText} />}
