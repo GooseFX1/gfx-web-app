f332c99a524989ffdab71d70ff821e57f1bbd3cf
Luke Woodward <20819255+lukewalt@users.noreply.github.com>
2023-05-09 23:56:09 -0400

    adds searchbar enhancements

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -6,14 +6,13 @@ import { useDarkMode } from '../context'
 import debounce from 'lodash.debounce'
 
 const SEARCH_BAR_WRAPPER = styled(SpaceBetweenDiv)<{ bgColor: string; width: number }>`
-${tw`sm:w-3/4 sm:!h-[45px] rounded-[45px] pb-5 !h-11 rounded-[45px]`}
+${tw`relative sm:w-3/4 sm:!h-[45px] !h-11 `}
   width: 50%;
   max-width: ${({ width }) => (width ? width : '583px')} !important;
 
   margin: 0 0 0 ${({ theme }) => theme.margin(3)};
-  padding: ${({ theme }) => theme.margin(1)} 19px;
-  background: ${({ theme }) => theme.searchbarBackground};
-  background: ${({ bgColor }) => bgColor} !important;
+  background: transparent;
+
   @media(max-width: 500px){
     margin: 0 0 0 10px;
   }
@@ -29,16 +28,19 @@ ${tw`sm:w-3/4 sm:!h-[45px] rounded-[45px] pb-5 !h-11 rounded-[45px]`}
   }
 
   > input {
-    ${tw`sm:w-full text-[15px] duration-500  h-[40px]`}
+    ${tw`sm:w-full text-[15px] duration-500 h-[44px] rounded-circle p-[0 42px 0 18px]`}
     text-align: left;
-    background: ${({ theme }) => theme.searchbarBackground};
-    background: ${({ bgColor }) => bgColor} !important;
+    background: ${({ bgColor, theme }) => (bgColor ? bgColor : theme.searchbarBackground)};
     flex: 1;
+
     ${({ $cssStyle }) => $cssStyle};
     color: ${({ theme }) => theme.text1};
     font-family: 'Montserrat';
-    border: none;
+    border: transparent;
     outline: none;
+    &:focus {
+      border: 1px solid ${({ theme }) => theme.text11};
+    }
     ::placeholder {
       color: ${({ theme }) => theme.text18};
     }
@@ -46,8 +48,8 @@ ${tw`sm:w-3/4 sm:!h-[45px] rounded-[45px] pb-5 !h-11 rounded-[45px]`}
 
 
     .ant-image-img {
+      ${tw`absolute w-[16px] right-4`}
       filter: ${({ theme }) => theme.filterWhiteIcon};
-      width: 16px;
     }
   }
 `
@@ -63,7 +65,7 @@ export const SearchBar: FC<any> = ({ placeholder, setSearchFilter, filter, bgCol
   return (
     <SEARCH_BAR_WRAPPER bgColor={bgColor} width={width} {...rest}>
       <input placeholder={placeholder || 'Search by nft name'} value={filter} onChange={handleInputValue} />
-      <img style={{ height: '20px', width: '20px' }} src={`/img/assets/search_${mode}.svg`} />
+      <img className="ant-image-img" src={`/img/assets/search_${mode}.svg`} />
     </SEARCH_BAR_WRAPPER>
   )
 }
