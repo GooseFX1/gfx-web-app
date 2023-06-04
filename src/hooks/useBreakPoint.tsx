85b6d7f76df1ad58cdbcbfb5a023707740097593
John Daffue <56412825+deadeagle63@users.noreply.github.com>
2023-06-04 22:25:47 +0100

    hooks

--- src/hooks/useBreakPoint.tsx
+++ src/hooks/useBreakPoint.tsx
@@ -1,24 +1,27 @@
 import { useState, useEffect, useCallback } from 'react'
+
 interface IUseBreakPoint {
   isMobile: boolean
   isTablet: boolean
   isDesktop: boolean
   isLaptop: boolean
 }
+
 const MOBILE_BREAKPOINT = 500
 const TABLET_BREAKPOINT = 1024
 const DESKTOP_BREAKPOINT = 1200
+
 function useBreakPoint(): IUseBreakPoint {
-  const [breakpoints, setBreakpoints] = useState({
-    isMobile: false,
-    isTablet: false,
-    isDesktop: false,
-    isLaptop: false
-  })
+  const [breakpoints, setBreakpoints] = useState(() => ({
+    isMobile: window.innerWidth <= MOBILE_BREAKPOINT,
+    isTablet: window.innerWidth > MOBILE_BREAKPOINT && window.innerWidth < TABLET_BREAKPOINT,
+    isLaptop: window.innerWidth >= TABLET_BREAKPOINT && window.innerWidth < DESKTOP_BREAKPOINT,
+    isDesktop: window.innerWidth >= DESKTOP_BREAKPOINT
+  }))
   const handleResize = useCallback(() => {
     setBreakpoints({
-      isMobile: window.innerWidth < MOBILE_BREAKPOINT,
-      isTablet: window.innerWidth >= MOBILE_BREAKPOINT && window.innerWidth < TABLET_BREAKPOINT,
+      isMobile: window.innerWidth <= MOBILE_BREAKPOINT,
+      isTablet: window.innerWidth > MOBILE_BREAKPOINT && window.innerWidth < TABLET_BREAKPOINT,
       isLaptop: window.innerWidth >= TABLET_BREAKPOINT && window.innerWidth < DESKTOP_BREAKPOINT,
       isDesktop: window.innerWidth >= DESKTOP_BREAKPOINT
     })
