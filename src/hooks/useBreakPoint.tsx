3b9d67d14d9b16613ecbfbb723e31f247fd5c0e9
John Daffue <56412825+deadeagle63@users.noreply.github.com>
2023-06-04 22:25:47 +0100

    hooks

--- src/hooks/useBreakPoint.tsx
+++ src/hooks/useBreakPoint.tsx
@@ -1,13 +1,16 @@
 import { useState, useEffect, useCallback } from 'react'
+
 interface IUseBreakPoint {
   isMobile: boolean
   isTablet: boolean
   isDesktop: boolean
   isLaptop: boolean
 }
+
 const MOBILE_BREAKPOINT = 500
 const TABLET_BREAKPOINT = 768
 const DESKTOP_BREAKPOINT = 1200
+
 function useBreakPoint(): IUseBreakPoint {
   const [breakpoints, setBreakpoints] = useState(() => ({
     isMobile: window.innerWidth <= MOBILE_BREAKPOINT,
@@ -17,8 +20,8 @@ function useBreakPoint(): IUseBreakPoint {
   }))
   const handleResize = useCallback(() => {
     setBreakpoints({
-      isMobile: window.innerWidth < MOBILE_BREAKPOINT,
-      isTablet: window.innerWidth >= MOBILE_BREAKPOINT && window.innerWidth < TABLET_BREAKPOINT,
+      isMobile: window.innerWidth <= MOBILE_BREAKPOINT,
+      isTablet: window.innerWidth > MOBILE_BREAKPOINT && window.innerWidth < TABLET_BREAKPOINT,
       isLaptop: window.innerWidth >= TABLET_BREAKPOINT && window.innerWidth < DESKTOP_BREAKPOINT,
       isDesktop: window.innerWidth >= DESKTOP_BREAKPOINT
     })
--- src/hooks/useRewards.tsx
+++ src/hooks/useRewards.tsx
@@ -1,157 +1,437 @@
 import { useConnection, useWallet } from '@solana/wallet-adapter-react'
-import { useCallback, useEffect, useReducer } from 'react'
+import React, { ReactNode, useCallback, useEffect, useReducer, useState } from 'react'
+import { GfxStakeRewards, GOFXVault, StakePool, UnstakeableTicket, UserMetadata } from 'goosefx-stake-rewards-sdk'
+import { useConnectionConfig } from '../context'
+import { Keypair, Transaction, TransactionInstruction } from '@solana/web3.js'
+import { notify } from '../utils'
+import styled from 'styled-components'
+import { Col, Row } from 'antd'
+import moment from 'moment'
+import * as anchor from '@project-serum/anchor'
+import { signAndSendRawTransaction } from '../web3'
+import { Wallet } from '@project-serum/anchor'
 
-export default function useRewards(): IUseRewards {
-  const [earned, dispatch] = useReducer(reducer, initialState)
-  const { wallet, connected } = useWallet()
-  const { connection } = useConnection()
+const MESSAGE = styled.div`
+  margin: -12px 0;
+  font-size: 12px;
+  font-weight: 700;
 
-  useEffect(() => {
-    if (!wallet || !connected) return
-    // TOOD: fetch data from solana
-    connection.getAccountInfo(wallet.adapter.publicKey)
-    //TODO: dispatch data
-    dispatch({ type: 'set_earned_symbol', payload: 'USDC' })
-  }, [wallet, connected])
-  const createAccount = useCallback(() => {
-    //TODO: create account
-  }, [])
-  const closeAccount = useCallback(() => {
-    //TODO: close account
-  }, [])
-  const stakeToken = useCallback((symbol: string, amount: number) => {
-    //TODO: handle staking - generified for future proof
-  }, [])
-  const unstakeToken = useCallback((symbol: string, tokenContract: string) => {
-    //TODO: handle unstaking
-  }, [])
-  const claimFees = useCallback((tokenContracts: string[]) => {
-    //TODO: handle claiming fees
-  }, [])
-  const redeemTickets = useCallback((ticketContracts: string[]) => {
-    //TODO: handle redeeming tickets
-  }, [])
-  const enterGiveaway = useCallback((giveawayContract: string) => {
-    //TODO: handle entering giveaway
-  }, [])
+  .m-title {
+    margin-bottom: 16px;
+  }
+
+  .m-icon {
+    width: 20.5px;
+    height: 20px;
+  }
+`
+const Notification = (title: string, isError: boolean, description: ReactNode) => (
+  <MESSAGE>
+    <Row className="m-title" justify="space-between" align="middle">
+      <Col>{title}</Col>
+      <Col>
+        <img
+          className="m-icon"
+          src={`/img/assets/${isError ? 'error-border-icon' : 'reward-notification-success'}.svg`}
+          alt=""
+        />
+      </Col>
+    </Row>
+    {description}
+    <div>
+      <p>Rewards Program</p>
+    </div>
+  </MESSAGE>
+)
+const fetchAllRewardData = async (stakeRewards: GfxStakeRewards) => {
+  const [userMetadata, stakePool, gofxVault] = await Promise.all([
+    stakeRewards.getUserMetaData(),
+    stakeRewards.getStakePool(),
+    stakeRewards.getGoFxVault()
+  ])
   return {
-    earned,
-    createAccount,
-    closeAccount,
-    stakeToken,
-    unstakeToken,
-    claimFees,
-    redeemTickets,
-    enterGiveaway
+    userMetadata,
+    stakePool,
+    gofxVault
   }
 }
-interface IBaseClaim {
+export default function useRewards(): IUseRewards {
+  const [rewards, dispatch] = useReducer(reducer, initialState)
+  const walletContext = useWallet()
+  const { connection } = useConnection()
+  const { network } = useConnectionConfig()
+  const [stakeRewards, setStakeRewards] = useState<GfxStakeRewards | null>(null)
+
+  useEffect(() => {
+    if (!walletContext.connected || !walletContext.wallet) {
+      return
+    }
+    const rewardNetwork = network == 'mainnet-beta' || network == 'testnet' ? 'MAINNET' : 'DEVNET'
+    // anchor wallet not used
+    const wallet = new Wallet(Keypair.generate())
+    const stakeRewards = new GfxStakeRewards(connection, rewardNetwork, wallet)
+    setStakeRewards(stakeRewards)
+    fetchAllRewardData(stakeRewards).then((data) => {
+      const payload = JSON.parse(JSON.stringify(rewards))
+      payload.user.staking.userMetadata = data.userMetadata
+      payload.stakePool = data.stakePool
+      payload.gofxVault = data.gofxVault
+      dispatch({ type: 'setAll', payload })
+    })
+  }, [walletContext, connection])
+  const checkForUserAccount = useCallback(
+    async (callback: () => Promise<TransactionInstruction>): Promise<Transaction> => {
+      const userMetadata = await stakeRewards.getUserMetaData()
+      const txn = new Transaction()
+      if (!userMetadata) {
+        txn.add(await stakeRewards.initializeUserAccount())
+      }
+      txn.add(await callback())
+      return txn
+    },
+    [stakeRewards]
+  )
+  const initializeUserAccount = useCallback(async () => {
+    const txn: TransactionInstruction = await stakeRewards.initializeUserAccount(null, walletContext.publicKey)
+    signAndSendRawTransaction(stakeRewards.connection, new Transaction().add(txn), walletContext)
+      .then(() =>
+        notify({
+          message: Notification(
+            'Initialize successful!',
+            false,
+            <div>
+              <p>Account initialized</p>
+            </div>
+          ),
+          type: 'success'
+        })
+      )
+      .catch((err) => {
+        console.log('initialize-failed', err)
+        notify({
+          message: Notification(
+            'Initialize failed!',
+            true,
+            <div>
+              <p>{err.msg}</p>
+            </div>
+          ),
+          type: 'error'
+        })
+      })
+  }, [stakeRewards, walletContext])
+  const closeUserAccount = useCallback(async () => {
+    const txn: Transaction = await checkForUserAccount(() =>
+      stakeRewards.closeUserAccount(null, walletContext.publicKey)
+    )
+
+    signAndSendRawTransaction(stakeRewards.connection, txn, walletContext)
+      .then(() =>
+        notify({
+          message: Notification(
+            'Close successful!',
+            false,
+            <div>
+              <p>Account closed</p>
+            </div>
+          ),
+          type: 'success'
+        })
+      )
+      .catch((err) => {
+        console.log('close-failed', err)
+        notify({
+          message: Notification(
+            'Close failed!',
+            true,
+            <div>
+              <p>{err.msg}</p>
+            </div>
+          ),
+          type: 'error'
+        })
+      })
+  }, [stakeRewards])
+  const stake = useCallback(
+    async (amount: number) => {
+      const txn: Transaction = await checkForUserAccount(() => stakeRewards.stake(amount, walletContext.publicKey))
+      signAndSendRawTransaction(stakeRewards.connection, txn, walletContext)
+        .then(() =>
+          notify({
+            message: Notification(
+              'Stake successful!',
+              false,
+              <Row>
+                <Col>Amount:</Col>
+                <Col>{amount.toFixed(2)} GOFX</Col>
+              </Row>
+            ),
+            type: 'success'
+          })
+        )
+        .catch((err) => {
+          console.log('stake-failed', err)
+          notify({
+            message: Notification(
+              'Stake failed!',
+              true,
+              <div>
+                <p>{err.msg}</p>
+              </div>
+            ),
+            type: 'error'
+          })
+        })
+    },
+    [stakeRewards, walletContext]
+  )
+  const unstake = useCallback(
+    async (amount: number) => {
+      const txn: Transaction = await checkForUserAccount(() =>
+        stakeRewards.unstake(amount, walletContext.publicKey)
+      )
+      const proposedEndDate = moment().add(7, 'days').calendar()
+      signAndSendRawTransaction(stakeRewards.connection, txn, walletContext)
+        .then(() =>
+          notify({
+            message: Notification(
+              'Cooldown started!',
+              false,
+              <>
+                <div>Amount: {amount.toFixed(2)} GOFX</div>
+                <div>End Date: {proposedEndDate}</div>
+              </>
+            )
+          })
+        )
+        .catch((err) => {
+          console.log('unstake-failed', err)
+          notify({
+            message: Notification(
+              'Unstake failed!',
+              true,
+              <div>
+                <p>{err.msg}</p>
+              </div>
+            )
+          })
+        })
+    },
+    [stakeRewards, walletContext]
+  )
+  const claimFees = useCallback(async () => {
+    const txn: Transaction = await checkForUserAccount(() => stakeRewards.claimFees(walletContext.publicKey))
+    const amount = getClaimableFees()
+    signAndSendRawTransaction(stakeRewards.connection, txn, walletContext)
+      .then(() =>
+        notify({
+          message: Notification(
+            'Fees claimed!',
+            false,
+            <>
+              <div>Amount: {amount.toFixed(2)} USDC</div>
+            </>
+          )
+        })
+      )
+      .catch((err) => {
+        console.log('unstake-failed', err)
+        notify({
+          message: Notification(
+            'Unstake failed!',
+            true,
+            <div>
+              <p>{err.msg}</p>
+            </div>
+          )
+        })
+      })
+  }, [stakeRewards, walletContext])
+  const redeemUnstakingTickets = useCallback(async () => {
+    const txn: Transaction = await checkForUserAccount(async () => {
+      const unstakingTickets = await stakeRewards.getUnstakingTickets(walletContext.publicKey)
+      const unstakeableTickets = stakeRewards.getUnstakeableTickets(unstakingTickets)
+      return stakeRewards.resolveUnstakingTicket(
+        unstakeableTickets.map((t) => t.index),
+        walletContext.publicKey
+      )
+    })
+
+    signAndSendRawTransaction(stakeRewards.connection, txn, walletContext)
+      .then(() =>
+        notify({
+          message: Notification(
+            'Unstake successful!',
+            false,
+            <div>
+              <p>Amount: {rewards.earned.claimable.toFixed(2)} GOFX</p>
+            </div>
+          ),
+          type: 'success'
+        })
+      )
+      .catch((err) => {
+        console.log('unstake-failed', err)
+        notify({
+          message: Notification(
+            'Unstake failed!',
+            true,
+            <div>
+              <p>{err.msg}</p>
+            </div>
+          ),
+          type: 'error'
+        })
+      })
+  }, [stakeRewards, walletContext])
+  const enterGiveaway = useCallback(
+    (giveawayContract: string) => {
+      //TODO: handle entering giveaway
+      console.log('enter-giveaway', giveawayContract)
+    },
+    [stakeRewards]
+  )
+  const getClaimableFees = useCallback((): number => {
+    if (!stakeRewards) return 0
+
+    return (
+      (rewards.stakePool.totalAccumulatedProfit.toNumber() -
+        rewards.user.staking.userMetadata.lastObservedTap.toNumber()) *
+      (rewards.user.staking.userMetadata.totalStaked.toNumber() / rewards.gofxVault.amount.toNumber())
+    )
+  }, [stakeRewards])
+
+  return {
+    rewards,
+    initializeUserAccount,
+    closeUserAccount,
+    stake,
+    unstake,
+    claimFees,
+    redeemUnstakingTickets,
+    enterGiveaway,
+    getClaimableFees
+  }
+}
+
+interface BaseClaim {
   totalClaimed: number
   claimable: number
 }
 
-interface IEarned extends IBaseClaim {
-  symbol: string
-  cooldown: number
-  lastClaimed: number
-  totalStaked: number
+interface Rewards extends BaseClaim {
+  userMetadata: UserMetadata
 }
 
-interface IReferred extends IBaseClaim {
+interface Referred extends BaseClaim {
   symbol: string
   referrals: string[]
 }
 
-interface IGiveaway extends IBaseClaim {
+interface Giveaway extends BaseClaim {
   // TODO: fill this in / should it extend base?
   someStateHere?: string
 }
 
 interface RewardState {
-  earned: IEarned
-  referred: IReferred
-  giveaway: IGiveaway
+  user: {
+    staking: Rewards
+    referred: Referred
+    giveaway: Giveaway
+  }
+  stakePool: StakePool
+  gofxVault: GOFXVault
 }
 
 const initialState: RewardState = {
   // TODO: state
-  earned: {
-    symbol: '',
-    cooldown: 0,
-    lastClaimed: 0,
-    totalStaked: 0,
-    totalClaimed: 0,
-    claimable: 0
+  user: {
+    staking: {
+      userMetadata: {
+        accountOpenedAt: new anchor.BN(0.0),
+        totalStaked: new anchor.BN(0.0),
+        lastObservedTap: new anchor.BN(0.0),
+        lastClaimed: new anchor.BN(0.0),
+        totalEarned: new anchor.BN(0.0),
+        unstakingTickets: []
+      },
+      totalClaimed: 0,
+      claimable: 0
+    },
+    referred: {
+      symbol: '',
+      referrals: [],
+      totalClaimed: 0,
+      claimable: 0
+    },
+    giveaway: {
+      totalClaimed: 0,
+      claimable: 0
+    }
   },
-  referred: {
-    symbol: '',
-    referrals: [],
-    totalClaimed: 0,
-    claimable: 0
+  stakePool: {
+    totalAccumulatedProfit: new anchor.BN(0.0),
+    protocolActivatedAt: new anchor.BN(0.0)
   },
-  giveaway: {
-    totalClaimed: 0,
-    claimable: 0
-  }
+  gofxVault: {}
 }
 
 function reducer(state: RewardState, action) {
   switch (action.type) {
     // Initial Cases - allows for higher order setting
-    case 'set_all':
+    case 'setAll':
       return action.payload
-    case 'set_earned':
-      state.earned = action.payload
+    case 'setStaking':
+      state.user.staking = action.payload
       return state
-    case 'set_referred':
-      state.referred = action.payload
+    case 'setReferred':
+      state.user.referred = action.payload
       return state
-    case 'set_giveaway':
-      state.giveaway = action.payload
+    case 'setGiveaway':
+      state.user.giveaway = action.payload
+      return state
+    case 'setStakePool':
+      state.stakePool = action.payload
+      return state
+    case 'setGoFxVault':
+      state.gofxVault = action.payload
       return state
     // Individual Cases - allows for finer grain setting
-    case 'set_earned_symbol':
-      state.earned.symbol = action.payload
+    case 'setUserMetadata':
+      state.user.staking.userMetadata = action.payload
       return state
-    case 'set_earned_cooldown':
-      state.earned.cooldown = action.payload
+    case 'setStakingTotalClaimed':
+      state.user.staking.totalClaimed = action.payload
       return state
-    case 'set_earned_last_claimed':
-      state.earned.lastClaimed = action.payload
+    case 'setStakingClaimable':
+      state.user.staking.claimable = action.payload
       return state
-    case 'set_earned_total_staked':
-      state.earned.totalStaked = action.payload
+    case 'setReferralSymbol':
+      state.user.referred.symbol = action.payload
       return state
-    case 'set_earned_total_claimed':
-      state.earned.totalClaimed = action.payload
+    case 'setReferralReferrals':
+      state.user.referred.referrals = action.payload
       return state
-    case 'set_earned_claimable':
-      state.earned.claimable = action.payload
+    case 'setReferredTotalClaimed':
+      state.user.referred.totalClaimed = action.payload
       return state
-    case 'set_referred_symbol':
-      state.referred.symbol = action.payload
-      return state
-    case 'set_referred_referrals':
-      state.referred.referrals = action.payload
-      return state
-    case 'set_referred_total_claimed':
-      state.referred.totalClaimed = action.payload
-      return state
-    case 'set_referred_claimable':
-      state.referred.claimable = action.payload
+    case 'setReferredClaimable':
+      state.user.referred.claimable = action.payload
       return state
     //TODO: add giveaway unique -  cases
     default:
       console.warn('unknown action--->', action)
   }
 }
+
 interface IUseRewards {
-  earned: IEarned
-  createAccount: () => void
-  closeAccount: () => void
-  stakeToken: (symbol: string, amount: number) => void
-  unstakeToken: (symbol: string, tokenContract: string) => void
-  claimFees: (tokenContracts: string[]) => void
-  redeemTickets: (ticketContracts: string[]) => void
+  rewards: RewardState
+  initializeUserAccount: () => Promise<void>
+  closeUserAccount: () => Promise<void>
+  stake: (amount: number) => Promise<void>
+  unstake: (amount: number) => Promise<void>
+  claimFees: () => Promise<void>
+  redeemUnstakingTickets: (ticketContracts: UnstakeableTicket[]) => Promise<void>
   enterGiveaway: (giveawayContract: string) => void
+  getClaimableFees: () => number
 }
