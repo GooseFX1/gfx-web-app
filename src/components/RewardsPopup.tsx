8ed70c3fec999fc8f27a53b5a353c330511f65c0
Luke Woodward <20819255+lukewalt@users.noreply.github.com>
2023-06-28 12:25:53 -0400

    adds Zone.Identifier to gitignore

--- src/components/RewardsPopup.tsx
+++ src/components/RewardsPopup.tsx
@@ -1,13 +1,14 @@
-import React, { FC, useCallback, useMemo } from 'react'
+import React, { FC, useCallback, useEffect, useState } from 'react'
 import styled from 'styled-components'
-import { RewardInfoComponent, RewardRedirectComponent } from './RewardDetails'
-import { useDarkMode, useRewardToggle } from '../context'
+import { useRewardToggle } from '../context/reward_toggle'
+import { PanelSelector, RewardInfoComponent, RewardRedirectComponent } from './RewardDetails'
 import tw from 'twin.macro'
 import 'styled-components/macro'
+import { useRive, useStateMachineInput } from '@rive-app/react-canvas'
 import useBreakPoint from '../hooks/useBreakPoint'
-// import { useRewardToggle } from '../context/reward_toggle'
-// import useRiveAnimations, { RiveAnimationWrapper } from '../hooks/useRiveAnimations'
-// import useRiveThemeToggle from '../hooks/useRiveThemeToggle'
+import { RIVE_ANIMATION } from '../constants'
+import { useDarkMode } from '../context'
+import useRewards from '../hooks/useRewards'
 
 const REWARD_INFO = styled.div`
   ${tw`w-full min-w-[65%]  rounded-bigger`}
@@ -30,23 +31,43 @@ const Wrapper = styled.div`
   font-family: Montserrat !important;
   background-color: ${({ theme }) => theme.bg9};
 `
-// const REWARDS_BTN = styled.button`
-//   ${tw`w-[111px] h-9 text-xs !font-semibold rounded-circle cursor-pointer text-white
-//   border-none border-0 sm:h-[70px] sm:w-full sm:text-regular  sm:rounded-none sm:rounded-t-bigger sm:p-4 sm:mb-32`}
-//   background-image: linear-gradient(90deg, #8ade75 0%, #4b831d 100%);
-//   line-height: inherit;
-// `
+const REWARDS_BTN = styled.button`
+  ${tw`w-[111px] h-9 text-xs font-semibold rounded-circle cursor-pointer text-white
+  border-none border-0 sm:h-[70px] sm:w-full sm:text-regular  sm:rounded-none sm:rounded-t-bigger sm:p-4 sm:mb-32
+  flex flex-row justify-center items-center
+  `}
+  background-image: linear-gradient(90deg, #4FCF95 0%, #18495D 100%);
+  line-height: inherit;
+`
 // const REWARDS_WITH_IMG = styled.img`
-//   ${tw`h-4 w-4 ml-2`}
+//   ${tw`ml-2`}
 // `
 
-// const REWARD_BTN_TITLE = styled.span`
-//   ${tw`text-smallest font-semibold sm:text-sm`}
-//
-//   @media (max-width: 500px) {
-//     line-height: inherit;
-//   }
-// `
+const REWARD_BTN_TITLE = styled.span`
+  ${tw`text-smallest font-semibold sm:text-sm`};
+
+  @media (max-width: 500px) {
+    line-height: inherit;
+  }
+`
+const RiveRewardsAnimation = () => {
+  const { mode } = useDarkMode()
+  const { rewards } = useRewards()
+  const { rive, RiveComponent } = useRive({
+    src: RIVE_ANIMATION['rewards'].src,
+    autoplay: true,
+    stateMachines: Object.keys(RIVE_ANIMATION['rewards'].stateMachines)
+  })
+  const animationColor = useStateMachineInput(
+    rive,
+    RIVE_ANIMATION['rewards'].stateMachines.Rewards.stateMachineName,
+    RIVE_ANIMATION['rewards'].stateMachines.Rewards.inputs.theme
+  )
+
+  useEffect(() => {
+    if (!animationColor) return
+    animationColor.value = mode == 'dark'
+  }, [mode, animationColor])
 
   return (
     <div css={tw`relative h-[30px] w-[30px]`}>
@@ -64,61 +85,22 @@ const Wrapper = styled.div`
   )
 }
 export const RewardsButton: FC = () => {
-  const { mode } = useDarkMode()
   const { rewardToggle } = useRewardToggle()
-  const breakpoint = useBreakPoint()
-  // const rewardsAnimation = useRiveAnimations({
-  //   animation: 'rewards',
-  //   autoplay: true,
-  //   canvasWidth: breakpoint.isMobile || breakpoint.isTablet ? 31 : 20,
-  //   canvasHeight: breakpoint.isMobile || breakpoint.isTablet ? 35 : 22.32
-  // })
-  // useRiveThemeToggle(rewardsAnimation.rive, 'rewards', 'Rewards')
+  const breakpoints = useBreakPoint()
 
-  const hasRewards = false // TODO: hook into useRewards hook
+  const handleModalClick = useCallback(() => {
+    rewardToggle(true)
+  }, [])
 
-  // const breakpointWidth = breakpoint.isMobile || breakpoint.isTablet ? 31 : 20
-  // const breakpointHeight = breakpoint.isMobile || breakpoint.isTablet ? 35 : 22.32
-  const riveComponent = useMemo(
-    () => (
-      <div css={[tw`relative`]}>
-        {/* <RiveAnimationWrapper
-          setContainerRef={rewardsAnimation.setContainerRef}
-          width={breakpointWidth}
-          height={breakpointHeight}
-        >
-          <rewardsAnimation.RiveComponent />
-        </RiveAnimationWrapper> */}
-
-        <img css={breakpoint.isMobile ? [tw`h-[30px]`] : [tw`h-[24px]`]} src={`img/mainnav/rewards-${mode}.svg`} />
-
-        {hasRewards && (
-          <img
-            css={tw`absolute top-[5px] min-md:top-[1px] right-0`}
-            src={'img/assets/red-notification-circle.svg'}
-          />
-        )}
-      </div>
-    ),
-    [mode, breakpoint, hasRewards]
-  )
-  const handleClick = useCallback(() => rewardToggle(true), [])
-  if (breakpoint.isMobile || breakpoint.isTablet) {
-    return <div css={[tw`cursor-pointer`]}>{riveComponent}</div>
-  }
-  return (
-    <div
-      css={[
-        tw`h-7.5 w-27.5 border-1 border-solid border-grey-1 dark:border-white rounded-full
-       bg-grey-5 dark:bg-black-1 pl-1 pr-2.25 py-0.5 flex flex-row items-center gap-1.75 cursor-pointer
-       text-tiny font-semibold text-black-4 dark:text-white
-       `
-      ]}
-      onClick={handleClick}
-    >
-      {riveComponent}
-      <span>Rewards</span>
-    </div>
+  return breakpoints.isMobile ? (
+    <button onClick={handleModalClick}>
+      <RiveRewardsAnimation />
+    </button>
+  ) : (
+    <REWARDS_BTN onClick={handleModalClick}>
+      <REWARD_BTN_TITLE>Rewards</REWARD_BTN_TITLE>
+      <RiveRewardsAnimation />
+    </REWARDS_BTN>
   )
 }
 
