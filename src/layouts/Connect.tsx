9d1643277ee8cb43459b8ec92da74c222284afc2
John Daffue <56412825+deadeagle63@users.noreply.github.com>
2023-08-01 22:24:11 +0100

    fixed connect + loader

--- src/layouts/Connect.tsx
+++ src/layouts/Connect.tsx
@@ -155,7 +155,7 @@ export const Connect: FC<MenuItemProps> = ({
       return 'Connect Wallet'
     } else if (!base58PublicKey) {
       return (
-        <div css={[tw`absolute top-[-5px]`]}>
+        <div css={[tw`absolute`]}>
           <Loader zIndex={1} />
         </div>
       )
