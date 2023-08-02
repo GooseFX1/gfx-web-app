cce217e8554168f8b0b60e66c4680ae71251979f
Kiran <kiran@goosefx.io>
2023-08-02 17:20:08 +0530

    add pnft sell/delist instructions implementation

--- src/web3/auction-house-sdk/cancelListing.ts
+++ src/web3/auction-house-sdk/cancelListing.ts
@@ -1,5 +1,12 @@
 import { Metaplex, Pda, SplTokenCurrency, toPublicKey, walletAdapterIdentity } from '@metaplex-foundation/js'
-import { Connection, PublicKey, SYSVAR_INSTRUCTIONS_PUBKEY, TransactionInstruction } from '@solana/web3.js'
+import {
+  AccountMeta,
+  ComputeBudgetProgram,
+  Connection,
+  PublicKey,
+  SYSVAR_INSTRUCTIONS_PUBKEY,
+  TransactionInstruction
+} from '@solana/web3.js'
 import { AUCTION_HOUSE } from '../ids'
 import { WalletContextState } from '@solana/wallet-adapter-react'
 import { INFTAsk } from '../../types/nft_details'
@@ -8,11 +15,26 @@ import {
   createCancelInstruction,
   createCancelListingReceiptInstruction
 } from '@metaplex-foundation/mpl-auction-house'
+import { derivePNFTAccounts } from './list'
 
 export const currency: SplTokenCurrency = {
   symbol: 'SOL',
   decimals: 9,
   namespace: 'spl-token'
+}
+
+interface ICancelAnchorRemainingAccounts {
+  metadataProgram: AccountMeta
+  delegateRecord: AccountMeta
+  programAsSigner: AccountMeta
+  metadataAccount: AccountMeta
+  edition: AccountMeta
+  tokenRecord: AccountMeta
+  tokenMint: AccountMeta
+  authRulesProgram: AccountMeta
+  authRules: AccountMeta
+  sysvarInstructions: AccountMeta
+  systemProgram: AccountMeta
 }
 
 export const cancelListing = async (
@@ -28,12 +50,38 @@ export const cancelListing = async (
       address: new PublicKey(AUCTION_HOUSE)
     })
 
+    const programAsSigner = metaplex.auctionHouse().pdas().programAsSigner()
+
+    const pnftAccounts = await derivePNFTAccounts(
+      connection,
+      wallet.publicKey,
+      programAsSigner,
+      new PublicKey(ask.token_account_mint_key)
+    )
+
+    const cancelAnchorRemainingAccounts: ICancelAnchorRemainingAccounts = {
+      metadataProgram: pnftAccounts.metadataProgram,
+      delegateRecord: pnftAccounts.delegateRecord,
+      programAsSigner: pnftAccounts.programAsSigner,
+      metadataAccount: pnftAccounts.metadataAccount,
+      edition: pnftAccounts.edition,
+      tokenRecord: pnftAccounts.tokenRecord,
+      tokenMint: pnftAccounts.tokenMint,
+      authRulesProgram: pnftAccounts.authRulesProgram,
+      authRules: pnftAccounts.authRules,
+      sysvarInstructions: pnftAccounts.sysvarInstructions,
+      systemProgram: pnftAccounts.systemProgram
+    }
+
+    const additionalComputeBudgetInstruction = ComputeBudgetProgram.setComputeUnitLimit({ units: 400000 })
+
     const listing = await auctionHouseClient.findListingByTradeState({
       auctionHouse,
       tradeStateAddress: toPublicKey(ask?.seller_trade_state)
     })
 
     const { asset, sellerAddress, receiptAddress, tradeStateAddress, price, tokens } = listing
+
     const { address, authorityAddress, feeAccountAddress } = auctionHouse
     const accounts: CancelInstructionAccounts = {
       wallet: sellerAddress,
@@ -42,14 +90,17 @@ export const cancelListing = async (
       authority: authorityAddress,
       auctionHouse: address,
       auctionHouseFeeAccount: feeAccountAddress,
-      tradeState: tradeStateAddress
+      tradeState: tradeStateAddress,
+      anchorRemainingAccounts: Object.values(cancelAnchorRemainingAccounts)
     }
 
     const args = {
       buyerPrice: price.basisPoints,
       tokenSize: tokens.basisPoints
     }
+
     const instructions: TransactionInstruction[] = []
+    instructions.push(additionalComputeBudgetInstruction)
     instructions.push(createCancelInstruction(accounts, args))
 
     /* eslint-disable no-extra-boolean-cast */
