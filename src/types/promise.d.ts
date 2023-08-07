8a16b8072cfd4f85f40aa52c05757f0ae6c52c93
Luke Woodward <20819255+lukewalt@users.noreply.github.com>
2023-08-07 19:21:09 -0400

    cleans up nft files

---
+++ src/types/promise.d.ts
@@ -0,0 +1,11 @@
+export interface PromiseFulfilledResult<T> {
+  status: 'fulfilled'
+  value: T
+}
+
+export interface PromiseRejectedResult {
+  status: 'rejected'
+  reason: any
+}
+
+export type PromiseSettledResult<T> = PromiseFulfilledResult<T> | PromiseRejectedResult
