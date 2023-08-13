5d1f3ece2716c764f889fdffaa9b8d9bc355d93b
Shashank Shekhar <shashank@Shashanks-MacBook-Pro.local>
2023-08-13 10:31:42 +0530

    ui integration sslv2 - initial commit

--- src/pages/FarmV3/Farm.tsx
+++ src/pages/FarmV3/Farm.tsx
@@ -6,6 +6,7 @@ import { usePriceFeedFarm } from '../../context'
 import 'styled-components/macro'
 import { FarmTable } from './FarmTable'
 import { Faqs } from './Faqs'
+import { FarmProvider } from '../../context'
 
 const WRAPPER = styled.div`
   ${tw`p-5 dark:bg-black-1 bg-grey-5 sm:pt-3.75 sm:pb-3.75 sm:pl-2.5 sm:pr-0`}
@@ -25,10 +26,12 @@ export const Farm: FC = () => {
   }, [])
 
   return (
-    <WRAPPER>
-      <FarmHeader setPoolIndex={setPoolIndex} />
-      <FarmTable poolIndex={poolIndex} setPoolIndex={setPoolIndex} />
-      <Faqs />
-    </WRAPPER>
+    <FarmProvider>
+      <WRAPPER>
+        <FarmHeader setPoolIndex={setPoolIndex} />
+        <FarmTable poolIndex={poolIndex} setPoolIndex={setPoolIndex} />
+        <Faqs />
+      </WRAPPER>
+    </FarmProvider>
   )
 }
