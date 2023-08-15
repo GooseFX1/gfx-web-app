95be0a267dc8d476f59bd48099d852ab444d588c
Clish254 <clishilla@gmail.com>
2023-08-15 21:46:46 +0300

    fix: hide scrollbar in the trade page container

--- src/pages/TradeV3/TradeContainer.tsx
+++ src/pages/TradeV3/TradeContainer.tsx
@@ -199,6 +199,15 @@ const DEX_CONTAINER = styled.div<{ $isLocked: boolean; $mode: string }>`
   #tv_chart_container {
     border: 1px solid ${({ theme }) => theme.tokenBorder};
   }
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
+  /* Hide scrollbar for IE, Edge and Firefox */
+  & {
+    -ms-overflow-style: none; /* IE and Edge */
+    scrollbar-width: none; /* Firefox */
+  }
 `
 
 const UNLOCKED_OVERLAY = styled.div<{ $isGeoBlocked?: boolean }>`
