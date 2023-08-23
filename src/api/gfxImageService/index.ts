5e933980e22386064abf92d13b19aad01a13fd8c
Shrihari <shrihari@Macs-MacBook-Pro.local>
2023-08-23 21:09:52 +0530

    quick fix for taiyo robotics collections

--- src/api/gfxImageService/index.ts
+++ src/api/gfxImageService/index.ts
@@ -2,16 +2,16 @@ export * from './constants'
 
 const gfxImageService = (size: string, collectionUUID: string, remoteURL: undefined | string): string => {
   const substringsToCheck = ['gfx-nest-image-resources', 'ipfs', '.gif', '.webp']
-  const containsAny = substringsToCheck.some((substring) => remoteURL.includes(substring))
+  const containsAny = substringsToCheck.some((substring) => remoteURL && remoteURL.includes(substring))
   if (remoteURL && containsAny) return remoteURL
-
+  if (!remoteURL) {
+    return '/img/assets/Aggregator/Unknown.svg'
+  }
   const arr = remoteURL.split('/')
   const file = arr[arr.length - 1]
   const formatedURL = arr.slice(2).join('/')
 
-  if (remoteURL === undefined) {
-    return '/img/assets/Aggregator/Unknown.svg'
-  } else if (file.includes('.')) {
+  if (file.includes('.')) {
     return `https://image-service.goosefx.io/${size}/${collectionUUID}/${formatedURL}`
   } else {
     return remoteURL
