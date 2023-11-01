964d2eb0178c3f5283fb8ff1403637727ca8f1cd
John Daffue <deadeagle63@users.noreply.github.com>
2023-11-01 01:51:49 +0000

    fixed chunking | memory limits

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import wasm from 'vite-plugin-wasm'
 import topLevelAwait from 'vite-plugin-top-level-await'
 import requireTransform from 'vite-plugin-require-transform'
+import { cpus } from 'os'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -26,6 +27,8 @@ export default defineConfig(({ mode }) => ({
     },
     sourcemap: mode === 'development',
     output: {
+      cache: false,
+      maxParallelFileOps: Math.max(1, cpus().length - 1),
       manualChunks: (id) => {
         if (id.includes('node_modules')) return 'vendor'
       }
