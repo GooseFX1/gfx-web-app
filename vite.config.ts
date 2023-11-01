d7e5166c00b4cedea06c2fde20d522caf283e833
John Daffue <deadeagle63@users.noreply.github.com>
2023-11-01 01:19:07 +0000

    build: fixed build|adjusted vite config to allow for commonJS transformations|adjusted tsconfig.{node}.json to align with vite config|added cross-env to ensure memory limit is okay due to rollUp

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,11 @@ import macrosPlugin from 'vite-plugin-babel-macros'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import wasm from 'vite-plugin-wasm'
 import topLevelAwait from 'vite-plugin-top-level-await'
+import requireTransform from 'vite-plugin-require-transform'
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/',
-  plugins: [react(), macrosPlugin(), nodePolyfills(), wasm(), topLevelAwait()],
+export default defineConfig(() => ({
+  plugins: [react(), macrosPlugin(), nodePolyfills(), wasm(), topLevelAwait(), requireTransform({})],
   css: {
     preprocessorOptions: {
       less: {
@@ -15,8 +16,13 @@ export default defineConfig({
       }
     }
   },
-  define: {
-    'process.env': {},
-    global: 'window'
+  server: {
+    port: 3000
+  },
+  build: {
+    outDir: 'build',
+    commonjsOptions: {
+      transformMixedEsModules: true
+    }
   }
-})
+}))
