6ad1a49445ce7d76f028e15fbe49ad597768501a
John Daffue <deadeagle63@users.noreply.github.com>
2023-11-02 15:58:27 +0000

    build optimizations test

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,24 @@
-import { defineConfig } from 'vite'
+import { defineConfig, splitVendorChunkPlugin } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import macrosPlugin from 'vite-plugin-babel-macros'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import wasm from 'vite-plugin-wasm'
 import topLevelAwait from 'vite-plugin-top-level-await'
 import requireTransform from 'vite-plugin-require-transform'
-import * as path from 'path'
 
+import * as path from 'path'
+import { cpus } from 'os'
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  plugins: [react(), macrosPlugin(), nodePolyfills(), wasm(), topLevelAwait(), requireTransform({})],
+  plugins: [
+    react(),
+    macrosPlugin(),
+    nodePolyfills(),
+    wasm(),
+    topLevelAwait(),
+    requireTransform({}),
+    splitVendorChunkPlugin()
+  ],
   css: {
     preprocessorOptions: {
       less: {
@@ -20,22 +29,36 @@ export default defineConfig(({ mode }) => ({
   server: {
     port: 3000
   },
+  esbuild: {
+    drop: mode == 'development' ? [] : ['console', 'debugger'],
+    pure: mode === 'production' ? ['console.log'] : []
+  },
   build: {
     outDir: 'build',
     commonjsOptions: {
       transformMixedEsModules: true
     },
+    minify: 'esbuild',
+    cssMinify: 'esbuild',
     sourcemap: mode === 'development',
     output: {
       cache: false,
       sourcemap: mode === 'development',
-      maxParallelFileOps: 1,
+      preserveModule: true,
+      maxParallelFileOps: Math.max(1, cpus().length - 1),
       manualChunks: (id) => {
         if (id.includes('node_modules')) return 'vendor'
+        if (id.includes('openbook-package')) return 'openbook'
+        if (id.includes('perps-wasm') || id.includes('wasm')) return 'wasm'
       },
       sourcemapIgnoreList: (relativeSourcePath) => {
         const normalizedPath = path.normalize(relativeSourcePath)
-        return normalizedPath.includes('node_modules')
+        return (
+          normalizedPath.includes('node_modules') ||
+          normalizedPath.includes('openbook-package') ||
+          normalizedPath.includes('perps-wasm') ||
+          normalizedPath.includes('wasm')
+        )
       }
     }
   }
