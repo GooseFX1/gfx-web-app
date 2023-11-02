f8fa50655e10334cab32e099ab9ce20aa2f4cdea
Shashank729 <shashank@goosefx.io>
2023-11-03 03:47:33 +0530

    bonk cap to 250k

--- src/pages/FarmV3/constants.tsx
+++ src/pages/FarmV3/constants.tsx
@@ -113,7 +113,7 @@ export const ADDRESSES: {
       token: 'BONK',
       name: 'BONK',
       address: new PublicKey('DezXAZ8z7PnrnRJjz3wXBoRgixCa6xjnB7YaB1pPB263'),
-      cappedDeposit: 200000
+      cappedDeposit: 250000
     }
   ],
   devnet: [
@@ -145,7 +145,7 @@ export const ADDRESSES: {
       token: 'BONK',
       name: 'BONK',
       address: new PublicKey('DezXAZ8z7PnrnRJjz3wXBoRgixCa6xjnB7YaB1pPB263'),
-      cappedDeposit: 200000
+      cappedDeposit: 250000
     }
   ],
   testnet: [
