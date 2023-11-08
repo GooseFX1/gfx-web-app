0bc938458e9e8915e7d9a6a1342f17eb2d0303db
Shashank729 <shashank@goosefx.io>
2023-11-08 15:44:22 +0530

    solves errors and ui/ux fixes for JITOSOL pool

--- src/context/rewardsContext.tsx
+++ src/context/rewardsContext.tsx
@@ -245,7 +245,10 @@ export const RewardsProvider: FC<{ children: ReactNode }> = ({ children }) => {
     updateStakeDetails().catch((err) => {
       console.warn('fetch-all-reward-data-failed', err)
     })
-    const interval = setInterval(updateStakeDetails, 10 * 1e3)
+    const interval = setInterval(
+      () => updateStakeDetails().catch((e) => console.warn('interval-fetch-all-reward-data-failed', e)),
+      10 * 1e3
+    )
     return () => clearInterval(interval)
   }, [walletContext.publicKey, updateStakeDetails])
   const checkForUserAccount = useCallback(
