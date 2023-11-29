9f4f03463f9fddbad24ac5903c2cdbbeaea7bb39
John Daffue <deadeagle63@users.noreply.github.com>
2023-11-29 23:22:41 +0000

    added missing reddot

--- src/components/rewards/v2/leftPanel/UnstakeBottomBar.tsx
+++ src/components/rewards/v2/leftPanel/UnstakeBottomBar.tsx
@@ -7,20 +7,27 @@ import Button from '../../../twComponents/Button'
 
 function RewardsUnstakeBottomBar(): JSX.Element {
   const [isModalOpen, setIsModalOpen] = useBoolean(false)
-  const { activeUnstakingTickets } = useRewards()
+  const { activeUnstakingTickets, unstakeableTickets } = useRewards()
   return (
     <div css={[tw`mt-3 min-md:mt-0 h-[38.5px] max-w-[580px]`]}>
       <AllUnstakingTicketsModal isOpen={isModalOpen} onClose={setIsModalOpen.off} />
       <Button
         css={[
-          tw`text-grey-2 dark:text-grey-1 underline
+          tw`text-grey-2 dark:text-grey-1 underline items-baseline
   `,
           activeUnstakingTickets.length > 0 && tw`text-blue-1 dark:text-white`
         ]}
         disabled={activeUnstakingTickets.length == 0}
         onClick={setIsModalOpen.on}
       >
-        {activeUnstakingTickets.length == 0 ? 'No Active Cooldowns' : 'See All Active Cooldowns'}
+        {activeUnstakingTickets.length == 0 ? (
+          'No Active Cooldowns'
+        ) : (
+          <>
+            <span>See All Active Cooldowns</span>
+            {unstakeableTickets.length > 0 && <span css={[tw`rounded-full w-2.5 h-2.5 bg-red-2`]} />}
+          </>
+        )}
       </Button>
     </div>
   )
