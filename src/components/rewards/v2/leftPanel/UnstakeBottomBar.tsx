7c942682905fdcf5327ee7a6d6c8d2ce2be95f24
John Daffue <deadeagle63@users.noreply.github.com>
2023-11-29 22:18:05 +0000

    fixed unstake bottom bar styling

--- src/components/rewards/v2/leftPanel/UnstakeBottomBar.tsx
+++ src/components/rewards/v2/leftPanel/UnstakeBottomBar.tsx
@@ -9,13 +9,13 @@ function RewardsUnstakeBottomBar(): JSX.Element {
   const [isModalOpen, setIsModalOpen] = useBoolean(false)
   const { activeUnstakingTickets } = useRewards()
   return (
-    <div css={[tw`mt-3 min-md:mt-0 h-[69px] max-w-[580px]`]}>
+    <div css={[tw`mt-3 min-md:mt-0 h-[38.5px] max-w-[580px]`]}>
       <AllUnstakingTicketsModal isOpen={isModalOpen} onClose={setIsModalOpen.off} />
       <Button
         css={[
           tw`text-grey-2 dark:text-grey-1 underline
   `,
-          activeUnstakingTickets.length == 0 && 'text-blue-1 dark:text-grey-5'
+          activeUnstakingTickets.length > 0 && tw`text-blue-1 dark:text-white`
         ]}
         disabled={activeUnstakingTickets.length == 0}
         onClick={setIsModalOpen.on}
