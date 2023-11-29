b4a696ae55f67cbb13e0bccf74c4085092f59641
John Daffue <deadeagle63@users.noreply.github.com>
2023-11-29 21:24:12 +0000

    boolean | timer fixes

--- src/hooks/useBoolean.tsx
+++ src/hooks/useBoolean.tsx
@@ -1,11 +1,14 @@
 import { useCallback, useState } from 'react'
 
-function useBoolean(initialValue = false): [boolean, { toggle: () => void; on: () => void; off: () => void }] {
+function useBoolean(
+  initialValue = false
+): [boolean, { toggle: () => void; on: () => void; off: () => void; set: (value: boolean) => void }] {
   const [val, setVal] = useState(initialValue)
   const toggle = useCallback(() => setVal((prev) => !prev), [])
   const on = useCallback(() => setVal(true), [])
   const off = useCallback(() => setVal(false), [])
-  return [val, { toggle, on, off }]
+  const set = useCallback((value: boolean) => setVal(value), [])
+  return [val, { toggle, on, off, set }]
 }
 
 export default useBoolean
