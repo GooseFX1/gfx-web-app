91f9f1efccaded5a9a5dd20ba192671c780d3d4c
John Daffue <deadeagle63@users.noreply.github.com>
2023-11-30 23:27:55 +0000

    adjusted gap + logo + fonts

--- src/pages/FarmV3/Faqs.tsx
+++ src/pages/FarmV3/Faqs.tsx
@@ -44,12 +44,12 @@ const FAQ_WRAPPER = styled.div`
 
 export const Faqs: FC = () => (
   <FAQ_WRAPPER>
-    <div tw="flex flex-row mt-10">
+    <div tw="flex flex-row my-10">
       <h2 tw="mr-auto text-[20px] font-semibold h-[35px] dark:text-grey-5 text-black-4 sm:text-lg pl-2">FAQs</h2>
       <a
         tw="w-[140px] h-[35px] bg-blue-1 cursor-pointer text-white font-bold
          text-regular flex flex-row justify-center items-center rounded-circle sm:w-1/3 sm:text-tiny"
-        href="https://docs.goosefx.io/features/farm/single-sided-liquidity-pools"
+        href="https://docs.goosefx.io/features/farm"
         target="_blank"
         rel="noreferrer"
       >
