23d9c90ede153b3e242d6ccfa8f3f03bb7e69f43
Luke Woodward <20819255+lukewalt@users.noreply.github.com>
2023-12-07 22:33:10 -0500

    updates wallet connect config

--- src/utils/wallets.ts
+++ src/utils/wallets.ts
@@ -34,7 +34,14 @@ export const getWalletAdapters = (network: WalletAdapterNetwork): WalletAdapter[
   new WalletConnectWalletAdapter({
     network: network == WalletAdapterNetwork.Testnet ? WalletAdapterNetwork.Devnet : network,
     options: {
-      projectId: process.env.REACT_APP_WALLETCONNECT_ID
+      relayUrl: 'wss://relay.walletconnect.com',
+      projectId: 'f294cea1e9cd00f0e185354688de6620',
+      metadata: {
+        name: 'GooseFX',
+        description: 'Solana Defi',
+        url: 'https://goosefx.io',
+        icons: ['https://media.goosefx.io/logos/Web_Clip.svg']
+      }
     }
   }),
   new LedgerWalletAdapter(),
