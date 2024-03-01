de3b242298f9f44e9d46353787d9ab6535059e14
Shashank729 <shashank@goosefx.io>
2024-03-01 14:44:15 +0530

    logs removed on withdraw flow + bonk deposit cap fix

--- src/web3/sslV2.ts
+++ src/web3/sslV2.ts
@@ -197,8 +197,6 @@ export const executeWithdraw = async (
   const amountInNative = convertToNativeValue(amount, token?.mintDecimals)
   const userAta = await findAssociatedTokenAddress(walletPublicKey, tokenMintAddress)
 
-  console.log('withdrawing', walletPublicKey.toBase58(), amountInNative)
-
   const withdrawTX: Transaction = new Transaction()
 
   const ataAddress = await getAssociatedTokenAddress(tokenMintAddress, walletPublicKey)
@@ -228,8 +226,8 @@ export const executeWithdraw = async (
   }
   let signature
   try {
-    signature = await wallet.sendTransaction(withdrawTX, connection, { skipPreflight: true })
-    console.log('signature of withdraw txn: ', signature)
+    signature = await wallet.sendTransaction(withdrawTX, connection)
+    console.log('signature', signature)
     const confirm = await confirmTransaction(connection, signature, 'processed')
     return { confirm, signature }
   } catch (error) {
