35f9d9a2697765f392ac98da0f03830d2e15e4c6
John Daffue <daffuejhn@gmail.com>
2024-03-13 19:25:02 +0000

    last fixes

--- src/components/rewards/v2/rightPanel/RewardsClaimButton.tsx
+++ src/components/rewards/v2/rightPanel/RewardsClaimButton.tsx
@@ -29,7 +29,7 @@ function RewardsClaimButton(): JSX.Element {
       disabled={buttonDisabled || isClaiming}
       onClick={handleClaim}
       variant={'outline'}
-      className={cn('text-white', claimable > 0 && `opacity-100`)}
+      className={cn('text-white bg-background-darkmode-primary', claimable > 0 && `opacity-100`)}
     >
       {isClaiming ? (
         <Loader zIndex={2} color={'blue-1'} />
--- src/components/rewards/v2/rightPanel/RewardsRightSidePanel.tsx
+++ src/components/rewards/v2/rightPanel/RewardsRightSidePanel.tsx
@@ -1,12 +1,12 @@
 import useRewards from '../../../../context/rewardsContext'
 import { useDarkMode } from '../../../../context'
 import { numberFormatter } from '../../../../utils'
-import tw from 'twin.macro'
 import { Tooltip } from '../../../Tooltip'
 import RewardsClaimButton from './RewardsClaimButton'
 import React from 'react'
 import Skeleton from 'react-loading-skeleton'
 import RewardsRightLayout from '../../layout/RewardsRightLayout'
+import { cn } from 'gfx-component-lib'
 
 export default function RewardsRightSidePanel({ apy }: { apy: number }): JSX.Element {
   const { totalEarned, totalStaked, gofxValue, totalStakedGlobally, userStakeRatio } = useRewards()
@@ -17,37 +17,42 @@ export default function RewardsRightSidePanel({ apy }: { apy: number }): JSX.Ele
 
   return (
     <RewardsRightLayout className={`bg-gradient-to-br from-green-gradient-3 to-green-gradient-4`}>
-      <h1 css={[tw`text-h2 min-md:text-h1 font-semibold !mb-0`]}>
+      <h1 className={`text-h2 min-md:text-h1 font-semibold !mb-0 text-white`}>
         {apy == 0 ? (
           <Skeleton highlightColor={'#37BB7D'} height={'15px'} width={'60px'} borderRadius={'1rem'} />
         ) : (
           `${apy}% APY`
         )}
       </h1>
-      <div css={[tw`flex flex-col gap-1.25 flex-1 max-w-[300px] w-full justify-center`]}>
-        <div css={[tw`flex flex-col gap-2 items-center`]}>
+      <div className={`flex flex-col gap-1.25 flex-1 max-w-[300px] w-full justify-center`}>
+        <div className={`flex flex-col gap-2 items-center`}>
           <p
-            css={[
-              tw`mb-0 text-45 min-sm:text-3xl text-white opacity-60 leading-[1] font-sans`,
-              totalEarned > 0 && tw`opacity-100`
-            ]}
+            className={cn(
+              `mb-0 text-45 min-sm:text-3xl text-white opacity-60 leading-[1] font-sans`,
+              totalEarned > 0 && `opacity-100`
+            )}
           >
             {totalEarnedString == '0.00' ? (totalEarned > 0 ? '< 0.00' : '0.00') : totalEarnedString}
           </p>
-          <p css={[tw`mb-0 text-regular min-sm:text-average text-white font-semibold`]}>Past USDC Earnings</p>
+          <p className={cn(`mb-0 text-regular min-sm:text-average text-white font-semibold`)}>
+            Past USDC Earnings
+          </p>
         </div>
-        <div css={[tw`flex flex-col items-center `]}>
-          <p css={[tw`mb-0 text-regular min-sm:text-average text-white flex items-center gap-1.25 font-semibold`]}>
+        <div className={cn(`flex flex-col items-center `)}>
+          <p
+            className={cn(`mb-0 text-regular min-sm:text-average text-white flex items-center gap-1.25
+          font-semibold`)}
+          >
             Total Staked&nbsp;
             <Tooltip
               title={
-                <div css={[tw`flex flex-col my-auto [&>p]:mb-0 [&>p]:text-tiny [&>p]:font-semibold `]}>
-                  <p css={[mode == 'dark' ? tw`text-black-4` : tw`text-grey-5`]}>Global Staked GOFX</p>
-                  <p css={[mode == 'dark' ? tw`text-black-4` : tw`text-grey-5`]}>
+                <div className={cn(`flex flex-col my-auto [&>p]:mb-0 [&>p]:text-tiny [&>p]:font-semibold `)}>
+                  <p className={cn(mode == 'dark' ? `text-black-4` : `text-grey-5`)}>Global Staked GOFX</p>
+                  <p className={cn(mode == 'dark' ? `text-black-4` : `text-grey-5`)}>
                     ≈ {numberFormatter(totalStakedGlobally, 2)}
                   </p>
-                  <p css={[mode == 'dark' ? tw`text-black-4` : tw`text-grey-5`]}>My Stake Ratio</p>
-                  <p css={[mode == 'dark' ? tw`text-black-4` : tw`text-grey-5`]}>
+                  <p className={cn(mode == 'dark' ? `text-black-4` : `text-grey-5`)}>My Stake Ratio</p>
+                  <p className={cn(mode == 'dark' ? `text-black-4` : `text-grey-5`)}>
                     ≈ {stakeRatio == '0.00' ? '<0.01' : stakeRatio}%
                   </p>
                 </div>
@@ -65,17 +70,19 @@ export default function RewardsRightSidePanel({ apy }: { apy: number }): JSX.Ele
           </p>
 
           <p
-            css={[
-              tw`mb-0 text-regular min-sm:text-average opacity-60 text-white font-semibold`,
-              totalStaked > 0 && tw`opacity-100`
-            ]}
+            className={cn(
+              `mb-0 text-regular min-sm:text-average opacity-60 text-white font-semibold`,
+              totalStaked > 0 && `opacity-100`
+            )}
           >
             {numberFormatter(totalStaked)} GOFX (≈ ${numberFormatter(gofxValue * totalStaked)})
           </p>
         </div>
         <RewardsClaimButton />
       </div>
-      <h4 css={[tw`mb-0 text-center text-white font-semibold text-tiny min-md:text-regular font-semibold !mb-0 `]}>
+      <h4
+        className={`mb-0 text-center text-white font-semibold text-tiny min-md:text-regular font-semibold !mb-0 `}
+      >
         During cooldown no rewards will be earned
       </h4>
     </RewardsRightLayout>
