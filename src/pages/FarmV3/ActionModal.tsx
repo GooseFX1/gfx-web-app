6ee1e7a4131d008d418ca96f1331a1f1dad9dc3e
John Daffue <daffuejhn@gmail.com>
2024-04-05 23:11:51 +0100

    fix: action modal

--- src/pages/FarmV3/ActionModal.tsx
+++ src/pages/FarmV3/ActionModal.tsx
@@ -1,33 +1,29 @@
 import { Dispatch, FC, SetStateAction, useCallback, useEffect, useMemo } from 'react'
-import tw, { styled } from 'twin.macro'
-import 'styled-components/macro'
-import { Button, PopupCustom } from '../../components'
 import { SSLToken } from './constants'
-import { checkMobile, truncateBigNumber } from '../../utils'
-import { Drawer } from 'antd'
+import { truncateBigNumber } from '../../utils'
 import useBreakPoint from '../../hooks/useBreakPoint'
 import { useDarkMode } from '../../context'
-import { Tooltip } from 'antd'
+import { Button, cn, Dialog, DialogBody, DialogContent, DialogOverlay, IconTooltip } from 'gfx-component-lib'
 
 //milliseconds in 5 minutes to be used to update the countdown every 5 minutes
 const TIMER = 300 * 1000
-
-const STYLED_POPUP = styled(PopupCustom)`
-  .ant-modal-content {
-    ${tw`h-full dark:bg-black-2 bg-white rounded-bigger`}
-  }
-  .ant-modal-close-x {
-    > img {
-      ${tw`sm:!h-4 sm:!w-4 absolute bottom-2 opacity-60`}
-    }
-  }
-  .ant-modal-body {
-    ${tw`p-5 sm:p-[15px]`}
-  }
-  .tooltipIcon {
-    ${tw`h-4 w-4 max-w-none ml-0`}
-  }
-`
+//
+// const STYLED_POPUP = styled(PopupCustom)`
+//   .ant-modal-content {
+//     ${tw`h-full dark:bg-black-2 bg-white rounded-bigger`}
+//   }
+//   .ant-modal-close-x {
+//     > img {
+//       ${tw`sm:!h-4 sm:!w-4 absolute bottom-2 opacity-60`}
+//     }
+//   }
+//   .ant-modal-body {
+//     ${tw`p-5 sm:p-[15px]`}
+//   }
+//   .tooltipIcon {
+//     ${tw`h-4 w-4 max-w-none ml-0`}
+//   }
+// `
 
 export const ActionModal: FC<{
   actionModal: boolean
@@ -64,7 +60,6 @@ export const ActionModal: FC<{
 }) => {
   const { mode } = useDarkMode()
   const breakpoint = useBreakPoint()
-  const elem = document.getElementById('farm-container')
 
   const handleUserAction = () => {
     console.log('handle user action', actionType)
@@ -94,19 +89,18 @@ export const ActionModal: FC<{
 
   const Content = useCallback(
     () => (
-      <div tw="h-full flex flex-col justify-between">
+      <div className="w-full h-full flex flex-col justify-between">
         <div>
-          {breakpoint.isMobile && (
-            <div
-              tw="absolute right-[25px] text-[25px] cursor-pointer"
-              onClick={() => {
-                setActionModal(false)
-              }}
-            >
-              <img key={`close-mobile-button`} src={`/img/mainnav/close-thin-${mode}.svg`} alt="close-icon" />
-            </div>
-          )}
-          <div tw="dark:text-grey-8 text-black-4 text-lg font-semibold mb-3.75 sm:text-average">
+          <div
+            className="absolute right-[25px] text-[25px] cursor-pointer"
+            onClick={() => {
+              setActionModal(false)
+            }}
+          >
+            <img key={`close-mobile-button`} src={`/img/mainnav/close-thin-${mode}.svg`} alt="close-icon" />
+          </div>
+
+          <div className="dark:text-grey-8 text-black-4 text-lg font-semibold mb-3.75 sm:text-average">
             {actionType === 'withdraw' ? (
               <h3>Withdraw</h3>
             ) : actionType === 'deposit' ? (
@@ -115,7 +109,7 @@ export const ActionModal: FC<{
               <h3>Claim</h3>
             )}
           </div>
-          <div tw="dark:text-grey-2 text-grey-1 text-regular font-semibold mb-3 sm:text-tiny">
+          <div className="dark:text-grey-2 text-grey-1 text-regular font-semibold mb-3 sm:text-tiny">
             <h5>
               {' '}
               By
@@ -128,11 +122,11 @@ export const ActionModal: FC<{
             </h5>
           </div>
           {actionType !== 'claim' && (
-            <div tw="flex flex-row items-center justify-between mb-3.75">
-              <div tw="dark:text-grey-2 text-grey-1 text-regular font-semibold">
+            <div className="flex flex-row items-center justify-between mb-3.75">
+              <div className="dark:text-grey-2 text-grey-1 text-regular font-semibold">
                 {<div>{actionType === 'deposit' ? 'Deposit' : 'Withdraw'} Amount</div>}
               </div>
-              <div tw="dark:text-grey-5 text-black-4 text-regular font-semibold">{`${
+              <div className="dark:text-grey-5 text-black-4 text-regular font-semibold">{`${
                 actionType === 'deposit'
                   ? depositAmount
                     ? truncateBigNumber(+depositAmount)
@@ -143,38 +137,28 @@ export const ActionModal: FC<{
               } ${token?.token}`}</div>
             </div>
           )}
-          <div tw="flex flex-row items-center justify-between mb-3.75">
-            <div tw="dark:text-grey-2 text-grey-1 text-regular font-semibold">Claimable yield</div>
-            <div tw="dark:text-grey-5 text-black-4 text-regular font-semibold">
+          <div className="flex flex-row items-center justify-between mb-3.75">
+            <div className="dark:text-grey-2 text-grey-1 text-regular font-semibold">Claimable yield</div>
+            <div className="dark:text-grey-5 text-black-4 text-regular font-semibold">
               {`${claimAmount ? `${truncateBigNumber(claimAmount)} ${token?.token}` : `00.00 ${token?.token}`}`}
             </div>
           </div>
           {actionType === 'withdraw' && earlyWithdrawFee > 0 && (
-            <div tw="flex flex-row items-center justify-between mb-3.75">
-              <div tw="flex flex-row">
-                <div tw="dark:text-grey-2 text-grey-1 text-regular font-semibold">Early Withdraw Fee</div>
-                <Tooltip
-                  color={mode === 'dark' ? '#FFF' : '#1C1C1C'}
-                  title={
-                    <span tw="dark:text-black-4 text-grey-8 font-semibold text-tiny">
-                      The early withdrawal penalty fee is to prevent manipulation of our pools by LPs. Please wait{' '}
-                      {getTimerCountdown} to avoid paying the fee.
-                    </span>
-                  }
-                  placement={checkMobile() ? 'topRight' : 'rightBottom'}
-                  overlayClassName={mode === 'dark' ? 'farm-tooltip dark' : 'farm-tooltip'}
-                  overlayInnerStyle={{ borderRadius: '8px' }}
+            <div className="flex flex-row items-center justify-between mb-3.75">
+              <div className="flex flex-row">
+                <div className="dark:text-grey-2 text-grey-1 text-regular font-semibold">Early Withdraw Fee</div>
+                <IconTooltip
+                  tooltipType={'outline'}
+                  tooltipClassName={'h-4 w-4 max-w-none ml-1'}
+                  // color={mode === 'dark' ? '#FFF' : '#1C1C1C'}
                 >
-                  <img
-                    src={mode === 'dark' ? '/img/assets/tooltip_holo.svg' : '/img/assets/tooltip_blue.svg'}
-                    alt="deposit-cap"
-                    tw="ml-2.5 sm:ml-1.25 max-w-none cursor-pointer"
-                    height={20}
-                    width={20}
-                  />
-                </Tooltip>
+                  <span className="dark:text-black-4 text-grey-8 font-semibold text-tiny">
+                    The early withdrawal penalty fee is to prevent manipulation of our pools by LPs. Please wait{' '}
+                    {getTimerCountdown} to avoid paying the fee.
+                  </span>
+                </IconTooltip>
               </div>
-              <div tw="dark:text-grey-5 text-black-4 text-regular font-semibold">
+              <div className="dark:text-grey-5 text-black-4 text-regular font-semibold">
                 {earlyWithdrawFee ? truncateBigNumber(earlyWithdrawFee) + ' ' + token?.token : 0}
               </div>
             </div>
@@ -182,9 +166,9 @@ export const ActionModal: FC<{
         </div>
         <div>
           <Button
-            height="35px"
-            cssStyle={tw`duration-500 w-full !h-8.75 bg-blue-1 text-regular border-none mx-auto mb-3.75
-                      !text-white font-semibold rounded-[50px] flex items-center justify-center outline-none`}
+            fullWidth
+            colorScheme={'blue'}
+            className={`duration-500`}
             onClick={handleUserAction}
             loading={isButtonLoading}
             disabled={
@@ -192,7 +176,6 @@ export const ActionModal: FC<{
               (actionType === 'deposit' && !depositAmount) ||
               (actionType === 'withdraw' && !withdrawAmount)
             }
-            disabledColor={tw`dark:bg-black-1 bg-grey-5 !text-grey-1 opacity-70`}
           >
             {`${
               actionType === 'deposit'
@@ -202,23 +185,25 @@ export const ActionModal: FC<{
                 : `${claimAmount ? `${truncateBigNumber(claimAmount)} ${token?.token}` : '00.00 ' + token?.token}`
             }`}
           </Button>
-          <div
-            tw="text-center text-red-2 font-bold text-regular cursor-pointer mb-2.5"
+          <Button
+            variant={'link'}
+            colorScheme={'red'}
+            fullWidth
             onClick={() => {
               handleCancel()
               setActionModal(false)
             }}
           >
             Cancel
-          </div>
-          <div tw="text-regular dark:text-grey-2 text-grey-1 text-tiny font-semibold text-center">
+          </Button>
+          <div className="text-regular dark:text-grey-2 text-grey-1 text-tiny font-semibold text-center">
             By {actionType === 'withdraw' ? 'withdrawing' : actionType === 'deposit' ? 'depositing' : 'claiming'},
             you agree to our{' '}
             <a
               href="https://www.goosefx.io/terms"
               target={'_blank'}
               rel={'noreferrer'}
-              tw="text-tiny font-semibold underline dark:text-white text-blue-1"
+              className="text-tiny font-semibold underline dark:text-white text-blue-1"
             >
               Terms of Service
             </a>
@@ -229,45 +214,28 @@ export const ActionModal: FC<{
     ),
     [breakpoint, mode, isButtonLoading, diffTimer]
   )
-
-  return breakpoint.isMobile ? (
-    <Drawer
-      title={null}
-      placement="bottom"
-      closable={false}
-      key="bottom"
-      open={actionModal}
-      getContainer={elem}
-      maskClosable={true}
-      height="auto"
-      destroyOnClose={true}
-      className={'gfx-drawer'}
-    >
-      {Content()}
-    </Drawer>
-  ) : (
-    <STYLED_POPUP
-      height={
-        breakpoint.isMobile
-          ? actionType === 'withdraw'
-            ? '320px'
+  return (
+    <Dialog open={actionModal}>
+      <DialogOverlay />
+      <DialogContent
+        className={cn(
+          'w-full min-lg:w-[560px] sm:rounded-b-none p-6',
+          breakpoint.isMobile
+            ? actionType === 'withdraw'
+              ? 'h-[320px]'
+              : actionType === 'claim'
+              ? 'h-[250px]'
+              : 'h-[275px]'
             : actionType === 'claim'
-            ? '250px'
-            : '275px'
-          : actionType === 'claim'
-          ? '250px'
-          : actionType === 'withdraw' && earlyWithdrawFee > 0
-          ? '330px'
-          : '295px'
-      }
-      width={'560px'}
-      title={null}
-      centered={true}
-      visible={actionModal ? true : false}
-      onCancel={() => setActionModal(false)}
-      footer={null}
-    >
-      {Content()}
-    </STYLED_POPUP>
+            ? 'h-[250px]'
+            : actionType === 'withdraw' && earlyWithdrawFee > 0
+            ? 'h-[330px]'
+            : 'h-[295px]'
+        )}
+        placement={breakpoint.isMobile ? 'bottom' : 'default'}
+      >
+        <DialogBody className={'w-full mx-auto'}>{Content()}</DialogBody>
+      </DialogContent>
+    </Dialog>
   )
 }
