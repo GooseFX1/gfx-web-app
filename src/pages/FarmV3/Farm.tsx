51f42588e9d23b6e33275f5c005e94fc8a7d03e6
Shashank729 <shashankshekhar729@gmail.com>
2024-05-27 13:20:30 +0530

    Removes banner from farm page

--- src/pages/FarmV3/Farm.tsx
+++ src/pages/FarmV3/Farm.tsx
@@ -20,21 +20,10 @@ const Farm: FC = () => {
         className={`dark:bg-black-1 bg-grey-5 h-[calc(100vh - 56px)] overflow-auto max-w-[1440px]
        m-auto no-scrollbar gap-7.5`}
       >
-        <div
-          className="flex items-center sm:h-12 justify-center h-6 bg-yellow-1 !w-[100vw] 
-        absolute left-0 sm:relative sm:p-1"
-        >
-          <h5 className="text-black-1 sm:text-[13px]">
-            Pool trading is temporarily disabled for the next 48 hours due to a program upgrade. However, deposits
-            and withdrawals remain operational.
-          </h5>
-        </div>
-        <div className="mt-6 sm:mt-0">
-          <FarmHeader />
-          <div className={'gap-7.5 mt-7.5 p-5 sm:px-2.5 sm:pb-3.75 pt-0'}>
-            <FarmTable />
-            <Faqs />
-          </div>
+        <FarmHeader />
+        <div className={'gap-7.5 mt-7.5 p-5 sm:px-2.5 sm:pb-3.75 pt-0'}>
+          <FarmTable />
+          <Faqs />
         </div>
       </div>
     </SSLProvider>
