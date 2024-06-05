f5b2103e1d4685c49876617a429d6ef7245b3255
John Daffue <daffuejhn@gmail.com>
2024-06-05 20:02:32 +0100

    fix: network status only based on status but not latency

--- src/hooks/useNetworkStatus.tsx
+++ src/hooks/useNetworkStatus.tsx
@@ -1,15 +1,15 @@
 import { useCallback, useEffect, useMemo, useState } from 'react'
 import { useConnectionConfig } from '@/context'
 import { INTERVALS } from '@/utils/time'
-type NETWORK_STATUS_UNKOWN = -1
-type NETWORK_STATUS_NORMAL = 0
-type NETWORK_STATUS_CONGESTED = 1
-type NETWORK_STATUS_DEGRADED = 2
+const NETWORK_STATUS_UNKOWN = -1
+const NETWORK_STATUS_NORMAL = 0
+const NETWORK_STATUS_CONGESTED = 1
+const NETWORK_STATUS_DEGRADED = 2
 type NETWORK_STATUS =
-  | NETWORK_STATUS_UNKOWN
-  | NETWORK_STATUS_NORMAL
-  | NETWORK_STATUS_CONGESTED
-  | NETWORK_STATUS_DEGRADED
+  | typeof NETWORK_STATUS_UNKOWN
+  | typeof NETWORK_STATUS_NORMAL
+  | typeof NETWORK_STATUS_CONGESTED
+  | typeof NETWORK_STATUS_DEGRADED
 type MAPPED_NETWORK_STATUS = 'Unknown' | 'Normal' | 'Congested' | 'Degraded'
 const SOLANA_DEFAULT_ENDPOINT = 'https://api.mainnet-beta.solana.com'
 type NetworkStatusReturn = {
@@ -31,7 +31,7 @@ type HealthResponse = {
 }
 function useNetworkStatus(): NetworkStatusReturn {
   const [status, setStatus] = useState<NETWORK_STATUS>(-1)
-  const { endpoint } = useConnectionConfig()
+  const { endpoint, latency } = useConnectionConfig()
   const refetch = useCallback(async () => {
     try {
       const res: HealthResponse = await fetch(`${endpoint ?? SOLANA_DEFAULT_ENDPOINT}`, {
@@ -77,6 +77,16 @@ function useNetworkStatus(): NetworkStatusReturn {
     const interval = setInterval(() => refetch(), INTERVALS.MINUTE * 5)
     return () => clearInterval(interval)
   }, [endpoint])
+  useEffect(()=>{
+    if (latency < 250 && status != NETWORK_STATUS_NORMAL) {
+      setStatus(0)
+    } else if (latency < 500 && status != NETWORK_STATUS_CONGESTED) {
+      setStatus(1)
+    } else if (latency >= 500 && status != NETWORK_STATUS_DEGRADED) {
+      setStatus(2)
+    }
+    console.log(latency)
+  },[latency])
   return { status, mappedStatus, refetch }
 }
 
