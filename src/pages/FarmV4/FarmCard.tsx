e8aee75e47d319b6fe2fd73d003f6eb7a5c40d22
Luke Woodward <20819255+lukewalt@users.noreply.github.com>
2024-07-16 16:28:21 -0400

    adjusts amm card border

--- src/pages/FarmV4/FarmCard.tsx
+++ src/pages/FarmV4/FarmCard.tsx
@@ -8,12 +8,20 @@ export const FarmCard: FC<{ token: any }> = ({ token }): ReactElement => {
   return (
     <div
       className="h-[207px] w-[24%] bg-red-100 mr-2.5 mb-3.75 border 
-        border-solid dark:border-black-4 border-grey-4 bg-white dark:bg-black-2 p-2.5"
+        border-solid dark:border-black-4 border-grey-4 bg-white dark:bg-black-2 p-2.5 rounded-[8px]"
     >
       <div className="flex flex-row justify-between mb-2.5">
-        <div className="flex">
-          <Icon src={`img/crypto/${token?.sourceToken}.svg`} size="lg" />
-          <Icon src={`img/crypto/${token?.targetToken}.svg`} size="lg" />
+        <div className="flex relative">
+          <Icon
+            src={`img/crypto/${token?.sourceToken}.svg`}
+            size="lg"
+            className={'border-solid dark:border-black-2 border-white border-[3px] rounded-full'}
+          />
+          <Icon
+            src={`img/crypto/${token?.targetToken}.svg`}
+            size="lg"
+            className={'absolute left-[30px] border-solid dark:border-black-2 border-white border-[3px] rounded-full'}
+          />
         </div>
         <Button
           className="cursor-pointer bg-blue-1 text-white"
