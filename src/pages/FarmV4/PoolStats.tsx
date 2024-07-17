1303b0571c7a23d90a0b2f877389ba84b3e6647e
Luke Woodward <20819255+lukewalt@users.noreply.github.com>
2024-07-16 21:21:53 -0400

    fix: adjusts flexbox of pool controls

--- src/pages/FarmV4/PoolStats.tsx
+++ src/pages/FarmV4/PoolStats.tsx
@@ -4,27 +4,35 @@ import { GradientButtonWithBorder } from "@/pages/TradeV3/perps/components/Perps
 
 export const PoolStats: FC<{token: any}> = ({ token }): ReactElement => {
     return (
-        <>
-            <div className="flex justify-between mb-2">
-                <span className="!font-regular font-semibold dark:text-grey-2 text-grey-1 underline decoration-dotted">Liquidity</span>
-                <span className="!font-regular font-semibold dark:text-grey-8 text-black-4">{token?.liquidity}</span>
-            </div>
-            <div className="flex justify-between mb-2">
-                <span className="!font-regular font-semibold dark:text-grey-2 text-grey-1 underline decoration-dotted">Volume</span>
-                <span className="!font-regular font-semibold dark:text-grey-8 text-black-4">{token?.volume}</span>
-            </div>
-            <div className="flex justify-between mb-2">
-                <span className="!font-regular font-semibold dark:text-grey-2 text-grey-1 underline decoration-dotted">Fees</span>
-                <span className="!font-regular font-semibold dark:text-grey-8 text-black-4">{token?.fees}</span>
-            </div>
-            <div className="flex justify-between mb-2">
-                <span className="!font-regular font-semibold dark:text-grey-2 text-grey-1 underline decoration-dotted">APR</span>
-                <div className="w-2/12">
-                    <GradientButtonWithBorder radius={5} height={25}>
-                        <span className="!font-regular font-semibold dark:text-grey-8 text-black-4">185%</span>
-                    </GradientButtonWithBorder>
-                </div>
-            </div>
-        </>
+      <>
+        <div className="flex justify-between mb-2">
+          <span className="!font-regular font-semibold dark:text-grey-2 text-grey-1 underline decoration-dotted">
+            Liquidity
+          </span>
+          <span className="!font-regular font-semibold dark:text-grey-8 text-black-4">{token?.liquidity}</span>
+        </div>
+        <div className="flex justify-between mb-2">
+          <span className="!font-regular font-semibold dark:text-grey-2 text-grey-1 underline decoration-dotted">
+            Volume
+          </span>
+          <span className="!font-regular font-semibold dark:text-grey-8 text-black-4">{token?.volume}</span>
+        </div>
+        <div className="flex justify-between mb-2">
+          <span className="!font-regular font-semibold dark:text-grey-2 text-grey-1 underline decoration-dotted">
+            Fees
+          </span>
+          <span className="!font-regular font-semibold dark:text-grey-8 text-black-4">{token?.fees}</span>
+        </div>
+        <div className="flex justify-between mb-2">
+          <span className="!font-regular font-semibold dark:text-grey-2 text-grey-1 underline decoration-dotted">
+            APR
+          </span>
+          <div className="w-2/12">
+            <GradientButtonWithBorder radius={5} height={22}>
+              <span className="!font-regular font-semibold font-poppins dark:text-grey-8 text-black-4">185%</span>
+            </GradientButtonWithBorder>
+          </div>
+        </div>
+      </>
     )
 }
