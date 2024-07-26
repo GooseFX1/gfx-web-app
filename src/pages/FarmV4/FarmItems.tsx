267e4b77a7674a83c51ec0c348d8b0fd7532493b
Shashank729 <shashankshekhar729@gmail.com>
2024-07-26 17:31:36 +0530

    Fixes toggle and filtering issues due to ssl context data

--- src/pages/FarmV4/FarmItems.tsx
+++ src/pages/FarmV4/FarmItems.tsx
@@ -27,32 +27,33 @@ const FarmItems: FC<{
 
   return (
     <div className={''}>
-      {((numberOfTokensDeposited === 0 && showDeposited) || tokens?.length === 0) && (
+      {(numberOfTokensDeposited === 0 && showDeposited) || tokens?.length === 0 ? (
         <NoResultsFound requestPool={!showDeposited} str={noResultsTitle} subText={noResultsSubText} />
+      ) : (
+        <div className="border-top grid gap-3 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
+          {pool.name === 'All' && !showDeposited && !searchTokens && <FindPool />}
+          {tokens
+            .filter((token: any) => {
+              if (pool.name === 'All') return true
+              else return pool.name === token.type
+            })
+            .map((token, i) => {
+              if (!token || !filteredLiquidityAccounts) return null
+              const liqAcc = filteredLiquidityAccounts[token.sourceTokenMintAddress]
+              const userDepositedAmount = truncateBigString(
+                liqAcc?.amountDeposited.toString(),
+                token.sourceTokenMintDecimals
+              )
+
+              const show =
+                (showDeposited && Boolean(userDepositedAmount) && userDepositedAmount != '0.00') || !showDeposited
+
+              return show ? (
+                <FarmCard token={token} key={`${token?.sourceToken}-${token?.targetToken}-${i}`} />
+              ) : null
+            })}
+        </div>
       )}
-      <div className="border-top grid gap-3 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
-        {pool.name === 'All' && !showDeposited && !searchTokens && <FindPool />}
-        {tokens
-          .filter((token: any) => {
-            if (pool.name === 'All') return true
-            else return pool.name === token.type
-          })
-          .map((token, i) => {
-            if (!token || !filteredLiquidityAccounts) return null
-            const liqAcc = filteredLiquidityAccounts[token.sourceTokenMintAddress]
-            const userDepositedAmount = truncateBigString(
-              liqAcc?.amountDeposited.toString(),
-              token.sourceTokenMintDecimals
-            )
-
-            const show =
-              (showDeposited && Boolean(userDepositedAmount) && userDepositedAmount != '0.00') || !showDeposited
-
-            return show ? (
-              <FarmCard token={token} key={`${token?.sourceToken}-${token?.targetToken}-${i}`} />
-            ) : null
-          })}
-      </div>
       {(numberOfTokensDeposited === 0 && showDeposited) || tokens?.length === 0 ? (
         <></>
       ) : (
