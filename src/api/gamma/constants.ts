4a6a3151f55b5e943b8150b06cd0537898b3a184
Luke Woodward <20819255+lukewalt@users.noreply.github.com>
2024-08-16 00:14:03 -0400

    creates init gamma context and api client

---
+++ src/api/gamma/constants.ts
@@ -0,0 +1,10 @@
+export const GAMMA_API_BASE = 'ssl-stats'
+
+export enum GAMMA_ENDPOINTS_V1 {
+    CONFIG = '/config',
+    STATS = '/stats',
+    POOLS = '/pools',
+    USER  = `/user`,
+    PORTFOLIO_STATS = `/portfolio-stats`,
+    LP_POSITIONS = `/lp-positions`
+}
--- src/types/gamma.ts
+++ src/types/gamma.ts
@@ -1,23 +1,13 @@
-// Enums
-enum PoolCategory {
-  Primary = 'Primary',
-  Hyper = 'Hyper'
+interface GAMMAConfig {
+  id: string
+  index: number
+  protocolFeeRate: number
+  tradeFeeRate: number
+  fundFeeRate: number
+  createPoolFee: string
 }
 
-// Types
-type TokenInfo = {
-  name: string
-  mint: string
-}
-
-type GAMMAUser = {
-  uuid: string
-  publicKey: string
-  favorites: string[] // GAMMAPool.uuid
-}
-
-// Interfaces
-interface GAMMAProtocolMetrics {
+interface GAMMAProtocolStats {
   userTotalEarningsUSD: string
   tvlUSD: string
   volume24hrUSD: string
@@ -28,27 +18,116 @@ interface GAMMAProtocolMetrics {
   allTimeFeesUSD: string
 }
 
-interface GAMMAPool {
-  uuid: string
-  tokenA: TokenInfo
-  tokenB: TokenInfo
-  poolCategory: PoolCategory
-  liquidityUSD: string
-  volume24hrUSD: string
-  fees24hrUSD: string
-  apr: number
-  feeTier: number
+// Token Models
+// TODO - marry with web3js types
+type ExtensionsItem = {
+  coingeckoId?: string
+  feeConfig?: TransferFeeDataBaseType
 }
 
-interface UserPortfolioMetrics {
+// Extracted from the state for a token-22 mint.
+interface TransferFeeDataBaseType {
+  transferFeeConfigAuthority: string
+  withdrawWithheldAuthority: string
+  withheldAmount: string
+  olderTransferFee: {
+    epoch: string
+    maximumFee: string
+    transferFeeBasisPoints: number
+  }
+  newerTransferFee: {
+    epoch: string
+    maximumFee: string
+    transferFeeBasisPoints: number
+  }
+}
+
+type GAMMAToken = {
+  chainId: number
+  address: string
+  programId: string
+  logoURI: string
+  symbol: string
+  name: string
+  decimals: number
+  tags: string[] // "hasFreeze" | "hasTransferFee" | "token-2022" | "community" | "unknown" ..etc
+  extensions: ExtensionsItem
+}
+
+type GAMMATokenList = {
+  mintList: GAMMAToken[]
+  blacklist: GAMMAToken[]
+  whitelist: string[]
+}
+
+// Pool Models
+enum GAMMAPoolCategory {
+  Primary = 'Primary',
+  Hyper = 'Hyper'
+}
+
+type GAMMAPoolKeysArray = GAMMAPoolKeys[]
+
+interface GAMMAPoolKeys {
+  programId: string
+  id: string
+  mintA: GAMMAToken
+  mintB: GAMMAToken
+  lookupTableAccount?: string
+  openTime: string
+  vault: { A: string; B: string }
+  authority: string
+  mintLp: GAMMAToken
+  config: GAMMAConfig
+}
+
+interface GAMMAPool {
+  programId: string
+  id: string
+  mintA: GAMMAToken
+  mintB: GAMMAToken
+  price: number
+  mintAmountA: number
+  mintAmountB: number
+  feeRate: number
+  openTime: string
+  tvl: number
+  day: GAMMAPoolStats
+  week: GAMMAPoolStats
+  month: GAMMAPoolStats
+  lpMint: GAMMAToken
+  lpPrice: number
+  lpAmount: number
+  config: GAMMAConfig
+}
+
+interface GAMMAPoolStats {
+  volume: number
+  volumeQuote: number
+  volumeFee: number
+  apr: number
+  feeApr: number
+  priceMin: number
+  priceMax: number
+  rewardApr: number[]
+}
+
+// user model
+type GAMMAUser = {
+  id: string
+  publicKey: string
+  favoritePools: string[] // GAMMAPool.id
+}
+
+interface UserPortfolioStats {
   totalPortfolioValue: string
   totalEarnings: string
 }
 
 interface UserPortfolioLPPosition {
-  uuid: string
-  tokenA: TokenInfo
-  tokenB: TokenInfo
+  id: string
+  tokenA: GAMMAToken
+  tokenB: GAMMAToken
   positionValueUSD: string
   tokenAQuantity: string
   tokenBQuantity: string
@@ -57,37 +136,19 @@ interface UserPortfolioLPPosition {
   apr: number
 }
 
-interface CreateGAMMAPool {
-  poolCategory: PoolCategory
-  tokenA: TokenInfo
-  tokenB: TokenInfo
-  tokenAAmount: string
-  tokenBAmount: string
-  initialPrice: string
-  minPrice: string
-  maxPrice: string
-  totalDeposit: string
-  feeTier: number
-}
-
-// Data model
-interface GAMMADataModel {
-  aggregateMetrics: GAMMAProtocolMetrics
-  pools: GAMMAPool[]
-  userPortfolio: {
-    user: GAMMAUser
-    aggregate: UserPortfolioMetrics
-    lpPositions: UserPortfolioLPPosition[]
-  }
-}
-
-export {
-  PoolCategory,
-  TokenInfo,
-  GAMMAProtocolMetrics,
+export type {
+  GAMMAConfig,
+  GAMMAProtocolStats,
+  GAMMATokenList,
+  GAMMAToken,
+  ExtensionsItem,
+  TransferFeeDataBaseType,
+  GAMMAPoolKeysArray,
+  GAMMAPoolKeys,
   GAMMAPool,
-  CreateGAMMAPool,
-  UserPortfolioMetrics,
-  UserPortfolioLPPosition,
-  GAMMADataModel
+  GAMMAPoolCategory,
+  GAMMAPoolStats,
+  GAMMAUser,
+  UserPortfolioStats,
+  UserPortfolioLPPosition
 }
