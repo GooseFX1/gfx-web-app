c6f0d63c9eb05ff8555a59d038101212fc71dc09
Shashank729 <shashankshekhar729@gmail.com>
2024-08-21 23:33:17 +0530

    Fixes switch for show deposited filter

--- src/pages/FarmV4/FarmContainer.tsx
+++ src/pages/FarmV4/FarmContainer.tsx
@@ -1,15 +1,9 @@
-/* eslint-disable */
-import React, { FC, useEffect, useMemo, useState } from 'react'
+import { FC, useEffect, useMemo, useState } from 'react'
 import { PublicKey } from '@solana/web3.js'
-import { ShowDepositedToggle, SkeletonCommon } from '../../components'
-import { useDarkMode, usePriceFeedFarm, useFarmContext, useRewardToggle } from '../../context'
-import { checkMobile, formatUserBalance, truncateBigString } from '../../utils'
-import useBreakPoint from '../../hooks/useBreakPoint'
-import { Pool, poolType, SSLToken, SSL_TOKENS } from './constants'
+import { useDarkMode, useFarmContext, useRewardToggle } from '../../context'
+import { poolType, SSL_TOKENS } from './constants'
 import { useWallet } from '@solana/wallet-adapter-react'
-import { getPriceObject } from '../../web3'
 import { USER_CONFIG_CACHE } from '../../types/app_params'
-import BN from 'bn.js'
 import { Icon, Switch, Button } from 'gfx-component-lib'
 import RadioOptionGroup from '@/components/common/RadioOptionGroup'
 import SearchBar from '@/components/common/SearchBar'
@@ -20,25 +14,15 @@ export const FarmContainer: FC = () => {
   const { mode } = useDarkMode()
   const existingUserCache: USER_CONFIG_CACHE = JSON.parse(window.localStorage.getItem('gfx-user-cache'))
 
-  const breakpoint = useBreakPoint()
   const { wallet } = useWallet()
   const {
     operationPending,
     pool,
-    setPool,
-    sslData,
-    filteredLiquidityAccounts,
-    sslTableData,
-    liquidityAmount
+    setPool
   } = useFarmContext()
   const [searchTokens, setSearchTokens] = useState<string>('')
-  const [initialLoad, setInitialLoad] = useState<boolean>(true)
   const [showDeposited, setShowDeposited] = useState<boolean>(existingUserCache.farm.showDepositedFilter)
-  const [sort, setSort] = useState<string>('ASC')
-  const [sortType, setSortType] = useState<string>(null)
-  const { prices } = usePriceFeedFarm()
-  const [allClaimModal, setAllClaimModal] = useState<boolean>(false)
-  const { isProMode, isPortfolio } = useRewardToggle()
+  const { isPortfolio } = useRewardToggle()
 
   const pubKey: PublicKey | null = useMemo(
     () => (wallet?.adapter?.publicKey ? wallet?.adapter?.publicKey : null),
@@ -48,46 +32,46 @@ export const FarmContainer: FC = () => {
   //TODO::need to change the calculation upon getting onchain data
   const numberOfTokensDeposited = 0
 
-  const farmTableRow = useMemo(
-    () =>
-      sslData.map((token: SSLToken) => {
-        const tokenName = token?.token === 'SOL' ? 'WSOL' : token?.token
-        const apy = Number(sslTableData?.[tokenName]?.apy)
-        const volume = sslTableData?.[tokenName]?.volume / 1_000_000
-        const nativeFees = sslTableData?.[tokenName]?.fee / 10 ** token?.mintDecimals
-        const feesInUSD =
-          prices[getPriceObject(token?.token)]?.current &&
-          prices[getPriceObject(token?.token)]?.current * nativeFees
-        const nativeLiquidity = liquidityAmount?.[token?.mint?.toBase58()]
-        const liquidityInUSD =
-          prices[getPriceObject(token?.token)]?.current &&
-          prices[getPriceObject(token?.token)]?.current * nativeLiquidity
-        const account = filteredLiquidityAccounts[token?.mint?.toBase58()]
-        const amountDeposited = formatUserBalance(account?.amountDeposited?.toString(), token?.mintDecimals)
-        const beforeDecimal = amountDeposited?.beforeDecimalBN
-        const dataObj = {
-          ...token,
-          apy: apy,
-          volume: volume,
-          fee: feesInUSD,
-          liquidity: liquidityInUSD,
-          beforeDecimal: beforeDecimal
-        }
-        return dataObj
-      }),
-    [sslData, sslTableData, liquidityAmount, filteredLiquidityAccounts]
-  )
+  // const farmTableRow = useMemo(
+  //   () =>
+  //     sslData.map((token: SSLToken) => {
+  //       const tokenName = token?.token === 'SOL' ? 'WSOL' : token?.token
+  //       const apy = Number(sslTableData?.[tokenName]?.apy)
+  //       const volume = sslTableData?.[tokenName]?.volume / 1_000_000
+  //       const nativeFees = sslTableData?.[tokenName]?.fee / 10 ** token?.mintDecimals
+  //       const feesInUSD =
+  //         prices[getPriceObject(token?.token)]?.current &&
+  //         prices[getPriceObject(token?.token)]?.current * nativeFees
+  //       const nativeLiquidity = liquidityAmount?.[token?.mint?.toBase58()]
+  //       const liquidityInUSD =
+  //         prices[getPriceObject(token?.token)]?.current &&
+  //         prices[getPriceObject(token?.token)]?.current * nativeLiquidity
+  //       const account = filteredLiquidityAccounts[token?.mint?.toBase58()]
+  //       const amountDeposited = formatUserBalance(account?.amountDeposited?.toString(), token?.mintDecimals)
+  //       const beforeDecimal = amountDeposited?.beforeDecimalBN
+  //       const dataObj = {
+  //         ...token,
+  //         apy: apy,
+  //         volume: volume,
+  //         fee: feesInUSD,
+  //         liquidity: liquidityInUSD,
+  //         beforeDecimal: beforeDecimal
+  //       }
+  //       return dataObj
+  //     }),
+  //   [sslData, sslTableData, liquidityAmount, filteredLiquidityAccounts]
+  // )
 
   const filteredTokens = useMemo(
     () =>
       searchTokens
         ? SSL_TOKENS.filter(
-            (token) =>
-              token?.sourceToken?.toLocaleLowerCase().includes(searchTokens?.toLocaleLowerCase()) ||
-              token?.targetToken?.toLocaleLowerCase().includes(searchTokens?.toLocaleLowerCase())
-          )
+          (token) =>
+            token?.sourceToken?.toLocaleLowerCase().includes(searchTokens?.toLocaleLowerCase()) ||
+            token?.targetToken?.toLocaleLowerCase().includes(searchTokens?.toLocaleLowerCase())
+        )
         : [...SSL_TOKENS],
-    [searchTokens, SSL_TOKENS, sort]
+    [searchTokens, SSL_TOKENS]
   )
 
   useEffect(() => {
@@ -104,39 +88,13 @@ export const FarmContainer: FC = () => {
       })
   }, [pubKey])
 
-  useEffect(() => {
-    sslData?.length && setInitialLoad(false)
-  }, [sslData])
-
-  const sortUserBalances = (sortValue: string) => {
-    farmTableRow.sort((a, b) => {
-      let firstBN = a?.beforeDecimal
-      let secondBN = b?.beforeDecimal
-      if (!firstBN) firstBN = new BN(0)
-      if (!secondBN) secondBN = new BN(0)
-      if (sort === 'DESC') return firstBN.gt(secondBN) ? -1 : 1
-      else return firstBN.lt(secondBN) ? -1 : 1
-    })
-    setSort((prev) => (prev === 'ASC' ? 'DESC' : 'ASC'))
-    setSortType(sortValue)
-  }
+  // useEffect(() => {
+  //   sslData?.length && setInitialLoad(false)
+  // }, [sslData])
 
   const initiateGlobalSearch = (value: string) => {
     setPool(poolType.all)
     setSearchTokens(value)
-  }
-
-  const handleColumnSort = (sortValue: string) => {
-    if (sortValue === 'balance') {
-      sortUserBalances(sortValue)
-      return
-    }
-    farmTableRow.sort((a, b) => {
-      if (sort === 'DESC') return a[sortValue] > b[sortValue] ? -1 : 1
-      else return a[sortValue] > b[sortValue] ? 1 : -1
-    })
-    setSort((prev) => (prev === 'ASC' ? 'DESC' : 'ASC'))
-    setSortType(sortValue)
   }
 
   const handleShowDepositedToggle = () => {
@@ -170,12 +128,12 @@ export const FarmContainer: FC = () => {
                 },
                 {
                   value: poolType.primary.name,
-                  label: isProMode ? 'CLMM' : 'Primary',
+                  label: 'Primary',
                   onClick: () => (operationPending ? null : setPool(poolType.primary))
                 },
                 {
                   value: poolType.hyper.name,
-                  label: isProMode ? 'Standard' : 'Hyper',
+                  label: 'Hyper',
                   onClick: () => (operationPending ? null : setPool(poolType.hyper))
                 }
               ]}
@@ -202,8 +160,15 @@ export const FarmContainer: FC = () => {
                 <div className={'flex flex-row ml-auto gap-3.75'}>
                   {pubKey != null && (
                     <div className="flex items-center mr-2">
-                      <ShowDepositedToggle enabled={showDeposited} setEnable={handleShowDepositedToggle} />
-                      <div className="h-full text-tiny text-left dark:text-grey-2 text-grey-1 font-semibold ml-2 hidden min-lg:block">
+                      <Switch 
+                        variant={"default"} 
+                        size={"sm"} 
+                        colorScheme={"primary"} 
+                        checked={showDeposited} 
+                        onClick={handleShowDepositedToggle} 
+                      />
+                      <div className="h-full text-tiny text-left dark:text-grey-2 text-grey-1 
+                        font-semibold ml-2 hidden min-lg:block">
                         Show <br /> Deposited
                       </div>
                     </div>
