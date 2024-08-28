2fdf03ea4f69e33b019d2a448581ffc8b1c8767e
John Daffue <daffuejhn@gmail.com>
2024-08-28 20:08:47 +0100

    fix: lottie animations

--- src/pages/FarmV4/PortfolioConnect.tsx
+++ src/pages/FarmV4/PortfolioConnect.tsx
@@ -1,16 +1,18 @@
-import { FC } from "react";
-import { Icon } from 'gfx-component-lib'
+import React, { FC } from 'react'
 import { useDarkMode } from '../../context'
 import { Connect } from '@/layouts'
+import Lottie from 'lottie-react'
+import ProfileLockedDark from '@/animations/profile_locked_dark.json'
+import ProfileLockedLite from '@/animations/profile_locked_lite.json'
 
 const PortfolioConnect: FC = (): JSX.Element => {
-    const { mode } = useDarkMode()
+    const { isDarkMode } = useDarkMode()
     return(
         <div className="flex flex-col items-center justify-center h-[calc(100vh-278px)]">
-        <Icon
-          src={`img/assets/portfolio_locked_${mode}.svg`}
-          className="!max-h-[97px] h-[97px] !max-w-[155px] w-[155px]"
-        />
+            <Lottie
+              animationData={isDarkMode ? ProfileLockedDark : ProfileLockedLite}
+              className="h-[97px] max-sm:h-[81px] w-[168px] mx-auto"
+            />
         <div className="font-semibold text-lg font-poppins dark:text-grey-8 text-black-4 mt-3.75">Portfolio</div>
         <div className="font-semibold text-regular dark:text-grey-2 text-grey-1 text-center mb-3.75 mt-1">
           Connect your wallet to view your portfolio, <br /> track pool, token value and more.
