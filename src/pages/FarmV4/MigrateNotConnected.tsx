3e89c3b5d1cfa764da70d4dcb873227b8b9d66ea
John Daffue <daffuejhn@gmail.com>
2024-08-30 21:55:14 +0100

    fix: quick fix on card

--- src/pages/FarmV4/MigrateNotConnected.tsx
+++ src/pages/FarmV4/MigrateNotConnected.tsx
@@ -1,7 +1,11 @@
 import React, { FC } from 'react'
 import { Connect } from '@/layouts'
+import { useWallet } from '@solana/wallet-adapter-react'
+import { Button } from 'gfx-component-lib'
 
-const MigrateNotConnected: FC = () => <div className={'mx-auto flex flex-col  items-center gap-2.5 max-w-[330px]'}>
+const MigrateNotConnected: FC<{noPositions:boolean}> = ({noPositions}) => {
+  const {connected} = useWallet()
+  return <div className={'mx-auto flex flex-col  items-center gap-2.5 max-w-[330px]'}>
     <div className={'flex flex-col gap-1.25 text-center'}>
       <h2 className={`text-text-lightmode-primary dark:text-text-darkmode-secondary`}>
         Boost Your Earnings!
@@ -12,8 +16,14 @@ const MigrateNotConnected: FC = () => <div className={'mx-auto flex flex-col  it
         benefits!
       </p>
     </div>
-    <Connect />
+    {!connected? <Connect /> : <Button
+    colorScheme={'blue'}
+    disabled={noPositions}
+    >
+      {noPositions?'No Positions':'Migrate Now'}
+    </Button>}
   </div>
+}
 
 
 export default MigrateNotConnected
