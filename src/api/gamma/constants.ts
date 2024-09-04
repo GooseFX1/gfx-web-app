7657d43ccea2f73d747484f2fffcbda77effba55
John Daffue <daffuejhn@gmail.com>
2024-09-04 21:30:07 +0100

    fix: initial work for fetching data

--- src/api/gamma/constants.ts
+++ src/api/gamma/constants.ts
@@ -1,12 +1,13 @@
 export const GAMMA_API_BASE = 'gamma-api'
 
 export enum GAMMA_ENDPOINTS_V1 {
-    CONFIG = '/config',
-    STATS = '/stats',
-    POOLS = '/pools',
-    USER  = `/user`,
-    PORTFOLIO_STATS = `/portfolio-stats`,
-    LP_POSITIONS = `/lp-positions`
+  CONFIG = '/config',
+  STATS = '/stats',
+  POOLS = '/pools',
+  USER = `/user`,
+  PORTFOLIO_STATS = `/portfolio-stats`,
+  LP_POSITIONS = `/lp-positions`,
+  TOKEN_LIST = '/token-list'
 }
 
 export const GAMMA_TOKENS = [
