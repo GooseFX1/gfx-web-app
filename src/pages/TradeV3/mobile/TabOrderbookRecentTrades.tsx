6d4440ffbc5f4d099093cca70693380eeee8f6ab
Luke Woodward <20819255+lukewalt@users.noreply.github.com>
2024-10-06 17:47:28 -0400

    removes tradingview chart

--- src/pages/TradeV3/mobile/TabOrderbookRecentTrades.tsx
+++ src/pages/TradeV3/mobile/TabOrderbookRecentTrades.tsx
@@ -1,20 +1,12 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger, cn } from 'gfx-component-lib'
-import React, { ReactElement, useEffect, useState } from 'react'
+import React, { ReactElement, useState } from 'react'
 import { TitleLabel } from '../perps/components/PerpsGenericComp'
 import { OrderBook } from '../OrderBook'
 import { RecentTrades } from '../RecentTrades'
-import { TVChartContainer } from '../../../pages/Crypto/TradingView/TradingView'
 
 const TabOrderbookRecentTrades = (): ReactElement => {
   const [selectedTab, setSelectedTab] = useState('chart')
-  const [chartContainer, setChartContainer] = useState<any>()
-  useEffect(() => {
-    setChartContainer(<TVChartContainer visible={true} />)
-    // setTimeout(() => {
-    //   setChartContainer(<></>)
-    //   setChartContainer(<TVChartContainer symbol={selectedCrypto.pair} visible={true} />)
-    // }, 300)
-  }, [])
+
   return (
     <div>
       <Tabs className="p-0 max-sm:max-h-[300px]" defaultValue="1">
@@ -47,9 +39,6 @@ const TabOrderbookRecentTrades = (): ReactElement => {
             <TitleLabel whiteText={selectedTab === 'trades'}>Recent Trades</TitleLabel>
           </TabsTrigger>
         </TabsList>
-        <TabsContent className={cn('h-[90%] max-sm:h-[220px] max-sm:max-h-[250px]')} value="1">
-          {chartContainer}
-        </TabsContent>
         <TabsContent className={cn('h-[90%] max-sm:h-[65%]')} value="2">
           <OrderBook />
         </TabsContent>
