fb29d6a1c2ad1d040a55e2ab807a5069cab5d053
John Daffue <daffuejhn@gmail.com>
2024-10-09 21:28:45 +0100

    fix: fixed step2.tsx

--- src/pages/FarmV4/Step2.tsx
+++ src/pages/FarmV4/Step2.tsx
@@ -24,6 +24,7 @@ import ScrollingHydrateContainer from '@/components/common/ScrollingHydrateConta
 import WindowingContainer from '@/pages/FarmV4/WindowingContainer'
 import { fetchPoolsByMints } from '@/api/gamma'
 import { GAMMAPool } from '@/types/gamma'
+import { useWallet } from '@solana/wallet-adapter-react'
 
 const Step2: FC<{
   tokenA: JupToken
@@ -43,166 +44,169 @@ const Step2: FC<{
   walletTokenB: string
   setIsCreatePool: Dispatch<SetStateAction<boolean>>
 }> = ({
-  tokenA,
-  setTokenA,
-  tokenB,
-  setTokenB,
-  handleChange,
-  amountTokenA,
-  amountTokenB,
-  // feeTier,
-  // setFeeTier,
-  poolExists,
-  setPoolExists,
-  initialPrice,
-  setInitialPrice,
-  walletTokenA,
-  walletTokenB,
-  setIsCreatePool
-}) => {
-    const { mode } = useDarkMode()
-    const [priceSwitch, setPriceSwitch] = useState(false)
-    const [poolExistsText, setPoolExistsText] = useState<string>('')
-    const [existingPool, setExistingPool] = useState<GAMMAPool>()
-    const { setSelectedCard, setOpenDepositWithdrawSlider } = useGamma()
+        tokenA,
+        setTokenA,
+        tokenB,
+        setTokenB,
+        handleChange,
+        amountTokenA,
+        amountTokenB,
+        // feeTier,
+        // setFeeTier,
+        poolExists,
+        setPoolExists,
+        initialPrice,
+        setInitialPrice,
+        walletTokenA,
+        walletTokenB,
+        setIsCreatePool
+      }) => {
+  const { mode } = useDarkMode()
+  const [priceSwitch, setPriceSwitch] = useState(false)
+  const [poolExistsText, setPoolExistsText] = useState<string>('')
+  const [existingPool, setExistingPool] = useState<GAMMAPool>()
+  const { setSelectedCard, setOpenDepositWithdrawSlider } = useGamma()
+  const { connected } = useWallet()
 
-    useEffect(() => {
-      if (+amountTokenA && +amountTokenB) {
-        !priceSwitch ? setInitialPrice((+amountTokenA / +amountTokenB)?.toFixed(2))
-          : setInitialPrice((+amountTokenB / +amountTokenA)?.toFixed(2))
-      } else {
-        setInitialPrice('')
-      }
-    }, [amountTokenA, amountTokenB, priceSwitch])
+  useEffect(() => {
+    if (+amountTokenA && +amountTokenB) {
+      !priceSwitch ? setInitialPrice((+amountTokenA / +amountTokenB)?.toFixed(2))
+        : setInitialPrice((+amountTokenB / +amountTokenA)?.toFixed(2))
+    } else {
+      setInitialPrice('')
+    }
+  }, [amountTokenA, amountTokenB, priceSwitch])
 
-    const navigateToPool = useCallback(async () => {
+  const navigateToPool = useCallback(async () => {
+    if (!tokenA || !tokenB) return
+    setSelectedCard(existingPool)
+    setIsCreatePool(false)
+    setOpenDepositWithdrawSlider(true)
+  }, [tokenA, tokenB, existingPool, setSelectedCard])
+
+  useLayoutEffect(() => {
+    ;(async () => {
       if (!tokenA || !tokenB) return
-      setSelectedCard(existingPool)
-      setIsCreatePool(false)
-      setOpenDepositWithdrawSlider(true)
-    }, [tokenA, tokenB, existingPool, setSelectedCard])
-
-    useLayoutEffect(() => {
-      ; (async () => {
-        if (!tokenA || !tokenB) return
-        const response = await fetchPoolsByMints(tokenA?.address, tokenB?.address)
-        if(!response || !response?.data?.pools || response?.data?.pools?.length <=0 || !response?.success) return
-        for (const pool of response?.data?.pools) {
-          if (pool?.mintA?.address == tokenA?.address && pool?.mintB?.address == tokenB?.address) {
-            setPoolExists(true)
-            setPoolExistsText(`${tokenA?.symbol} - ${tokenB?.symbol}`)
-            setExistingPool(pool)
-            return
-          }
-          if (pool?.mintA?.address == tokenB?.address && pool?.mintB?.address == tokenA?.address) {
-            setPoolExists(true)
-            setPoolExistsText(`${tokenB?.symbol} - ${tokenA?.symbol}`)
-            setExistingPool(pool)
-            return
-          }
+      const response = await fetchPoolsByMints(tokenA?.address, tokenB?.address)
+      if (!response || !response?.data?.pools || response?.data?.pools?.length <= 0 || !response?.success) return
+      for (const pool of response?.data?.pools) {
+        if (pool?.mintA?.address == tokenA?.address && pool?.mintB?.address == tokenB?.address) {
+          setPoolExists(true)
+          setPoolExistsText(`${tokenA?.symbol} - ${tokenB?.symbol}`)
+          setExistingPool(pool)
+          return
         }
-        setPoolExists(false)
-      })()
-    }, [tokenA, tokenB, setPoolExists])
+        if (pool?.mintA?.address == tokenB?.address && pool?.mintB?.address == tokenA?.address) {
+          setPoolExists(true)
+          setPoolExistsText(`${tokenB?.symbol} - ${tokenA?.symbol}`)
+          setExistingPool(pool)
+          return
+        }
+      }
+      setPoolExists(false)
+    })()
+  }, [tokenA, tokenB, setPoolExists])
 
-    return (
-      <>
-        <div className="text-regular !text-grey-2 dark:!text-grey-1 border-b border-solid dark:border-black-4
+  return (
+    <>
+      <div className="text-regular !text-grey-2 dark:!text-grey-1 border-b border-solid dark:border-black-4
               border-grey-4 p-2.5 h-17 ">
-          <span className="text-purple-3">Step 2</span> of 3
-          <h2 className="dark:text-grey-8 text-black-4 font-semibold font-sans text-[18px] mt-2">
-            Pool Settings
-          </h2>
-        </div>
-        <div className="p-3 flex flex-col overflow-scroll border-b-none border-solid 
+        <span className="text-purple-3">Step 2</span> of 3
+        <h2 className="dark:text-grey-8 text-black-4 font-semibold font-sans text-[18px] mt-2">
+          Pool Settings
+        </h2>
+      </div>
+      <div className="p-3 flex flex-col overflow-scroll border-b-none border-solid
           dark:border-black-4 border-grey-4 gap-5">
-          <div>
-            <div className="flex flex-row justify-between items-center mb-2.5">
-              <div className="font-sans text-regular font-semibold dark:text-grey-8 text-black-4">
-                1. Select Token A
-              </div>
-              <div className={cn('flex flex-row items-center', !tokenA && 'invisible')}>
-                <img src={`/img/assets/wallet-${mode}-${walletTokenA !== '0.00' ? 'enabled' : 'disabled'}.svg`}
-                  alt="wallet" className="mr-1.5" />
-                <span className={
-                  cn(
-                    'text-regular font-semibold dark:text-grey-2 text-black-4',
-                    walletTokenA === '0.00' && 'text-text-lightmode-secondary dark:text-text-darkmode-secondary'
-                  )
-                }>
+        <div>
+          <div className="flex flex-row justify-between items-center mb-2.5">
+            <div className="font-sans text-regular font-semibold dark:text-grey-8 text-black-4">
+              1. Select Token A
+            </div>
+            <div className={cn('flex flex-row items-center', !tokenA && 'invisible')}>
+              <img src={`/img/assets/wallet-${mode}-${walletTokenA !== '0.00' ? 'enabled' : 'disabled'}.svg`}
+                   alt="wallet" className="mr-1.5" />
+              <span className={
+                cn(
+                  'text-regular font-semibold dark:text-grey-2 text-black-4',
+                  walletTokenA === '0.00' && 'text-text-lightmode-secondary dark:text-text-darkmode-secondary'
+                )
+              }>
                   {walletTokenA} {tokenA?.symbol}
                 </span>
-              </div>
             </div>
-            <TokenSelectionInput
-              token={tokenA}
-              handleChange={handleChange}
-              amountToken={amountTokenA}
-              setToken={setTokenA}
-            />
           </div>
-          <div>
-            <div className="flex flex-row justify-between items-center mb-2.5">
-              <div className="font-sans text-regular font-semibold dark:text-grey-8 text-black-4">
-                2. Select Token B
-              </div>
-              <div className={cn('flex flex-row items-center', !tokenB && 'invisible')}>
-                <img src={`/img/assets/wallet-${mode}-${walletTokenB !== '0.00' ? 'enabled' : 'disabled'}.svg`}
-                  alt="wallet" className="mr-1.5" />
-                <span className={
-                  cn(
-                    'text-regular font-semibold dark:text-grey-2 text-black-4',
-                    walletTokenB === '0.00' && 'text-text-lightmode-secondary dark:text-text-darkmode-secondary'
-                  )
-                }>
+          <TokenSelectionInput
+            token={tokenA}
+            otherToken={tokenB}
+            handleChange={handleChange}
+            amountToken={amountTokenA}
+            setToken={setTokenA}
+          />
+        </div>
+        <div>
+          <div className="flex flex-row justify-between items-center mb-2.5">
+            <div className="font-sans text-regular font-semibold dark:text-grey-8 text-black-4">
+              2. Select Token B
+            </div>
+            <div className={cn('flex flex-row items-center', !tokenB && 'invisible')}>
+              <img src={`/img/assets/wallet-${mode}-${walletTokenB !== '0.00' ? 'enabled' : 'disabled'}.svg`}
+                   alt="wallet" className="mr-1.5" />
+              <span className={
+                cn(
+                  'text-regular font-semibold dark:text-grey-2 text-black-4',
+                  walletTokenB === '0.00' && 'text-text-lightmode-secondary dark:text-text-darkmode-secondary'
+                )
+              }>
                   {walletTokenB} {tokenB?.symbol}
                 </span>
-              </div>
             </div>
-            <TokenSelectionInput
-              token={tokenB}
-              handleChange={(e) => handleChange(e, false)}
-              amountToken={amountTokenB}
-              setToken={setTokenB} />
           </div>
-          <div>
-            <div className="flex flex-row justify-between items-center mb-2.5">
-              <Tooltip>
-                <TooltipTrigger className={`font-sans text-regular font-semibold dark:text-grey-8
+          <TokenSelectionInput
+            token={tokenB}
+            otherToken={tokenA}
+            handleChange={(e) => handleChange(e, false)}
+            amountToken={amountTokenB}
+            setToken={setTokenB} />
+        </div>
+        <div>
+          <div className="flex flex-row justify-between items-center mb-2.5">
+            <Tooltip>
+              <TooltipTrigger className={`font-sans text-regular font-semibold dark:text-grey-8
                         text-black-4 underline !decoration-dotted`}>
-                  3. Initial Price
-                </TooltipTrigger>
-                <TooltipContent className={'z-[1001]'} align={'start'}>
-                  The initial price is based on the ratio of tokens you deposit for initial liquidity.
-                </TooltipContent>
-              </Tooltip>
-              <div className={cn('flex flex-row items-center',
-                (!tokenA || !tokenB) && 'invisible')} onClick={() => setPriceSwitch((prev) => !prev)}>
-                <img src={`/img/assets/switch_${mode}.svg`}
-                  alt="switch"
-                  className="mr-1.5"
-                />
-                <span className={cn(`text-regular font-bold dark:text-white
+                3. Initial Price
+              </TooltipTrigger>
+              <TooltipContent className={'z-[1001]'} align={'start'}>
+                The initial price is based on the ratio of tokens you deposit for initial liquidity.
+              </TooltipContent>
+            </Tooltip>
+            <div className={cn('flex flex-row items-center',
+              (!tokenA || !tokenB) && 'invisible')} onClick={() => setPriceSwitch((prev) => !prev)}>
+              <img src={`/img/assets/switch_${mode}.svg`}
+                   alt="switch"
+                   className="mr-1.5"
+              />
+              <span className={cn(`text-regular font-bold dark:text-white
                 text-blue-1 underline cursor-pointer`)}>
                   {!priceSwitch ? `${tokenA?.symbol} per ${tokenB?.symbol}` : `${tokenB?.symbol} per ${tokenA?.symbol}`}
                 </span>
-              </div>
             </div>
-            <div className="h-[45px] dark:bg-black-1 bg-grey-5 flex p-2
+          </div>
+          <div className="h-[45px] dark:bg-black-1 bg-grey-5 flex p-2
                     flex-row justify-between rounded-[3px] border border-solid border-black-4
                     dark:border-grey-4 items-center">
               <span className="text-regular font-semibold dark:text-grey-8 text-black-4">
                 {initialPrice}
               </span>
-              <span className={cn(
-                'text-regular font-semibold dark:text-grey-1 text-grey-9',
-                (!tokenA || !tokenB) && 'invisible'
-              )}>
+            <span className={cn(
+              'text-regular font-semibold dark:text-grey-1 text-grey-9',
+              (!tokenA || !tokenB) && 'invisible'
+            )}>
                 {!priceSwitch ? `${tokenA?.symbol} / ${tokenB?.symbol}` : `${tokenB?.symbol} / ${tokenA?.symbol}`}
               </span>
-            </div>
           </div>
-          {/* <div>
+        </div>
+        {/* <div>
             <div className="font-sans text-regular font-semibold dark:text-grey-8 text-black-4">
               4. Fee Tier
             </div>
@@ -235,31 +239,31 @@ const Step2: FC<{
               ]}
             />
           </div> */}
-          {/* We need the swap component here but later */}
-          {(tokenA && tokenB) && (+amountTokenA && +amountTokenB)
-            && (+amountTokenA > +walletTokenA || +amountTokenB > +walletTokenB) ? (
-            <span className='text-red-1 font-sembold text-regular'>
-              You don't have enough tokens in the wallet!
+        {/* We need the swap component here but later */}
+        {(tokenA && tokenB) && (+amountTokenA && +amountTokenB)
+        && (+amountTokenA > +walletTokenA || +amountTokenB > +walletTokenB) ? (
+          <span className="text-red-1 font-sembold text-regular">
+              {connected ? 'You don\'t have enough tokens in the wallet!' : 'Please connect your wallet to proceed!'}
             </span>
-          ) : tokenA && tokenB && (tokenA?.symbol === tokenB?.symbol) ?
-            <span className='text-red-1 font-sembold text-regular'>
+        ) : tokenA && tokenB && (tokenA?.symbol === tokenB?.symbol) ?
+          <span className="text-red-1 font-sembold text-regular">
               Token A and Token B cannot be same! Please create a pool with two different mints!
             </span> : <></>}
-          {poolExists && <div>
-            <Container className={'flex flex-col gap-2.5 p-2.5'}>
-              <Text as={'h3'}>Existing Pool!</Text>
-              <Text as={'p'}>The {poolExistsText} pool exists. Start adding your funds now!</Text>
-              <Button
-                fullWidth
-                colorScheme={'blue'}
-                onClick={navigateToPool}
-              >Go to {poolExistsText} Pool</Button>
-            </Container>
-          </div>}
-        </div>
-      </>
-    )
-  }
+        {poolExists && <div>
+          <Container className={'flex flex-col gap-2.5 p-2.5'}>
+            <Text as={'h3'}>Existing Pool!</Text>
+            <Text as={'p'}>The {poolExistsText} pool exists. Start adding your funds now!</Text>
+            <Button
+              fullWidth
+              colorScheme={'blue'}
+              onClick={navigateToPool}
+            >Go to {poolExistsText} Pool</Button>
+          </Container>
+        </div>}
+      </div>
+    </>
+  )
+}
 export const loadUriImage = (uri: string): boolean => {
   const image = new Image()
   image.src = uri
@@ -275,13 +279,16 @@ export const loadBackUpImage = (symbol: string, mode: string): string => {
   else imageUrl = `/img/crypto/${symbol}.svg`
   return imageUrl
 }
+
 function TokenSelectionInput({
-  token,
-  handleChange,
-  amountToken,
-  setToken
-}: {
+                               token,
+                               handleChange,
+                               amountToken,
+                               setToken,
+                               otherToken
+                             }: {
   token: JupToken | null
+  otherToken: JupToken | null
   handleChange: (e: any, boolean) => void
   amountToken: string
   setToken: Dispatch<SetStateAction<JupToken>>
@@ -331,10 +338,10 @@ function TokenSelectionInput({
             <ScrollingHydrateContainer
               ref={(ref) => setScrollingContainerRef(ref)}
               callback={() => {
-                if (maxTokensReached) return;
+                if (maxTokensReached) return
                 updateTokenList(page + 1, TOKEN_LIST_PAGE_SIZE).then(() =>
                   setPage(page + 1)
-                );
+                )
               }}
             >
               {tokenList.length > 0 ? (
@@ -346,6 +353,7 @@ function TokenSelectionInput({
                       className={'group gap-2 cursor-pointer'}
                       onClick={() => setToken(item)}
                       key={item?.symbol}
+                      disabled={otherToken?.symbol === item?.symbol}
                     >
                       <Icon
                         src={loadUriImage(item?.logoURI) ? item?.logoURI : loadBackUpImage(item?.symbol, mode)}
