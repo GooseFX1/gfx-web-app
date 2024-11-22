627eeb536f2341cf4a7feed7c9dad451576da735
Nishant Chandla <nishantchandla6@gmail.com>
2024-11-22 23:54:19 +0530

    fix: fixed scrolling issues and pools not showing to end users

--- src/pages/FarmV4/WindowingContainer.tsx
+++ src/pages/FarmV4/WindowingContainer.tsx
@@ -9,78 +9,53 @@ type WindowContainerProps<T> = {
 } & HTMLAttributes<HTMLDivElement>
 
 const WindowingContainer: FC<WindowContainerProps<unknown>> = ({
-                                                                 items,
-                                                                 render,
-                                                                 rootElement,
-                                                                 className,
-                                                                 itemClassName,
-                                                                 itemPadding: ITEM_PADDING = 8,
-                                                                 ...rest
-                                                               }): JSX.Element => {
+  items,
+  render,
+  rootElement,
+  className,
+  itemClassName,
+  itemPadding: ITEM_PADDING = 8,
+  ...rest
+}): JSX.Element => {
   const ref = useRef<HTMLDivElement>(null)
-  const [visibleRange, setVisibleRange] = useState({ start: 0, end: 10 })
   const [itemHeight, setItemHeight] = useState(0)
-  const [containerHeight, setContainerHeight] = useState(0)
 
   useLayoutEffect(() => {
     const elementToTarget = rootElement || ref.current
     if (!elementToTarget) return
-
-    //eslint-disable-next-line
-    const handleScroll = (e) => {
-      //console.log('SCROLLING',e.target.scrollTop)
-      if (!elementToTarget) return
-      const scrollTop = elementToTarget.scrollTop
-      const newStart = Math.floor(scrollTop / itemHeight)
-      const newEnd = Math.min(items.length, newStart + Math.ceil(containerHeight / itemHeight))
-      setVisibleRange({ start: newStart, end: newEnd })
-    }
 
     const calculateHeights = () => {
       if (!elementToTarget) return
       const firstItem = elementToTarget.querySelector('[data-item]') as HTMLElement
       if (firstItem) {
         setItemHeight(firstItem.offsetHeight + ITEM_PADDING)
-        setContainerHeight(elementToTarget.clientHeight)
       }
     }
-    //console.log('SCROLL CONNECTED',elementToTarget)
-    const observer = new ResizeObserver(calculateHeights)
-    observer.observe(elementToTarget)
 
     calculateHeights()
-    elementToTarget.addEventListener('scroll', handleScroll)
+  }, [items, rootElement])
 
-    return () => {
-      if (elementToTarget) {
-        elementToTarget.removeEventListener('scroll', handleScroll)
-        observer.unobserve(elementToTarget)
-        observer.disconnect()
-      }
-    }
-  }, [items, itemHeight, containerHeight, rootElement])
-
-  const totalHeight = items.length * itemHeight
+  const windowHeight = Math.min(10, items.length) * itemHeight
 
   return (
     <div
       className={className}
       ref={ref}
       {...rest}
-      style={{ overflowY: 'auto', position: 'relative', height: totalHeight }}
+      style={{ overflowY: 'scroll', position: 'relative', height: windowHeight }}
     >
-      {items.slice(visibleRange.start, visibleRange.end).map((item, index) => (
+      {items.map((item, index) => (
         <div
           className={itemClassName}
-          key={visibleRange.start + index}
+          key={index}
           data-item
           style={{
             position: 'absolute',
-            top: (visibleRange.start + index) * itemHeight + ITEM_PADDING,
+            top: (index) * itemHeight + ITEM_PADDING,
             width: '100%'
           }}
         >
-          {render(item, visibleRange.start + index)}
+          {render(item, index)}
         </div>
       ))}
     </div>
