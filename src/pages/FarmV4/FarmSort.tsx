4a5728541b514ab026c44234c2fca82fb1e2c71f
John Daffue <daffuejhn@gmail.com>
2024-12-18 23:40:12 +0800

    fix: partly rendering

--- src/pages/FarmV4/FarmSort.tsx
+++ src/pages/FarmV4/FarmSort.tsx
@@ -17,37 +17,25 @@ import { GAMMA_SORT_CONFIG } from '@/pages/FarmV4/constants'
 import { useConnectionConfig, useDarkMode, useGamma } from '@/context'
 import { useWalletBalance } from '@/context/walletBalanceContext'
 
-function FarmSort({ isOpen, setIsOpen }: {
-  isOpen: boolean
-  setIsOpen: (isOpen: boolean) => void
-}) {
+function FarmSort({ isOpen, setIsOpen }: { isOpen: boolean; setIsOpen: (isOpen: boolean) => void }) {
   const { userCache, updateUserCache } = useConnectionConfig()
-  const {
-    showCreatedPools,
-    setShowCreatedPools,
-    currentSort,
-    handlePoolSort,
-    showDeposited,
-    setShowDeposited
-  } = useGamma()
+  const { showCreatedPools, setShowCreatedPools, currentSort, handlePoolSort, showDeposited, setShowDeposited } =
+    useGamma()
   const { mode } = useDarkMode()
-  const { publicKey} = useWalletBalance();
+  const { publicKey } = useWalletBalance()
 
-  const handleFilterByCreated = useCallback(
-    () => {
-      setShowCreatedPools((prev) => {
-        updateUserCache({
-          gamma: {
-            ...userCache.gamma,
-            showCreatedFilter: !prev
-          }
-        })
-
-        return !prev
+  const handleFilterByCreated = useCallback(() => {
+    setShowCreatedPools((prev) => {
+      updateUserCache({
+        gamma: {
+          ...userCache.gamma,
+          showCreatedFilter: !prev
+        }
       })
-    },
-    [showCreatedPools, userCache]
-  )
+
+      return !prev
+    })
+  }, [showCreatedPools, userCache])
 
   const handleShowDepositedToggle = () => {
     setShowDeposited((prev) => {
@@ -61,7 +49,7 @@ function FarmSort({ isOpen, setIsOpen }: {
       return !prev
     })
   }
-  console.log({publicKey})
+  console.log({ publicKey })
   return (
     <DropdownMenu open={isOpen} onOpenChange={setIsOpen}>
       <DropdownMenuTrigger asChild className={'focus-visible:outline-none'}>
@@ -71,11 +59,13 @@ function FarmSort({ isOpen, setIsOpen }: {
             size={'md'}
             className={'!max-h-[35px] !max-w-[35px] !h-[35px] !w-[35px]'}
           />
-          {(currentSort !== '1' || showCreatedPools) ? <img
-            className={`absolute top-0.5 left-0 border-1 border-solid w-2.5 h-2.5
+          {currentSort !== '1' || showCreatedPools ? (
+            <img
+              className={`absolute top-0.5 left-0 border-1 border-solid w-2.5 h-2.5
                         border-background-lightmode-primary dark:border-background-darkmode-primary rounded-full`}
-            src={'/img/assets/red-notification-circle.svg'}
-          />:null}
+              src={'/img/assets/red-notification-circle.svg'}
+            />
+          ) : null}
         </Button>
       </DropdownMenuTrigger>
 
@@ -84,12 +74,12 @@ function FarmSort({ isOpen, setIsOpen }: {
           <h4 className="dark:text-white text-black-4 pb-2">Filters</h4>
           <div className={'flex flex-col gap-3'}>
             <div className="flex items-center justify-between ">
-                            <span
-                              className="h-full text-regular text-left dark:text-grey-2 text-grey-1
+              <span
+                className="h-full text-regular text-left dark:text-grey-2 text-grey-1
                                               font-semibold"
-                            >
-                            Show created pools
-                            </span>
+              >
+                Show created pools
+              </span>
               <Switch
                 variant={'default'}
                 size={'sm'}
@@ -98,25 +88,22 @@ function FarmSort({ isOpen, setIsOpen }: {
                 onClick={handleFilterByCreated}
               />
             </div>
-            {publicKey != null && (
-              <div className="flex items-center justify-between">
-                              <span
-                                className="h-full text-regular text-left dark:text-grey-2 text-grey-1
+            <div className="flex items-center justify-between">
+              <span
+                className="h-full text-regular text-left dark:text-grey-2 text-grey-1
                                               font-semibold"
-                              >
-                                Show Deposited
-                              </span>
-                <Switch
-                  variant={'default'}
-                  size={'sm'}
-                  colorScheme={'primary'}
-                  checked={showDeposited}
-                  onClick={handleShowDepositedToggle}
-                />
-              </div>
-            )}
+              >
+                Show Deposited
+              </span>
+              <Switch
+                variant={'default'}
+                size={'sm'}
+                colorScheme={'primary'}
+                checked={showDeposited}
+                onClick={handleShowDepositedToggle}
+              />
+            </div>
           </div>
-
         </>
 
         <h4 className="dark:text-white text-black-4 py-2">Sort By</h4>
