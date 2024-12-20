f850e91aaf68e0d1ab30c5dd31f7355986fe1c84
Shashank729 <shashankshekhar729@gmail.com>
2024-12-20 20:02:43 +0530

    Adds maxPoolFee rate into create pool flow

--- src/web3/Farm.ts
+++ src/web3/Farm.ts
@@ -542,7 +542,8 @@ export const createPool = async (
     program: Program,
     connection: Connection,
     tokenAType: 'spl-token' | 'native' | 'spl-token-2022',
-    tokenBType: 'spl-token' | 'native' | 'spl-token-2022'
+    tokenBType: 'spl-token' | 'native' | 'spl-token-2022',
+    poolType: string
 ) => {
     let token0 = new PublicKey(tokenA?.address)
     let token1 = new PublicKey(tokenB?.address)
@@ -577,7 +578,7 @@ export const createPool = async (
         new BN(amountTokenABN),
         new BN(amountTokenBBN),
         new BN(Math.floor(Date.now() / 1000)),
-        new BN(0),
+        poolType === 'Stable' ? new BN(10000) : poolType === 'Primary' ? new BN(25000) : new BN(100000),
         new BN(0), {
         accounts: createPoolAcc
     })
