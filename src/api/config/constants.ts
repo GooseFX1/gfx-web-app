05ab4299661bf2cad09c2f28f72cae4c8a9205a3
Luke Woodward <20819255+lukewalt@users.noreply.github.com>
2025-02-16 22:49:30 -0500

    adds gammaBoostedRewards config param

--- src/api/config/constants.ts
+++ src/api/config/constants.ts
@@ -2,5 +2,6 @@ export const WEBAPP_CONFIG_URL = 'https://gfx-webapp-config.goosefx.workers.dev'
 
 export enum WEBAPP_CONFIG_ENDPOINTS {
   MAINTENANCE   = '/maintenanceStatus',
+  GAMMA_BOOSTED_REWARDS = '/gammaBoostedRewards',
   GLOBAL_BANNER = '/global-banner'
 }
