c77e8c12bbfb0785d0d3ff3e78d7e0b2a487dbaf
Nishant Chandla <nishantchandla6@gmail.com>
2025-02-19 01:11:36 +0530

    resolve comments

--- src/components/bottom-drawer/index.tsx
+++ src/components/bottom-drawer/index.tsx
@@ -1,5 +1,5 @@
 import useBreakPoint from '@/hooks/useBreakPoint'
-import { Button, Dialog, DialogBody, DialogContent, DialogOverlay } from 'gfx-component-lib'
+import { Dialog, DialogBody, DialogContent, DialogOverlay } from 'gfx-component-lib'
 
 export const BottomDrawer = ({
   isOpen,
@@ -26,15 +26,6 @@ export const BottomDrawer = ({
         fullScreen={!isMobile}
         placement={'bottom'}
       >
-        <Button
-          onClick={() => {
-            /** */
-          }}
-          variant={'ghost'}
-          className={`hidden min-md:inline-block absolute p-[inherit] right-3.75 top-3 min-md:right-5
-                     min-md:top-2.5 z-[1] w-max p-0`}
-          size={'sm'}
-        ></Button>
         <DialogBody
           className={`bg-white dark:bg-black-2 relative min-md:min-h-[441px]
            w-full flex flex-row max-md:flex-col rounded-t-[10px]`}
