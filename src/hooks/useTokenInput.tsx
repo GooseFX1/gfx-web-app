44c9b4aa7c1dc714388aacb331b4fc5e1a1e9df5
John Daffue <daffuejhn@gmail.com>
2025-02-19 20:19:15 +0000

    Fixes:
    Fixed swap input by creating new useTokenInput hook.

---
+++ src/hooks/useTokenInput.tsx
@@ -0,0 +1,61 @@
+import { ChangeEvent, useCallback, useState } from 'react'
+import useMemoizedArrayReturn from '@/hooks/useMemoizedArrayReturn'
+
+export type useTokenInputCommands = {
+  clear: () => void
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void
+  onBlur: (e: ChangeEvent<HTMLInputElement>) => void
+  set: (value: string) => void
+}
+type ReturnValue = [string, useTokenInputCommands]
+
+function useTokenInput() {
+  const [value, setValue] = useState('')
+  const clear = useCallback(() => setValue(''), [])
+  const onChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    let value = e.target.value
+    const cursorPosition: number | null = e.target.selectionStart
+    const mantissas = value.match(new RegExp(/\./g, "g"));
+    const mantissaCount = mantissas?.length ?? 0
+
+    // prepend 0 if value starts with . and its first input to prevent EXPLOSION
+    if (value.startsWith('.') && mantissaCount <= 1 && cursorPosition != null && cursorPosition > 0) {
+      value = '0' + value
+    }
+
+    // remove trailing dot if we have digits after it
+    if (value.endsWith('.') && mantissaCount > 2) {
+      value = value.slice(0, -1)
+    }
+
+    // 2 or more dots in a row - replace with one
+    value = value.replace(/\.{2,}/g, '.');
+
+    // remove leading dot if it is the first character - only
+    if (mantissaCount > 1 && value.startsWith('.')) {
+      value = value.slice(1)
+    }
+
+    setValue(value)
+  }, [])
+
+  const onBlur = useCallback((e) => {
+    let value = e.target.value
+    const mantissas = value.match(new RegExp(/\./g, "g"));
+    const mantissaCount = mantissas?.length ?? 0
+    // prepend 0 if value starts with . and its first input to prevent EXPLOSION
+    if (value.startsWith('.') && mantissaCount <= 1) {
+      value = '0' + value
+    }
+    // remove trailing dot if we have digits after it
+    if (value.endsWith('.')) {
+      value = value + '0'
+    }
+
+    setValue(value)
+  }, [])
+  const set = useCallback((value: string) => setValue(value), [])
+  return useMemoizedArrayReturn<ReturnValue>(value, { clear, onChange, onBlur, set })
+}
+
+export default useTokenInput
