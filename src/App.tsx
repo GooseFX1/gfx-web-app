5bab2e4ef9b582844d66b7a9504ebbc962521c5f
John Daffue <daffuejhn@gmail.com>
2025-02-26 17:33:04 +0000

    added devtools

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.less'
 import AppInner from './AppInner'
 import AppErrorBoundary from '@/components/AppErrorBoundary'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 const queryClient = new QueryClient()
 
@@ -11,6 +12,7 @@ export default function App(): JSX.Element {
     <AppErrorBoundary>
       <QueryClientProvider client={queryClient}>
         <AppInner />
+        <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </AppErrorBoundary>
   )
