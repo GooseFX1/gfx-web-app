d4bb8863c66fd84f5023f36055e0f41fdaec0353
John Daffue <daffuejhn@gmail.com>
2025-03-07 19:45:58 +0000

    calendar changes

--- src/components/token-rewards/index.tsx
+++ src/components/token-rewards/index.tsx
@@ -16,8 +16,8 @@ export const TokenRewardsDrawer = ({ isOpen, setOpen }: { isOpen: boolean; setOp
   const [pool, setPool] = useState<GAMMAPool | null>(null)
   const [selectedToken, setSelectedToken] = useState<JupToken | null>(null)
   const [amountToken, setAmountToken] = useState<string>('')
-  const [startDate, setStartDate] = useState<dayjs.Dayjs>(null)
-  const [endDate, setEndDate] = useState<dayjs.Dayjs>(null)
+  const [startDate, setStartDate] = useState<dayjs.Dayjs>(dayjs())
+  const [endDate, setEndDate] = useState<dayjs.Dayjs>(dayjs().add(7,'day'))
   const { isMobile } = useBreakPoint()
 
   useEffect(() => {
