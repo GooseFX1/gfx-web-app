bf8bdc13c8e0f339ac3fca8dbbce355623f636b1
John Daffue <daffuejhn@gmail.com>
2025-03-24 22:49:14 +0000

    fix: manual refetching due to deposit/withdraw impacting this

--- src/queries/GAMMA/pools/useGammaProgramUserLiquidityQuery.tsx
+++ src/queries/GAMMA/pools/useGammaProgramUserLiquidityQuery.tsx
@@ -1,6 +1,5 @@
 import { useQuery } from '@tanstack/react-query'
 import { QUERY_KEY, UseGammaProgramUserLiquidityKey } from '@/queries/query.helper'
-import { INTERVALS } from '@/utils/time'
 import { usePriceFeedFarm } from '@/context'
 import { PublicKey } from '@solana/web3.js'
 
@@ -10,7 +9,7 @@ function useGammaProgramUserLiquidityQuery({ liqKey }: { liqKey: PublicKey }) {
   return useQuery({
     queryKey: [QUERY_KEY, UseGammaProgramUserLiquidityKey, liqKey?.toBase58(), !!GammaProgram],
     queryFn: async () => await GammaProgram.account.userPoolLiquidity.fetch(liqKey),
-    staleTime: INTERVALS.MINUTE,
+    staleTime: Infinity,
     enabled: !!liqKey && !!GammaProgram
   })
 }
