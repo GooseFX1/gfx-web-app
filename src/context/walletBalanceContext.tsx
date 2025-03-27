03a31646bf99481de4616883ac794e504d58a5a4
John Daffue <daffuejhn@gmail.com>
2025-03-27 18:25:53 +0000

    fix: migrate wallet balance

--- src/context/walletBalanceContext.tsx
+++ src/context/walletBalanceContext.tsx
@@ -1,6 +1,5 @@
-import { createContext, useContext, useEffect, useMemo, useState } from 'react'
+import { createContext, useContext, useEffect } from 'react'
 import {
-  AccountInfo,
   ParsedAccountData,
   PublicKey,
   TokenAmount,
@@ -9,19 +8,21 @@ import {
 } from '@solana/web3.js'
 import { useWallet } from '@solana/wallet-adapter-react'
 import { useConnectionConfig } from '@/context/settings'
-import { useSolSubMulti } from '@/hooks/useSolSubActivity'
-import { SubType } from '@/hooks/useSolSub'
 // It exists :/
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import { TOKEN_2022_PROGRAM_ID, TOKEN_PROGRAM_ID } from '@solana/spl-token'
 import { createAssociatedTokenAccountInstruction } from '@solana/spl-token-v2'
-import { AccountLayout, confirmTransaction } from '@/web3'
+import { confirmTransaction } from '@/web3'
 import { toast } from 'sonner'
 import { fetchTokensByPublicKey } from '@/api/gamma'
 import Decimal from 'decimal.js-light'
+import { useQuery } from '@tanstack/react-query'
+import { QUERY_KEY } from '@/queries/query.helper'
+import { INTERVALS } from '@/utils/time'
 
 const NATIVE_MINT = new PublicKey('So11111111111111111111111111111111111111112')
+type TokenType = 'spl-token' | 'native' | 'spl-token-2022'
 
 export interface UserTokenAccounts {
   symbol: string
@@ -33,7 +34,7 @@ export interface UserTokenAccounts {
   tokenAmount: TokenAmount
   value: Decimal
   price: number
-  tokenType: 'spl-token' | 'native' | 'spl-token-2022'
+  tokenType: TokenType
 }
 
 export type Balance = Record<string, UserTokenAccounts>
@@ -49,7 +50,6 @@ export interface IWalletBalanceContext {
   createTokenAccount: (data: CreateTokenAccountParams) => Promise<void>
   createTokenAccounts: (data: CreateTokenAccountParams[]) => Promise<void>
   walletValue: string
-  refreshTokenBalance: () => void
 }
 
 const WalletBalanceContext = createContext<IWalletBalanceContext>(null)
@@ -59,169 +59,127 @@ function WalletBalanceProvider({ children }: { children?: React.ReactNode }): JS
 
   const publicKey: PublicKey | null = wallet?.adapter?.publicKey ?? null
   const base58PublicKey = publicKey?.toBase58() ?? ''
-  const { network, connection } = useConnectionConfig()
-  const [balance, setBalance] = useState<Balance>({})
-  const [tokenAccounts, setTokenAccounts] = useState<UserTokenAccounts[]>([])
-  const [walletValue, setWalletValue] = useState<string>('0.0')
+  const { connection } = useConnectionConfig()
 
-  const topBalances: UserTokenAccounts[] = useMemo(() => {
-    const values = Object.values(balance)
-    const alreadyAdded = new Set<string>()
-    return values
-      .filter((v) => {
-        if (alreadyAdded.has(v.symbol) || v.value.isZero()) {
-          return false
-        }
-        alreadyAdded.add(v.symbol)
-        return true
-      })
-      .sort((a, b) => (a.value.gte(b.value) ? -1 : 1))
-  }, [balance])
+  const onChainTokenQuery = useQuery({
+    queryKey: [QUERY_KEY, 'wallet-tokens-onchaim', base58PublicKey],
+    queryFn: async () => {
+      const [standardTokens, token2022, solBalance] = await Promise.all([
+        connection.getParsedTokenAccountsByOwner(publicKey, {
+          programId: TOKEN_PROGRAM_ID
+        }),
+        connection.getParsedTokenAccountsByOwner(publicKey, {
+          programId: TOKEN_2022_PROGRAM_ID
+        }),
+        connection.getBalance(publicKey)
+      ])
 
-  const tokens = tokenAccounts.map((account) => ({
-    publicKey: account.pda,
-    callback: async (accountInfo: AccountInfo<Buffer>) => {
-      console.log(`updating balance for ${account.symbol}`, account)
-
-      if (account.mint === NATIVE_MINT.toBase58()) {
-        const dec = account.decimals || 9
-        const uiAmount = new Decimal(accountInfo.lamports).div(10 ** dec).toNumber()
-        setBalanceBySymbol(account.mint, {
-          amount: accountInfo.lamports.toString(),
-          decimals: dec,
-          uiAmount: uiAmount,
-          uiAmountString: uiAmount.toFixed(2)
-        })
-        console.log('updating balance for - sol', uiAmount)
-        return
-      }
-      const decodedAccount = AccountLayout.decode(accountInfo.data)
-      const uiAmount = new Decimal(decodedAccount.amount.toString()).div(10 ** account.decimals).toNumber()
-      const amount: TokenAmount = {
-        amount: decodedAccount.amount.toString(),
-        decimals: account.decimals,
-        uiAmount: uiAmount,
-        uiAmountString: uiAmount.toFixed(2)
-      }
-      console.log(`updating balance for - ${account.symbol}`, uiAmount)
-      setBalanceBySymbol(account.mint, amount)
-    }
-  }))
-
-  const { callbackOn, callbackOff } = useSolSubMulti({
-    subType: SubType.AccountChange,
-    publicKeys: tokens
+      const accounts = [...standardTokens.value, ...token2022.value]
+      return { accounts, solBalance }
+    },
+    staleTime: Infinity,
+    enabled: !!base58PublicKey
   })
-  useEffect(() => {
-    if (tokenAccounts.length > 0) {
-      callbackOn()
-    }
-    return () => {
-      callbackOff()
-    }
-  }, [tokenAccounts])
-  useEffect(() => {
-    if (!publicKey) {
-      callbackOff()
-      setTokenAccounts([])
-      setBalance({})
-      return
-    }
+  const gammaTokenQuery = useQuery({
+    queryKey: [QUERY_KEY, 'wallet-gamma-tokens', onChainTokenQuery.data],
+    queryFn: async () => {
+      try {
+        const tokenAccounts: Balance = {}
+        const tokenInfo = {}
+        let addresses = NATIVE_MINT.toBase58() + ','
+        onChainTokenQuery.data.accounts.forEach((account) => {
+          const data = account.account.data as ParsedAccountData
+          addresses += data.parsed.info.mint + ','
+          tokenInfo[data.parsed.info.mint] = data.parsed.info
+          tokenInfo[data.parsed.info.mint].pda = account.pubkey
+          tokenInfo[data.parsed.info.mint].tokenType = data.program
+        })
 
-    getTokenAccounts()
-  }, [connection, network, publicKey])
+        addresses = addresses.slice(0, -1)
 
-  function setBalanceBySymbol(mint: string, amount: TokenAmount) {
-    setBalance((prev) => {
-      const originalValue = prev[mint]
-      return {
-        ...prev,
-        [mint]: {
-          ...prev[mint],
-          tokenAmount: amount,
-          value: new Decimal(amount.uiAmount).mul(originalValue.price)
+        const solUIAmount = onChainTokenQuery.data.solBalance / 10 ** 9
+
+        const sol = {
+          isNative: true,
+          symbol: 'SOL',
+          name: 'Solana',
+          logoURI:
+            'https://raw.githubusercontent.com/solana-labs/token-list/main/assets/' +
+            'mainnet/So11111111111111111111111111111111111111112/logo.png',
+          decimals: 9,
+          mint: NATIVE_MINT.toBase58(),
+          pda: publicKey,
+          tokenAmount: {
+            amount: onChainTokenQuery.data.solBalance.toString(),
+            decimals: 9,
+            uiAmountString: solUIAmount.toFixed(2),
+            uiAmount: solUIAmount
+          },
+          price: 0.0,
+          value: new Decimal(0.0),
+          tokenType: 'native' as TokenType
         }
-      }
-    })
-  }
+        tokenAccounts[NATIVE_MINT.toBase58()] = sol
+        tokenAccounts[publicKey.toBase58()] = sol
 
-  async function getTokenAccounts() {
-    if (!publicKey) return
-    const [standardTokens, token2022, solBalance] = await Promise.all([
-      connection.getParsedTokenAccountsByOwner(publicKey, {
-        programId: TOKEN_PROGRAM_ID
-      }),
-      connection.getParsedTokenAccountsByOwner(publicKey, {
-        programId: TOKEN_2022_PROGRAM_ID
-      }),
-      connection.getBalance(publicKey)
-    ])
+        const tokenListResponse = await fetchTokensByPublicKey(addresses)
 
-    const accounts = [...standardTokens.value, ...token2022.value]
-    const tokenAccounts = {}
-    const tokenInfo = {}
-    let addresses = NATIVE_MINT.toBase58() + ','
-    accounts.forEach((account) => {
-      const data = account.account.data as ParsedAccountData
-      addresses += data.parsed.info.mint + ','
-      tokenInfo[data.parsed.info.mint] = data.parsed.info
-      tokenInfo[data.parsed.info.mint].pda = account.pubkey
-      tokenInfo[data.parsed.info.mint].tokenType = data.program
-    })
+        let currentWalletValue = new Decimal(0.0)
+        if (tokenListResponse.success && tokenListResponse.data.tokens.length > 0) {
+          for (const data of tokenListResponse.data.tokens) {
+            const { address, ...rest } = data
+            if (!(data.address in tokenAccounts) && data.address in tokenInfo) {
+              tokenAccounts[data.address] = { ...tokenInfo[data.address] }
+            }
 
-    addresses = addresses.slice(0, -1)
-
-    const solUIAmount = solBalance / 10 ** 9
-
-    const sol = {
-      isNative: true,
-      symbol: 'SOL',
-      name: 'Solana',
-      logoURI:
-        'https://raw.githubusercontent.com/solana-labs/token-list/main/assets/' +
-        'mainnet/So11111111111111111111111111111111111111112/logo.png',
-      decimals: 9,
-      mint: NATIVE_MINT.toBase58(),
-      pda: publicKey,
-      tokenAmount: {
-        amount: solBalance.toString(),
-        decimals: 9,
-        uiAmountString: solUIAmount.toFixed(2),
-        uiAmount: solUIAmount
-      },
-      price: 0.0,
-      value: new Decimal(0.0),
-      tokenType: 'native'
-    }
-    tokenAccounts[NATIVE_MINT.toBase58()] = sol
-    tokenAccounts[publicKey.toBase58()] = sol
-    let currentWalletValue = new Decimal(0.0)
-    try {
-      const tokenListResponse = await fetchTokensByPublicKey(addresses)
-
-      if (tokenListResponse.success && tokenListResponse.data.tokens.length > 0) {
-        for (const data of tokenListResponse.data.tokens) {
-          const { address, ...rest } = data
-          if (!(data.address in tokenAccounts) && data.address in tokenInfo) {
-            tokenAccounts[data.address] = { ...tokenInfo[data.address] }
+            tokenAccounts[data.address].mint = address
+            tokenAccounts[data.address] = Object.assign(tokenAccounts[data.address], rest)
+            tokenAccounts[data.address].price = data.price
+            const value = new Decimal(tokenAccounts[data.address].tokenAmount.uiAmount).mul(data.price)
+            tokenAccounts[data.address].value = value
+            currentWalletValue = currentWalletValue.add(value)
           }
-
-          tokenAccounts[data.address].mint = address
-          tokenAccounts[data.address] = Object.assign(tokenAccounts[data.address], rest)
-          tokenAccounts[data.address].price = data.price
-          const value = new Decimal(tokenAccounts[data.address].tokenAmount.uiAmount).mul(data.price)
-          tokenAccounts[data.address].value = value
-          currentWalletValue = currentWalletValue.add(value)
         }
+        const values = Object.values(tokenAccounts)
+        const alreadyAdded = new Set<string>()
+        const topBalances = values
+          .filter((v) => {
+            if (alreadyAdded.has(v.symbol) || v.value.isZero()) {
+              return false
+            }
+            alreadyAdded.add(v.symbol)
+            return true
+          })
+          .sort((a, b) => (a.value.gte(b.value) ? -1 : 1))
+        return {
+          tokenAccounts: Object.values(tokenAccounts),
+          balance: tokenAccounts,
+          walletValue: currentWalletValue.toFixed(2),
+          topBalances
+        }
+      } catch (e) {
+        console.error('Error fetching token list', e)
       }
-      console.log('tokenAccounts', { tokenAccounts, tokenListResponse })
-    } catch (e) {
-      console.error('Error fetching token list', e)
+    },
+    placeholderData: {
+      tokenAccounts: [],
+      balance: {},
+      walletValue: '0.0',
+      topBalances: []
+    },
+    staleTime: INTERVALS.MINUTE,
+    enabled: onChainTokenQuery.isSuccess && !onChainTokenQuery.isFetching && !!base58PublicKey
+  })
+
+  useEffect(() => {
+    if (!base58PublicKey) return
+    const id = connection.onAccountChange(publicKey, () => onChainTokenQuery.refetch(), {
+      commitment: 'confirmed'
+    })
+    return () => {
+      connection.removeAccountChangeListener(id)
     }
-    setTokenAccounts(Object.values(tokenAccounts))
-    setBalance(tokenAccounts)
-    setWalletValue(currentWalletValue.toFixed(2))
-  }
+  }, [connection, base58PublicKey, publicKey])
 
   function createTokenAccountInstruction(data: CreateTokenAccountParams) {
     return createAssociatedTokenAccountInstruction(publicKey, data.pda, publicKey, data.mint)
@@ -282,21 +240,20 @@ function WalletBalanceProvider({ children }: { children?: React.ReactNode }): JS
       }
     }
   }
-  const balanceProxy = new Proxy(balance, balanceProxyHandler)
+  const balanceProxy = new Proxy(gammaTokenQuery.data?.balance, balanceProxyHandler)
 
   return (
     <WalletBalanceContext.Provider
       value={{
         balance: balanceProxy,
-        topBalances,
+        topBalances: gammaTokenQuery.data?.topBalances ?? [],
         publicKey: publicKey,
         base58PublicKey,
         createTokenAccountInstruction,
         createTokenAccountInstructions,
         createTokenAccount,
         createTokenAccounts,
-        walletValue,
-        refreshTokenBalance: getTokenAccounts
+        walletValue: gammaTokenQuery.data?.walletValue ?? '0.0',
       }}
     >
       {children}
