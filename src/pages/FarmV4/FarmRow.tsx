6b7e86ca76632a0edcedabf14bfed34dcd2caf31
Luke Woodward <20819255+lukewalt@users.noreply.github.com>
2025-04-12 11:55:03 -0400

    fix: patch apr on pool stats

--- src/pages/FarmV4/FarmRow.tsx
+++ src/pages/FarmV4/FarmRow.tsx
@@ -179,8 +179,8 @@ const FarmRow: FC<FarmRowProps> = ({ pool, ...props }) => {
                 <div>
                   <h2 className="text-[10px] text-primary-gradient">Boosted Rewards</h2>
 
-                  {activeReward.map((reward) => (
-                  <div className="flex flex-row items-center mb-3">
+                  {activeReward.map((reward, index) => (
+                  <div key={`${reward.token.symbol}-${index}`} className="flex flex-row items-center mb-3">
                     <IconWithFallback
                       src={loadIconImage(reward.token.logoURI, mode)}
                       className="border-solid dark:border-black-2 border-white
--- src/pages/FarmV4/PoolStats.tsx
+++ src/pages/FarmV4/PoolStats.tsx
@@ -4,8 +4,16 @@ import { GAMMAPool } from '@/types/gamma'
 import { numberFormatter } from '@/utils'
 import BigNumber from 'bignumber.js'
 import { fetchTokensByPublicKey } from '@/api/gamma'
+import { getPoolValuesByRange } from '@/pages/FarmV4/FarmRow'
+import { useBoostedRewards } from '@/context/boostedRewardsContext'
+import { useQuery } from '@tanstack/react-query'
+import { PublicKey } from '@solana/web3.js'
+import { QUERY_KEY } from '@/queries/query.helper'
+import { useGamma } from '@/context'
 
 export const PoolStats: FC<{ pool: GAMMAPool }> = ({ pool }): ReactElement => {
+  const { viewRange } = useGamma()
+  const { getActiveRewardByPoolId } = useBoostedRewards()
   const poolTVL = useMemo(() => {
     const liquidity = parseFloat(pool.tvl)
     return liquidity ? numberFormatter(Math.max(0, liquidity)) : '0.00'
@@ -17,10 +25,23 @@ export const PoolStats: FC<{ pool: GAMMAPool }> = ({ pool }): ReactElement => {
 
   const [fees, setFees] = useState<string>('Loading')
 
-  const dailyAPR = useMemo(
-    () => numberFormatter(Math.max(0, pool?.stats?.daily?.feesAprUsd)),
-    [pool?.stats?.daily?.feesAprUsd]
+  const { data: activeReward } = useQuery({
+    queryKey: [QUERY_KEY, 'activeReward', pool.id],
+    queryFn: () => getActiveRewardByPoolId(new PublicKey(pool.id)),
+    enabled: !!pool.id
+  })
+
+  const { formattedAPR } = useMemo(
+    () => getPoolValuesByRange(pool, viewRange),
+    [pool.stats, viewRange]
   )
+
+  const activeRewardsAmount = activeReward?.reduce((acc, curr) => acc.plus(curr.pricePerDayUsd), new BigNumber(0))
+  const apr = activeReward
+    ? numberFormatter(
+        new BigNumber(formattedAPR).plus(activeRewardsAmount.multipliedBy(100).div(365).toNumber()).toNumber()
+      )
+    : numberFormatter(formattedAPR)
 
   useEffect(() => {
     ;(async () => {
@@ -104,7 +125,7 @@ export const PoolStats: FC<{ pool: GAMMAPool }> = ({ pool }): ReactElement => {
           <TooltipContent>This is the yield generated on a 24H basis annualized</TooltipContent>
         </Tooltip>
         <Badge variant="default" size={'lg'} className={'to-brand-secondaryGradient-secondary/50'}>
-          <span className={'font-poppins font-semibold my-0.5'}>{dailyAPR}%</span>
+          <span className={'font-poppins font-semibold my-0.5'}>{apr}%</span>
         </Badge>
       </div>
     </>
