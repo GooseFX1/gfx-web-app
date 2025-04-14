3cde9ecbfb7bf4e819bcd2f52356e15903ca0379
Luke Woodward <20819255+lukewalt@users.noreply.github.com>
2025-04-14 17:31:54 -0400

    fixes dup tooltip prop

--- src/pages/FarmV4/FarmItemsProSort.tsx
+++ src/pages/FarmV4/FarmItemsProSort.tsx
@@ -83,9 +83,8 @@ const FarmItemsProSort: FC = () => {
       {isDesktop && (
         <FarmRowItem
           title={'Fee Tier'}
-          tooltip={`The percentage fee taken by the pool, this influence the rewards you'll earn.`}
-          canSort={false}
           tooltip={`This is a dynamic fee that can vary between 0.01-10% depending on the token volatility`}
+          canSort={false}
         />
       )}
       <FarmRowItem
