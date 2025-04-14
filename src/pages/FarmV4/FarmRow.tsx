2f43649b834f6d9145c6f56479f4a2d8ec41670b
Shashank729 <shashankshekhar729@gmail.com>
2025-04-14 16:45:37 +0530

    Fixes ui issues as per Emiliano

--- src/pages/FarmV4/FarmRow.tsx
+++ src/pages/FarmV4/FarmRow.tsx
@@ -79,7 +79,7 @@ const FarmRow: FC<FarmRowProps> = ({ pool, ...props }) => {
               src={`/img/assets/owner-${mode}.svg`}
               alt="pool-owner"
               size={'sm'}
-              className=" absolute top-0.5 left-0.5"
+              className="absolute top-[-10px] left-[-2px]"
             />
             </TooltipTrigger>
             <TooltipContent>
