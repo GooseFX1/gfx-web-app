ac9fa24cd2b95c6b3a975cda252611b274b6219f
John Daffue <56412825+deadeagle63@users.noreply.github.com>
2025-04-14 18:57:30 +0100

    Swap: Added Half & Max Buttons (#1179)
    Co-authored-by: Luke Woodward <20819255+lukewalt@users.noreply.github.com>

--- src/pages/Swap/Swap.tsx
+++ src/pages/Swap/Swap.tsx
@@ -48,6 +48,7 @@ import useGammaProgramAmmConfig from '@/queries/GAMMA/pools/useGammaProgramAmmCo
 import { QUERY_KEY } from '@/queries/query.helper'
 import { useMutation, useQuery } from '@tanstack/react-query'
 import useGetGammaSwapAccounts from '@/queries/GAMMA/pools/useGetGammaSwapAccounts'
+import { TokenAmount } from '@solana/web3.js'
 
 export const Swap: FC = () => {
   const { isDarkMode, mode } = useDarkMode()
@@ -274,7 +275,24 @@ export const Swap: FC = () => {
     swapAccountsQuery.isFetching ||
     swapMutation.isLoading
   const doesPoolExist = poolStateQuery.isFetched ? !!poolStateQuery.data : true
-
+  const handleHalf = () => {
+    const tokenBal: TokenAmount = balance[selectedTokenA?.address].tokenAmount
+    if (tokenBal.uiAmount == 0.0) {
+      return
+    }
+    amountTokenACommands.set(new Decimal(tokenBal.amount).div(Math.pow(10, tokenBal.decimals)).div(2).toString())
+  }
+  const handleMax = () => {
+    const tokenBal: TokenAmount = balance[selectedTokenA?.address].tokenAmount
+    if (tokenBal.uiAmount == 0.0) {
+      return
+    }
+    let tokenValue = new Decimal(tokenBal.amount).div(Math.pow(10, tokenBal.decimals))
+    if (selectedTokenA.symbol.toLowerCase() === 'sol') {
+      tokenValue = tokenValue.minus(0.01)
+    }
+    amountTokenACommands.set(tokenValue.toString())
+  }
   return (
     <div
       className={`
@@ -385,23 +403,45 @@ mt-8 flex items-center justify-center
             </PopoverContent>
           </Popover>
         </div>
-        <div className={'flex flex-col px-2.5 py-3.75 gap-3.75'}>
-          <div className={'flex w-full flex-col gap-3.75'}>
-            <div className={'flex w-full'}>
-              <h4 className={'text-text-lightmode-primary dark:text-text-darkmode-primary'}>You're Selling:</h4>
-              <p
-                className={cn(
-                  `ml-auto text-b2 cursor-pointer text-text-lightmode-primary dark:text-text-darkmode-primary`,
-                  balance[selectedTokenA?.address].tokenAmount.uiAmount == 0 &&
-                    `cursor-not-allowed text-text-lightmode-tertiary dark:text-text-darkmode-tertiary`
-                )}
-                onClick={() => {
-                  amountTokenACommands.set(balance[selectedTokenA?.address].tokenAmount.uiAmountString)
-                }}
-              >
-                Balance: {numberFormatter(balance[selectedTokenA?.address].tokenAmount.uiAmount)}{' '}
-                {selectedTokenA?.symbol}
-              </p>
+        <div className={'flex flex-col px-2.5 py-3.75 gap-3'}>
+          <div className={'flex w-full flex-col gap-3'}>
+            <div className={'flex w-full md:items-center gap-2 flex-col md:flex-row'}>
+              <div className={'flex w-full'}>
+                <h4 className={'text-text-lightmode-primary dark:text-text-darkmode-primary'}>You're Selling:</h4>
+                <p
+                  className={cn(
+                    `ml-auto text-b2 cursor-pointer text-text-lightmode-primary dark:text-text-darkmode-primary`,
+                    balance[selectedTokenA?.address].tokenAmount.uiAmount == 0 &&
+                      `cursor-not-allowed text-text-lightmode-tertiary dark:text-text-darkmode-tertiary`
+                  )}
+                  onClick={() => {
+                    amountTokenACommands.set(balance[selectedTokenA?.address].tokenAmount.uiAmountString)
+                  }}
+                >
+                  Balance: {numberFormatter(balance[selectedTokenA?.address].tokenAmount.uiAmount)}{' '}
+                  {selectedTokenA?.symbol}
+                </p>
+              </div>
+              {selectedTokenA && (
+                <div className={'flex gap-2'}>
+                  <Button
+                    variant={'outline'}
+                    size={'sm'}
+                    colorScheme={!isDarkMode ? 'blue' : 'default'}
+                    onClick={handleHalf}
+                  >
+                    Half
+                  </Button>
+                  <Button
+                    variant={'outline'}
+                    size={'sm'}
+                    colorScheme={!isDarkMode ? 'blue' : 'default'}
+                    onClick={handleMax}
+                  >
+                    Max
+                  </Button>
+                </div>
+              )}
             </div>
             <div className={'flex flex-col gap-1.5'}>
               <TokenSelectInput
@@ -438,7 +478,7 @@ mt-8 flex items-center justify-center
               amountTokenBCommands.set(amountTokenA)
             }}
           />
-          <div className={'flex w-full flex-col gap-3.75'}>
+          <div className={'flex w-full flex-col gap-3'}>
             <div className={'flex w-full'}>
               <h4 className={'text-text-lightmode-primary dark:text-text-darkmode-primary'}>You're Buying:</h4>
               <p
@@ -573,6 +613,7 @@ mt-8 flex items-center justify-center
               variant={'primary'}
               colorScheme={'blue'}
               fullWidth
+              className={'mt-1'}
               disabled={
                 swapNotValid || loadingPriceQuote || isLoading || !swapAccountsQuery.data || !doesPoolExist
               }
