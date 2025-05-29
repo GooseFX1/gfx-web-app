bb2e29217fd42c5f247fea932dcadee3dc0db17e
John Daffue <daffuejhn@gmail.com>
2025-05-29 19:02:50 +0100

    animation replacement

--- src/pages/TokenFeed/TokenFeed.tsx
+++ src/pages/TokenFeed/TokenFeed.tsx
@@ -1,19 +1,31 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import TokenFeedContainer, {
   TokenFeedContainerHeader,
   TokenFeedContainerHeaderWithTitle,
   TokenFeedContentContainer,
   TokenFeedTokensContainer
 } from '@/pages/TokenFeed/TokenFeedContainer'
-import { Button, Icon, RadioGroup, RadioGroupItem } from 'gfx-component-lib'
+import { Button, cn, Icon, RadioGroup, RadioGroupItem } from 'gfx-component-lib'
 import { useDarkMode } from '@/context'
 import { Circle } from '@/components/common/Circle'
 import { useWallet } from '@solana/wallet-adapter-react'
 import { Connect } from '@/layouts'
+import Lottie from 'lottie-react'
+import PerformanceLite from '@/animations/performance_lite.json'
+import PerformanceDark from '@/animations/performance_dark.json'
+import LockedDark from '@/animations/profile_locked_dark.json'
+import LockedLite from '@/animations/profile_locked_lite.json'
 
 function TokenFeed() {
   const { mode } = useDarkMode()
   const { connected } = useWallet()
+
+  const performanceAnimation = useMemo(() => {
+    if (connected) {
+      return mode === 'dark' ? PerformanceDark : PerformanceLite
+    }
+    return mode === 'dark' ? LockedDark : LockedLite
+  }, [mode, connected])
   return (
     <div
       className={`mt-2 flex flex-col  gap-4 px-7.5  w-full items-center h-full
@@ -38,8 +50,8 @@ function TokenFeed() {
         </Button>
       </div>
       <div
-        className={`grid items-center justify-center gap-4 grid-cols-[repeat(1,_minmax(450px,_1fr))] 
-      md:grid-cols-[repeat(2,_minmax(450px,_1fr))] xl:grid-cols-[repeat(3,_minmax(450px,_1fr))] 
+        className={`grid items-center justify-center gap-4 grid-cols-1
+      md:grid-cols-2 xl:grid-cols-3
       max-w-[1380px]
        `}
       >
@@ -67,9 +79,8 @@ function TokenFeed() {
           </TokenFeedContainerHeader>
           <TokenFeedContentContainer className={'items-center justify-center w-full h-full'}>
             <div className={`flex flex-col items-center justify-between w-full gap-4`}>
-              <img
-                src={`/img/assets/performance_${mode}${connected ? '_connected' : ''}.svg`}
-                className={'w-[80px]'}
+              <Lottie animationData={performanceAnimation}
+              className={cn(`h-[80px]`, connected && `h-[77px]`)}
               />
               <div className={`flex flex-col gap-2 items-center text-center w-[300px]`}>
                 <h2 className={`text-text-lightmode-primary dark:text-text-darkmode-primary`}>My Performance</h2>
--- src/pages/TokenFeed/TokenFeedContainer.tsx
+++ src/pages/TokenFeed/TokenFeedContainer.tsx
@@ -71,7 +71,7 @@ export const TokenFeedTokensContainer: FC<{ tokens: TokenFeedToken[] }> = ({ tok
 function TokenFeedContainer({ children, className }: { children: ReactNode | ReactNode[]; className?: string }) {
   return (
     <div
-      className={cn(`flex flex-col p-4 w-[450px] border-1 border-solid dark:border-border-darkmode-secondary
+      className={cn(`flex flex-col p-4 w-full max-w-[450px] border-1 border-solid dark:border-border-darkmode-secondary
     rounded-[16px] border-border-lightmode-secondary h-full overflow-hidden
     `, className)}
     >
