64bcae5dee4fba1b9766c60fda4627b3077d27d9
John Daffue <daffuejhn@gmail.com>
2025-06-20 20:29:45 +0100

    fixed footer removal

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -7,7 +7,6 @@ import { cn } from 'gfx-component-lib'
 export const AppLayout: FC<{ children: ReactNode }> = ({ children }) => {
   const { rewardModal } = useRewardToggle()
   const { mode } = useDarkMode()
-
   // To enable dark mode using tailwind - using dark:classname
   return (
     <>
@@ -24,8 +23,8 @@ export const AppLayout: FC<{ children: ReactNode }> = ({ children }) => {
         <div className={`min-2xl:w-[2500px] overflow-scroll md:mb-[39px] py-4 no-scrollbar`} id="gfx-app-layout">
           {children}
         </div>
-        <Footer />
       </div>
+      <Footer />
     </>
   )
 }
