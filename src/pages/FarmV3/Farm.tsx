7a0935db937c02689225483212fe756d318cf4f5
Luke Woodward <20819255+lukewalt@users.noreply.github.com>
2025-07-12 11:47:26 -0400

    adds new ssl message

--- src/pages/FarmV3/Farm.tsx
+++ src/pages/FarmV3/Farm.tsx
@@ -3,7 +3,7 @@ import { FarmHeader } from './FarmHeader'
 import { SSLProvider, usePriceFeedFarm } from '../../context'
 import { FarmTable } from './FarmTable'
 import { Faqs } from './Faqs'
-// import { Alert, AlertDescription } from 'gfx-component-lib'
+import { Alert, AlertDescription } from 'gfx-component-lib'
 
 const Farm: FC = () => {
   const { refreshTokenData } = usePriceFeedFarm()
@@ -20,7 +20,16 @@ const Farm: FC = () => {
         className={`dark:bg-black-1 bg-grey-5 h-[calc(100vh - 56px)] overflow-auto max-w-[1440px]
        m-auto no-scrollbar gap-7.5`}
       >
-
+        <div className="mt-4 px-3">
+          <Alert className="p-2 dark:border-white border-grey-2">
+            <AlertDescription className="dark:text-white text-black-4 ">
+              <h5 className="inline">Notice: </h5>
+              SSLv2 pools have officially been deprecated, withdrawals were allowed for a 12 month window
+              between July 2024 to July 2025. The withdrawal period is now over and we are not processing any
+              further withdrawals. This page will be discontinued end of July 2025.{' '}
+            </AlertDescription>
+          </Alert>
+        </div>
         <FarmHeader />
         <div className={'gap-7.5 mt-7.5 p-5 max-sm:px-2.5 max-sm:pb-3.75 pt-0'}>
           <FarmTable />
